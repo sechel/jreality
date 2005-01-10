d18a9fff31312a652081a2cdc1236b03c2625506
Steffen Weissmann <steffen.weissmann@gmail.com>
2005-01-10 21:29:55 +0000

    ...
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@54 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-portal/de/jreality/remote/portal/smrj/PortalServerImplementation.java
+++ src-portal/de/jreality/remote/portal/smrj/PortalServerImplementation.java
@@ -371,8 +371,13 @@ public class PortalServerImplementation extends RemoteDistributedViewer implemen
 
     
     public static void main(String[] args) throws Exception {
+	    boolean nio = true;
+	    try {
+		    nio = !args[1].startsWith("io");
+	    } catch (Exception e) {}
+	    if (!nio) System.err.println("using blocking io!");
         String hostname = INetUtilities.getHostname();
-        PortalServerImplementation rsi = new PortalServerImplementation(new TCPBroadcasterNIO(8868).getRemoteFactory());
+        PortalServerImplementation rsi = new PortalServerImplementation(nio ? new TCPBroadcasterNIO(8868).getRemoteFactory() : new TCPBroadcasterIO(8868).getRemoteFactory());
         rsi.setBackgroundColor(new Color(120, 10, 44, 20));
         rsi.loadWorld(args[0]);
     }
