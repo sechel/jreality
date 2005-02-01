5252ca96c3bebfc484234d7d82fb815ab1be42ac
Charles Gunn <gunn@math.tu-berlin.de>
2005-02-01 17:09:05 +0000

    Newest examples for testing.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@100 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ test/de/jreality/worlds/TestAutomaticTubing.java
@@ -0,0 +1,64 @@
+/*
+ * Created on Feb 1, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package de.jreality.worlds;
+import de.jreality.geometry.GeometryUtility;
+import de.jreality.jogl.DiscreteSpaceCurve;
+import de.jreality.jogl.shader.DefaultVertexShader;
+import de.jreality.scene.Appearance;
+import de.jreality.scene.CommonAttributes;
+import de.jreality.scene.IndexedLineSet;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.util.ConfigurationAttributes;
+import de.jreality.util.Pn;
+import de.jreality.util.SceneGraphUtilities;
+
+
+/**
+ * @author gunn
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class TestAutomaticTubing extends AbstractLoadableScene {
+
+	static double[][] form = {{1,1,1}, {1,-1,1},{1,-1,-1},{1,1,-1},{-1,1,-1},{-1,-1,-1},{-1,-1,1},{-1,1,1}};
+
+	public SceneGraphComponent makeWorld() {
+		IndexedLineSet ils = GeometryUtility.createCurveFromPoints(form, true);
+		ils = DiscreteSpaceCurve.discreteTorusKnot(1.0, .4, 5, 3, 50);
+		SceneGraphComponent root = SceneGraphUtilities.createFullSceneGraphComponent("testAutomaticTubing");
+		root.setGeometry(ils);
+		Appearance ap1 = root.getAppearance();
+		ap1.setAttribute(CommonAttributes.LINE_SHADER+"."+CommonAttributes.DIFFUSE_COLOR, DefaultVertexShader.BLUE);
+//		ap1.setAttribute(CommonAttributes.LINE_SHADER+"."+CommonAttributes.TUBES_DRAW, true);
+		ap1.setAttribute(CommonAttributes.LINE_SHADER+"."+CommonAttributes.TUBE_RADIUS, .03);
+		ap1.setAttribute(CommonAttributes.FACE_DRAW,false);
+		ap1.setAttribute(CommonAttributes.EDGE_DRAW,true);
+		ap1.setAttribute(CommonAttributes.VERTEX_DRAW,true);
+		ap1.setAttribute(CommonAttributes.POINT_SHADER+"."+CommonAttributes.SPHERES_DRAW,true);
+		ap1.setAttribute(CommonAttributes.POINT_SHADER+"."+CommonAttributes.POINT_RADIUS,.03);
+		ap1.setAttribute(CommonAttributes.POINT_SHADER+"."+CommonAttributes.POINT_SIZE, 3.0);
+		ap1.setAttribute(CommonAttributes.POINT_SHADER+"."+CommonAttributes.DIFFUSE_COLOR, DefaultVertexShader.RED);
+		return root;
+	}
+		 
+	public void setConfiguration(ConfigurationAttributes config) {
+	}
+
+		public int getSignature() {
+			return Pn.EUCLIDEAN;
+		}
+
+	 
+
+	public boolean addBackPlane() {
+		return true;
+	}
+	public boolean isEncompass() {
+		return true;
+	}
+}
