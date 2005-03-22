d6377679c997c1fe163cf5484f0d1818280f10b1
Peter Brinkmann <peter.brinkmann@gmail.com>
2005-03-22 21:00:43 +0000

    new scene for testing tools
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@186 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ test/de/jreality/scene/tool/ToolTestScene.java
@@ -0,0 +1,84 @@
+/*
+ * Created on Mar 22, 2005
+ *
+ */
+package de.jreality.scene.tool;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+import de.jreality.scene.Camera;
+import de.jreality.scene.PointLight;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.scene.Sphere;
+import de.jreality.scene.Transformation;
+import de.jreality.scene.Viewer;
+import de.jreality.scene.SceneGraphPath;
+import de.jreality.soft.DefaultViewer;
+
+/**
+ * @author brinkman
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class ToolTestScene {
+
+	//Viewer viewer = new de.jreality.jogl.Viewer();
+	Viewer viewer = new DefaultViewer();
+	JFrame frame = new JFrame("viewer");
+	
+	void createScene() {
+		SceneGraphComponent root = new SceneGraphComponent();
+		root.setName("test root");
+		SceneGraphComponent camNode = new SceneGraphComponent();
+		camNode.setName("test camera");
+		camNode.setTransformation(new Transformation());
+		Camera cam = new Camera();
+		camNode.setCamera(cam);
+		camNode.setLight(new PointLight());
+		
+		SceneGraphComponent scene = new SceneGraphComponent();
+		SceneGraphComponent sphere = new SceneGraphComponent();
+		sphere.setGeometry(new Sphere());
+		//sphere.setGeometry(new CatenoidHelicoid(10));
+		root.addChild(scene);
+		scene.addChild(sphere);
+		root.addChild(camNode);
+		SceneGraphPath camPath = new SceneGraphPath();
+		camPath.push(root);
+		camPath.push(camNode);
+		camPath.push(cam);
+		viewer.setSceneRoot(root);
+		viewer.setCameraPath(camPath);
+		new InputDeviceHandler(viewer);
+		camNode.addTool(new EgoShooterTool());
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		frame.getContentPane().add(viewer.getViewingComponent());
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent arg0) {
+				System.exit(0);
+			}
+		});
+	}
+	
+	public void render() {
+		while (true) {
+			viewer.render();
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void main(String[] args) {
+		ToolTestScene tts = new ToolTestScene();
+		tts.createScene();
+		tts.render();
+	}
+}
