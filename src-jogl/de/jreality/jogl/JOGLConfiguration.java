f2f63a6327997515bd64ebf8f294844cd8dcd313
Charles Gunn <gunn@math.tu-berlin.de>
2005-04-07 14:22:15 +0000

    Centralize all getProperty() calls.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@202 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-jogl/de/jreality/jogl/JOGLConfiguration.java
@@ -0,0 +1,63 @@
+package de.jreality.jogl;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * Author	gunn
+ * Created on Apr 7, 2005
+ *
+ */
+
+/**
+ * @author gunn
+ *
+ */
+public class JOGLConfiguration {
+
+	final public static Logger theLog;
+	static boolean debugGL = false;
+	static boolean sharedContexts = false;
+	static boolean isLinux = false;
+	static boolean multiSample = true;
+	static boolean portalUsage = false;
+	static String resourceDir = System.getProperty("home.dir"), saveResourceDir = System.getProperty("home.dir");
+	static {
+		theLog	= Logger.getLogger("de.jreality.jogl");
+//		theLog.setLevel(Level.FINEST);
+		String foo = System.getProperty("jreality.jogl.debugGL");
+		if (foo != null) { if (foo.equals("false")) debugGL = false; else debugGL =true;}
+		foo = System.getProperty("jreality.jogl.multisample");
+		if (foo != null) 
+			if (foo.indexOf("false") != -1) multiSample = false;
+		foo = System.getProperty("jreality.jogl.portalUsage");
+		if (foo != null) 
+			if (foo.indexOf("true") != -1) portalUsage = true;
+		foo = System.getProperty("os.name");
+		if (foo != null && foo.indexOf("Linux") != -1) isLinux = true;
+		// allocate a GLCanvas to be the "sharer": it will never be destroyed
+		foo = System.getProperty("jreality.jogl.sharedContexts");
+		if (foo != null && foo.indexOf("true") != -1) sharedContexts = true;
+		if (sharedContexts)	{
+//			GLCapabilities capabilities = new GLCapabilities();
+//			firstOne = GLDrawableFactory.getFactory().createGLCanvas(capabilities, null, null);	
+			JOGLConfiguration.theLog.log(Level.WARNING,"Not allowing shared contexts now");
+			sharedContexts=false;
+			theLog.log(Level.INFO,"Using shared contexts: "+sharedContexts);
+		}
+		foo = System.getProperty("jreality.jogl.resourceDir");
+		if (foo != null) saveResourceDir = resourceDir = foo; 
+	}
+	/**
+	 * 
+	 */
+	static JOGLConfiguration sharedInstance = null;
+	private JOGLConfiguration() { super(); }
+
+	public static JOGLConfiguration sharedInstance()	{
+		if (sharedInstance == null) {
+			sharedInstance = new JOGLConfiguration();
+		}
+		return sharedInstance;
+	}
+}
