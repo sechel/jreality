6eece7cf202ebcb9d58362a69e52c63d9f43059f
Steffen Weissmann <steffen.weissmann@gmail.com>
2005-04-13 21:30:38 +0000

    refactored Portal Viewer
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@216 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-portal/de/jreality/portal/PortalJoglClientViewer.java
@@ -0,0 +1,229 @@
+/*
+ * Created on Apr 13, 2005
+ *
+ * This file is part of the de.jreality.portal package.
+ * 
+ * This program is free software; you can redistribute and/or modify 
+ * it under the terms of the GNU General Public License as published 
+ * by the Free Software Foundation; either version 2 of the license, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITTNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307
+ * USA 
+ */
+package de.jreality.portal;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+import de.jreality.jogl.Viewer;
+import de.jreality.scene.Appearance;
+import de.jreality.scene.Camera;
+import de.jreality.scene.CommonAttributes;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.scene.SceneGraphPath;
+import de.jreality.scene.Transformation;
+import de.jreality.scene.proxy.scene.RemoteSceneGraphComponent;
+import de.jreality.util.CameraUtility;
+import de.jreality.util.ConfigurationAttributes;
+import de.jreality.util.P3;
+import de.jreality.util.Rn;
+import de.smrj.ClientFactory;
+
+/**
+ * @author weissman
+ *
+ */
+public class PortalJoglClientViewer implements RemoteJoglViewer, ClientFactory.ResetCallback {
+
+	private Viewer viewer;
+	private JFrame frame;
+	
+    private SceneGraphComponent cameraTranslationNode;
+    private SceneGraphComponent cameraOrientationNode;
+    static double[] correction;
+    static {
+        double[] axis = ConfigurationAttributes.getDefaultConfiguration().getDoubleArray("camera.correction.axis");
+        double angle = ConfigurationAttributes.getDefaultConfiguration().getDouble("camera.correction.angle");
+        angle *= (Math.PI*2.)/360.;
+        correction = P3.makeRotationMatrix(null, axis, angle);
+    }
+
+	ConfigurationAttributes config;
+	
+    private static final class Singleton {
+        private static final PortalJoglClientViewer instance = new PortalJoglClientViewer();
+    }
+    
+    public static PortalJoglClientViewer getInstance() {
+        Singleton.instance.initFrame();
+        return Singleton.instance;
+    }
+    
+    private PortalJoglClientViewer() {
+        viewer = new de.jreality.jogl.Viewer();
+        config = ConfigurationAttributes.getDefaultConfiguration();
+        // frame settings
+        frame = new JFrame(config.getProperty("frame.title", "no title"));
+        frame.addWindowListener(new WindowAdapter() {
+        	public void windowClosing(WindowEvent e) {
+        		System.exit(0);
+        	}
+        });
+        frame.getContentPane().add(viewer.getViewingComponent());
+	    init();
+	    initFrame();
+	    cameraTranslationNode = new SceneGraphComponent();
+	    cameraTranslationNode.setTransformation(new Transformation());
+	    
+	    // TODO: this is a hack!
+	    cameraTranslationNode.addChild(PortalServerViewer.makeLights());
+	    
+	    cameraOrientationNode = new SceneGraphComponent();
+	    cameraOrientationNode.setTransformation(new Transformation());
+        double[] rot = config.getDoubleArray("camera.orientation");
+        cameraOrientationNode.getTransformation().setRotation(
+                rot[0] * ((Math.PI * 2.0) / 360.), rot[1], rot[2], rot[3]);
+        cameraTranslationNode.addChild(cameraOrientationNode);
+    }
+	
+	protected void initFrame() {
+		frame.validate();
+		frame.show();
+	}
+
+	protected void init() {
+		if (config.getBool("frame.fullscreen")) {
+			// disable mouse cursor in fullscreen mode
+			BufferedImage cursorImg = new BufferedImage(16, 16,
+					BufferedImage.TYPE_INT_ARGB);
+			Graphics2D gfx = cursorImg.createGraphics();
+			gfx.setColor(new Color(0, 0, 0, 0));
+			gfx.fillRect(0, 0, 16, 16);
+			gfx.dispose();
+			frame.setCursor(frame.getToolkit().createCustomCursor(cursorImg,
+					new Point(), ""));
+			frame.dispose();
+			frame.setUndecorated(true);
+			frame.getGraphicsConfiguration().getDevice().setFullScreenWindow(frame);
+		} else {
+			frame.setSize(config.getInt("frame.width"), config
+					.getInt("frame.height"));
+		}
+		viewer.setStereoType(de.jreality.jogl.Viewer.CROSS_EYED_STEREO);
+		viewer.setAutoSwapMode(config.getBool("viewer.autoBufferSwap"));
+	}
+
+    public void setManualSwapBuffers(boolean b) {
+        viewer.setAutoSwapMode(!b);
+    }
+    public void swapBuffers() {
+        viewer.swapBuffers();
+    }
+    public void setUseDisplayLists(boolean b) {
+        viewer.getRenderer().setUseDisplayLists(b);
+    }
+    public void waitForRenderFinish() {
+        viewer.waitForRenderFinish();
+    }
+	public void setRemoteSceneRoot(RemoteSceneGraphComponent r) {
+		viewer.setSceneRoot((SceneGraphComponent) r);
+	}
+
+	public void setRemoteCameraPath(List list) {
+		if (viewer.getCameraPath() != null) {
+			SceneGraphPath last = viewer.getCameraPath();
+			last.pop(); // Camera
+			last.getLastComponent().setCamera(null);
+			last.pop(); // Orientation
+			last.pop(); // Translation
+			last.getLastComponent().removeChild(cameraTranslationNode);
+		}
+		if (list == null || list.isEmpty()) {
+			viewer.setCameraPath(null);
+			return;
+		}
+		SceneGraphPath camPath = SceneGraphPath.fromList(list);
+		Camera cam = (Camera) camPath.getLastElement();
+		
+		// make stereo settings
+        cam.setStereo(config.getBool("camera.stereo"));
+        cam.setEyeSeparation(config.getDouble("camera.eyeSeparation"));
+        cam.setNear(config.getDouble("camera.nearPlane"));
+        cam.setFar(config.getDouble("camera.farPlane"));
+        
+        cam.setOnAxis(false);
+		
+		camPath.pop();
+		camPath.getLastComponent().addChild(cameraTranslationNode);
+		cameraOrientationNode.setCamera(cam);
+		camPath.push(cameraTranslationNode);
+		camPath.push(cameraOrientationNode);
+		camPath.push(cam);
+		viewer.setCameraPath(camPath);
+	}
+
+	public void render(double[] headMatrix) {
+		setHeadMatrix(headMatrix);
+		viewer.render();
+	}
+
+	public void setSignature(int sig) {
+		viewer.setSignature(sig);
+	}
+
+	public void reset() {
+		init();
+	}
+
+    public void resetCalled() {
+        System.out.println("disposing prev viewer instance");
+        setRemoteSceneRoot(null);
+        setRemoteCameraPath(null);
+        frame.hide();
+    }
+
+    Transformation t = new Transformation();
+    double[] tmp = new double[16];
+    double[] totalOrientation = new double[16];
+    
+    public void setHeadMatrix(double[] tm) {
+        t.setMatrix(tm);
+        //TODO move sensor between the eyes
+        Camera cam = CameraUtility.getCamera(viewer);
+        cameraTranslationNode.getTransformation().setTranslation(
+                t.getTranslation());
+        Rn.times(tmp, t.getMatrix(), correction);
+        Rn.times(totalOrientation, Rn.inverse(null,
+                cameraOrientationNode.getTransformation().getMatrix()), tmp);
+        cam.setOrientationMatrix(totalOrientation);
+        cam.setViewPort(CameraUtility.calculatePORTALViewport(viewer, t));
+    }
+
+	public void setBackgroundColor(java.awt.Color c) {
+		Appearance ap = viewer.getSceneRoot().getAppearance();
+		if (ap == null)	{
+			ap = new Appearance();
+			viewer.getSceneRoot().setAppearance(ap);
+		}
+		ap.setAttribute(CommonAttributes.BACKGROUND_COLOR, c);
+	}
+	
+
+}
---
+++ src-portal/de/jreality/portal/PortalServerViewer.java
@@ -0,0 +1,355 @@
+package de.jreality.portal;
+import java.awt.Color;
+import java.awt.Component;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+
+import szg.framework.event.HeadEvent;
+import szg.framework.event.HeadMotionListener;
+import szg.framework.event.remote.RemoteEventQueueImpl;
+
+import de.jreality.reader.Readers;
+import de.jreality.remote.portal.smrj.HeadtrackedRemoteJOGLViewer;
+import de.jreality.remote.portal.smrj.HeadtrackedRemoteJOGLViewerImp;
+import de.jreality.scene.Camera;
+import de.jreality.scene.DirectionalLight;
+import de.jreality.scene.Drawable;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.scene.SceneGraphPath;
+import de.jreality.scene.SpotLight;
+import de.jreality.scene.Transformation;
+import de.jreality.scene.Viewer;
+import de.jreality.scene.proxy.scene.RemoteSceneGraphComponent;
+import de.jreality.scene.proxy.smrj.SMRJMirrorScene;
+import de.jreality.scene.tool.DraggingTool;
+import de.jreality.scene.tool.EgoShooterTool;
+import de.jreality.scene.tool.PickSystem;
+import de.jreality.scene.tool.TestTool;
+import de.jreality.scene.tool.ToolSystem;
+import de.jreality.util.CmdLineParser;
+import de.jreality.util.Lock;
+import de.jreality.util.LoggingSystem;
+import de.jreality.util.P3;
+import de.jreality.util.Rn;
+import de.jreality.util.SceneGraphUtilities;
+import de.smrj.RemoteFactory;
+import de.smrj.tcp.TCPBroadcasterIO;
+import de.smrj.tcp.TCPBroadcasterNIO;
+/*
+ * Created on Apr 13, 2005
+ *
+ * This file is part of the  package.
+ * 
+ * This program is free software; you can redistribute and/or modify 
+ * it under the terms of the GNU General Public License as published 
+ * by the Free Software Foundation; either version 2 of the license, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITTNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307
+ * USA 
+ */
+
+/**
+ * @author weissman
+ *
+ */
+public class PortalServerViewer implements Viewer, HeadMotionListener {
+	
+	SceneGraphComponent root;
+	SceneGraphPath camPath;
+	RemoteFactory factory;
+	private int signature;
+
+	RemoteJoglViewer clients;
+    SMRJMirrorScene proxyScene;
+
+    private RemoteEventQueueImpl szgQueue;
+    
+    private ToolSystem toolSystem;
+
+    private final Lock headMatrixLock = new Lock();
+	private final double[] headMatrix = new double[16];
+	private volatile boolean headChanged;
+	private boolean manualSwapBuffers;
+	private boolean rendering;
+	private boolean reRender;
+
+	public PortalServerViewer(RemoteFactory factory) throws IOException, MalformedURLException, RemoteException, NotBoundException {
+		this.factory = factory;
+        clients = (RemoteJoglViewer) factory.createRemoteViaStaticMethod(
+        		PortalJoglClientViewer.class,
+        		PortalJoglClientViewer.class, "getInstance");
+        proxyScene = new SMRJMirrorScene(factory);
+        szgQueue = new RemoteEventQueueImpl();
+	}
+	
+	public void start() {
+        szgQueue.addHeadMotionListener(this);
+	}
+	
+	public void pause() {
+		szgQueue.removeHeadMotionListener(this);
+	}
+	
+	public void headMoved(HeadEvent event) {
+        headMatrixLock.writeLock();
+        System.arraycopy(Rn.transposeF2D(null, event.getMatrix()), 0,
+                headMatrix, 0, 16);
+        headMatrixLock.writeUnlock();
+        headChanged = true;
+        render();
+	}
+
+	public SceneGraphComponent getSceneRoot() {
+		return root;
+	}
+
+	public void setSceneRoot(SceneGraphComponent r) {
+		this.root = r;
+		RemoteSceneGraphComponent rsgc = (RemoteSceneGraphComponent) proxyScene.createProxyScene(root);
+		clients.setRemoteSceneRoot(rsgc);
+	}
+
+	public SceneGraphPath getCameraPath() {
+		return camPath;
+	}
+
+	public void setCameraPath(SceneGraphPath p) {
+		camPath = p;
+		clients.setRemoteCameraPath(proxyScene.getProxies(p.toList()));
+	}
+
+    int statDelay = 10000; // milliseconds between statistic output
+    int runs = 0;
+    int renders = 0;
+    long startTime = System.currentTimeMillis();
+    long maxFrameTime = 0;
+
+    public void render() {
+    	if (root == null || camPath == null) {
+    		LoggingSystem.getLogger(this).log(Level.FINER, "not rendering - roo or camera path is null");
+    		return;
+    	}
+        if (rendering) {
+            reRender = true;
+            return;
+        }
+        rendering = reRender = true;
+        while (reRender) {
+            reRender = false;
+            long start = System.currentTimeMillis();
+            renders++;
+            headMatrixLock.readLock();
+            clients.render(headMatrix);
+            headMatrixLock.readUnlock();
+            clients.waitForRenderFinish();
+            if (manualSwapBuffers) clients.swapBuffers();
+            long delay = System.currentTimeMillis() - start;
+            if (maxFrameTime < delay) maxFrameTime = delay;
+
+            long locTime = System.currentTimeMillis() - startTime;
+            if (locTime >= statDelay) {// statistics
+                System.out.println("************* stats  *****************");
+                System.out.println("elapsed time: " + locTime * 0.001 + " sec");
+                System.out.println("fps: " + ((double) renders)
+                        / ((double) locTime * 0.001));
+                System.out.println("wait(): " + runs);
+                System.out.println("Max. frametime: " + maxFrameTime + "[fps: "
+                        + (1. / (maxFrameTime * 0.001)) + "]");
+                System.out.println("******************************");
+                runs = renders = 0;
+                maxFrameTime = 0;
+                startTime = System.currentTimeMillis();
+            }
+            runs++;
+            rendering = false;
+        }
+    }
+
+	/**
+	 * TODO: open frame for keyboard/mouse input!?
+	 */
+	public boolean hasViewingComponent() {
+		return false;
+	}
+
+	public Component getViewingComponent() {
+		return null;
+	}
+
+	public boolean hasDrawable() {
+		return false;
+	}
+
+	public Drawable getDrawable() {
+		return null;
+	}
+
+	public void initializeFrom(Viewer v) {
+	}
+
+	public int getSignature() {
+		return signature;
+	}
+
+	public void setSignature(int sig) {
+		this.signature = sig;
+	}
+
+    private static String usage(CmdLineParser parser) {
+        String ret = "usage: java PortalServerImplementation "+parser.usageString()+" <filename | classname>\n";
+        ret +=parser.descriptionString();
+        ret +="\tfilename\t3D data file\n";
+        ret +="\tclassname\tfull classname of class that implements de.jreality.scene.LoadableScene\n";
+        return ret;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        CmdLineParser parser = new CmdLineParser();
+        CmdLineParser.Option ioOption = parser.addBooleanOption("io");
+        parser.addDescription("io", "use blocking io");
+        CmdLineParser.Option worldOpt = parser.addBooleanOption('w', "world");
+        parser.addDescription("world", "given argument is classname of a LoadableScene (not a 3d data file)");
+        CmdLineParser.Option propOpt = parser.addStringOption('p', "properties");
+        parser.addDescription("properties", "the jreality property file to use");
+        CmdLineParser.Option scaleOpt = parser.addDoubleOption('s', "scale");
+        parser.addDescription("scale", "the global scale for displaying the given scene");
+        CmdLineParser.Option helpOpt = parser.addBooleanOption('h', "help");
+        parser.addDescription("help", "print this usage message");
+        parser.parse(args);
+        if (((Boolean)parser.getOptionValue(helpOpt)).booleanValue()) {
+            System.out.println(usage(parser));
+            System.exit(0);
+        }
+        String propFile = (String) parser.getOptionValue(propOpt);
+        if (propFile != null) {
+            System.setProperty("jreality.config", Readers.resolveFile(propFile).getAbsolutePath());
+        }
+        System.out.println("jreality.config: "+System.getProperty("jreality.config"));
+        String[] dataArgs = parser.getRemainingArgs();
+        if (dataArgs == null || dataArgs.length != 1) {
+            System.err.println(usage(parser));
+            System.exit(2);
+        }
+        Boolean b = ((Boolean) parser.getOptionValue(ioOption));
+        boolean io = b.booleanValue();
+        if (io) System.err.println("Warning: using blocking IO");
+        PortalServerViewer rsi = new PortalServerViewer(
+                io ? new TCPBroadcasterIO(8868).getRemoteFactory()
+                    : new TCPBroadcasterNIO(8868).getRemoteFactory());
+        Double scale = (Double) parser.getOptionValue(scaleOpt);
+        if (scale != null) {
+            System.out.println("setting scale to "+scale);
+            rsi.setFileScale(scale.doubleValue());
+        }
+        boolean loadWorld = ((Boolean)parser.getOptionValue(worldOpt)).booleanValue();
+        if (loadWorld) rsi.loadWorld(dataArgs[0]);
+        else rsi.loadFile(dataArgs[0]);
+        rsi.setBackgroundColor(new java.awt.Color(0f, 0.2f, 0.2f));
+        rsi.start();
+    }
+
+	public void setBackgroundColor(Color color) {
+		clients.setBackgroundColor(color);
+	}
+
+	private SceneGraphComponent defaultRoot;
+    private SceneGraphPath defaultCamPath;
+    
+    private void initDefaultScene() {
+    	defaultRoot = new SceneGraphComponent();
+    	defaultRoot.setTransformation(new Transformation());
+    	defaultRoot.setName("root node");
+    	SceneGraphComponent portal = new SceneGraphComponent();
+    	portal.setTransformation(new Transformation());
+    	portal.setName("portal node");
+    	defaultRoot.addChild(portal);
+    	Camera camera = new Camera();
+    	portal.setCamera(camera);
+    	defaultCamPath = new SceneGraphPath();
+    	defaultCamPath.push(defaultRoot);
+    	defaultCamPath.push(portal);
+    	defaultCamPath.push(camera);
+    	defaultRoot.addChild(makeLights());
+    	portal.addTool(new EgoShooterTool());
+    }
+    
+    private double fileScale=1.;
+	private void setFileScale(double d) {
+		fileScale = d;
+	}
+
+	private void loadFile(String name) {
+        long t = System.currentTimeMillis();
+        de.jreality.scene.SceneGraphComponent world = Readers.readFile(new File(name));
+        if (world != null) {
+        	if (defaultRoot == null || !defaultCamPath.isValid()) {
+        		initDefaultScene();
+        	}
+        	defaultRoot.addChild(world);
+        	world.addTool(new DraggingTool());
+        	world.addTool(new TestTool());
+        	world.setTransformation(new Transformation());
+        	world.getTransformation().setStretch(fileScale);
+        	setSceneRoot(defaultRoot);
+        	setCameraPath(defaultCamPath);
+        	ToolSystem ts = new ToolSystem(this);
+        	try {
+        		PickSystem ps = (PickSystem) Class.forName("de.jreality.jme.intersection.proxy.JmePickSystem").newInstance();
+        		ts.setPickSystem(ps);
+        	} catch (Exception e) {
+        		LoggingSystem.getLogger(this).log(Level.WARNING, "PickSystem instanciation failed", e);
+        	}
+        }
+        long s = System.currentTimeMillis() - t;
+        System.out.println("loaded file " + name + " successful. ["+s+"ms]");
+	}
+
+	private void loadWorld(String string) {
+		throw new UnsupportedOperationException();
+	}
+
+    static SceneGraphComponent makeLights()    {
+        SceneGraphComponent lights = new SceneGraphComponent();
+        lights.setName("lights");
+        SpotLight pl = new SpotLight();
+        pl.setFalloff(1.0, 0.0, 0.0);
+        pl.setColor(new Color(120, 250, 180));
+        pl.setConeAngle(Math.PI);
+        pl.setIntensity(0.6);
+        SceneGraphComponent l0 = SceneGraphUtilities.createFullSceneGraphComponent("light0");
+        l0.setLight(pl);
+        lights.addChild(l0);
+        DirectionalLight dl = new DirectionalLight();
+        dl.setColor(new Color(250, 100, 255));
+        dl.setIntensity(0.6);
+        l0 = SceneGraphUtilities.createFullSceneGraphComponent("light1");
+        double[] zaxis = {0,0,1};
+        double[] other = {1,1,1};
+        l0.getTransformation().setMatrix( P3.makeRotationMatrix(null, zaxis, other));
+        l0.setLight(dl);
+        lights.addChild(l0);
+        return lights;
+    }
+
+	/**
+	 * @return
+	 */
+	public RemoteEventQueueImpl getSzgQueue() {
+		return szgQueue;
+	}
+
+}
