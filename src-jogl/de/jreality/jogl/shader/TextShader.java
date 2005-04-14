5614f57fe4c58c5e6aea70975b5332df51af7880
Charles Gunn <gunn@math.tu-berlin.de>
2005-04-14 19:30:59 +0000

    New shader for text.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@219 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-jogl/de/jreality/jogl/shader/TextShader.java
@@ -0,0 +1,42 @@
+/*
+ * Author	gunn
+ * Created on Apr 12, 2005
+ *
+ */
+package de.jreality.jogl.shader;
+
+import java.awt.Color;
+
+import net.java.games.jogl.GL;
+
+import de.jreality.jogl.JOGLRenderer;
+import de.jreality.jogl.OpenGLState;
+import de.jreality.scene.CommonAttributes;
+import de.jreality.util.EffectiveAppearance;
+import de.jreality.util.NameSpace;
+
+/**
+ * @author gunn
+ *
+ */
+public class TextShader implements Shader {
+	Color  diffuseColor = null;
+	float[] diffuseColorAsFloat = null;
+	
+	public void postRender(JOGLRenderer jr) {
+	}
+
+	public void render(JOGLRenderer jr) {
+		GL gl = jr.getCanvas().getGL();
+		if (!(OpenGLState.equals(diffuseColorAsFloat, jr.openGLState.diffuseColor, (float) 10E-5))) {
+			gl.glColor4fv( diffuseColorAsFloat);
+			System.arraycopy(diffuseColorAsFloat, 0, jr.openGLState.diffuseColor, 0, 4);
+			//jr.openGLState.diffuseColor = diffuseColorAsFloat;
+		}
+	}
+
+	public void setFromEffectiveAppearance(EffectiveAppearance eap, String name) {
+		diffuseColor = (Color) eap.getAttribute(NameSpace.name(name,CommonAttributes.DIFFUSE_COLOR), CommonAttributes.DIFFUSE_COLOR_DEFAULT);
+		diffuseColorAsFloat = diffuseColor.getRGBComponents(null);
+	}
+}
