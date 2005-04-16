8def4084cd58a269ffe7eb62ba35caad793c7bc2
Peter Brinkmann <peter.brinkmann@gmail.com>
2005-04-16 01:07:19 +0000

    added some tests
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@223 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- test/de/jreality/util/MatrixTest.java
+++ test/de/jreality/util/MatrixTest.java
@@ -30,15 +30,165 @@ import junit.framework.TestCase;
  **/
 public class MatrixTest extends TestCase {
 
-    public void testGetColumn() {
-        Matrix m = new Matrix();
-        double[] thirdColumn = m.getColumn(2);
-        assertEquals(thirdColumn, new double[]{0,0,1,0});
-    }
+	final Matrix id = new Matrix(new double[] {
+			1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1}
+	);
+	Matrix rnd;
+	final double[] range = new double[] {
+			0, 1, 2, 3,
+			4, 5, 6, 7,
+			8, 9, 10, 11,
+			12, 13, 14, 15
+	};
+	
+	protected void setUp() {
+	     double[] v = new double[16];
+	     do {
+	     	for(int i=0; i<16; i++) {
+	     		v[i]=Math.random();
+	     	}
+	     	rnd = new Matrix(v);
+	     } while (rnd.getDeterminant()<Matrix.TOLERANCE);  
+	}
+	
+	public void testConstructor() {
+		Matrix m0 = new Matrix();
+		Matrix m1 = new Matrix(range);
+		Matrix m2 = new Matrix(m1);
+		assertTrue(m1.getArray().length==m2.getArray().length);
+		assertTrue(m1.getArray().length==16);
+		assertFalse(m1.getArray()==m2.getArray());
+		assertTrue(m1.getArray()==range);
+		for(int i=0; i<16; i++) {
+			assertTrue(m1.getArray()[i] == m2.getArray()[i]);
+			assertTrue(m0.getArray()[i] == id.getArray()[i]);
+		}
+	}
+	
+	public void testEquals() {
+		Matrix m1 = new Matrix(range);
+		Matrix m2 = new Matrix(m1);
+		assertTrue(m1.equals(m2));
+		for(int i=0; i<16; i++) {
+			double tmp = m1.getArray()[i];
+			m1.getArray()[i]=-1;
+			assertFalse(m1.equals(m2));
+			m1.getArray()[i]=tmp;
+		}
+		
+	}
+	
+	public void testAssign() {
+		Matrix m = new Matrix();
+		double[] v = m.getArray();
+		m.assignFrom(range);
+		assertTrue(m.getArray()==v);
+		for(int i=0; i<16; i++) {
+			assertTrue(m.getArray()[i]==range[i]);
+		}
+		m.assignFrom(rnd);
+		assertTrue(m.getArray()==v);
+		for(int i=0; i<16; i++) {
+			assertTrue(m.getArray()[i]==rnd.getArray()[i]);
+		}
+		m.assignIdentity();
+		assertTrue(m.getArray()==v);
+		assertTrue(m.equals(id));
+	}
+	
+	public void testEntry() {
+		Matrix m = new Matrix(rnd);
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<4; j++) {
+				assertTrue(m.getArray()[4*i+j]==m.getEntry(i, j));
+				double x=Math.random();
+				m.setEntry(i, j, x);
+				assertTrue(m.getEntry(i, j)==x);
+			}
+		}
+	}
+	
+	public void testTrace() {
+		Matrix m = new Matrix(range);
+		assertTrue(m.getTrace()==30);
+	}
+	
+	public void testScale() {
+		Matrix m = new Matrix(rnd);
+		double x = Math.random();
+		m.scale(x);
+		for(int i=0; i<16; i++) {
+			assertTrue(m.getArray()[i]==rnd.getArray()[i]*x);
+		}
+	}
+	
+	public void testTranspose() {
+		Matrix m = new Matrix(rnd);
+		m.transpose();
+		Matrix mm = m.getTranspose();
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<4; j++) {
+				assertTrue(m.getEntry(i, j)==rnd.getEntry(j, i));
+				assertTrue(mm.getEntry(i, j)==rnd.getEntry(i, j));
+			}
+		}
+	}
+	
+	public void testWrite() {
+		double[] v = rnd.writeToArray(null);
+		assertTrue(v!=rnd.getArray());
+		for(int i=0; i<16; i++) {
+			assertTrue(v[i]==rnd.getArray()[i]);
+		}
+		double[] v0=v;
+		(new Matrix(range)).writeToArray(v);
+		for(int i=0; i<16; i++) {
+			assertTrue(v[i]==range[i]);
+		}
+	}
+	
+	
+/*   
+  
+  TODO: Write tests for the following methods:
+  
+    public static Matrix product(Matrix A, Matrix B)
+    public static Matrix sum(Matrix A, Matrix B)
+    public static Matrix conjugate(Matrix A, Matrix B)
+    public double getDeterminant()
+    public double[] getRow(int i) 
+    public void setRow(int i, double[] v)
+    public double[] getColumn(int i)
+    public void setColumn(int i, double[] v)
+    public void multiplyOnRight(double[] T)
+    public void multiplyOnRight(Matrix T)
+    public void multiplyOnLeft(double[] T)
+    public void multiplyOnLeft(Matrix T)
+    public void conjugateBy(Matrix T)
+    public void add(Matrix T)
+    public Matrix getInverse() 
+    public void invert()
+    public String toString()
     
-    static void assertEquals(double[] v1, double[] v2) {
-        // null test missing
-        assert (v1.length == v2.length);
-        for (int i = 0; i < v1.length; i++) assertEquals(v1[i], v2[i], 0);
-    }
+Done, sort of:
+    public Matrix()
+    public Matrix(Matrix T)
+    public Matrix(double[] m)
+    public boolean equals(Matrix T) 
+    public void assignFrom(double[] initValue)
+    public void assignFrom(Matrix initValue) 
+    public void assignIdentity()
+    public double[] getArray()
+    public double getEntry(int row, int column)
+    public void setEntry(int row, int column, double value)
+    public double getTrace()
+    public void scale(double lambda)
+    public Matrix getTranspose()
+    public void transpose()
+    public double[] writeToArray(double[] aMatrix)
+ 
+*/
 }
