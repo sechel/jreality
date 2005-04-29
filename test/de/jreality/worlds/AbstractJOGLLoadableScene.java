2cb6be17629fae41eca7101404f6d0d3d6e01ada
Charles Gunn <gunn@math.tu-berlin.de>
2005-04-29 07:48:14 +0000

    As the name implies.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@251 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ test/de/jreality/worlds/AbstractJOGLLoadableScene.java
@@ -0,0 +1,41 @@
+/*
+ * Author	gunn
+ * Created on Apr 28, 2005
+ *
+ */
+package de.jreality.worlds;
+
+import java.awt.Component;
+
+import javax.swing.JMenuBar;
+
+import de.jreality.jogl.JOGLLoadableScene;
+import de.jreality.scene.Viewer;
+import de.jreality.util.ConfigurationAttributes;
+
+/**
+ * @author gunn
+ *
+ */
+public abstract class AbstractJOGLLoadableScene extends AbstractLoadableScene implements JOGLLoadableScene {
+	
+	ConfigurationAttributes config = null;
+
+	public void setConfiguration(ConfigurationAttributes config) {
+		this.config = config;
+	}
+	
+	public Component getInspector()	{ return null; }
+
+	public boolean addBackPlane() {
+		return false;
+	}
+	
+	public boolean isEncompass() {
+		return false;
+	}
+
+	public void dispose() {}
+	
+
+}
