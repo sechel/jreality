bfb6f346fab6c3102e07f5c8fe32df5bd4fae78e
Charles Gunn <gunn@math.tu-berlin.de>
2005-05-30 15:59:14 +0000

    A junit test for the new pick support for world-space rays.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@297 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ test/de/jreality/util/TestWorldPick.java
@@ -0,0 +1,57 @@
+/*
+ * Author	gunn
+ * Created on May 30, 2005
+ *
+ */
+package de.jreality.util;
+
+import java.util.Vector;
+
+import de.jreality.geometry.Primitives;
+import de.jreality.jogl.InteractiveViewer;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.scene.Sphere;
+import de.jreality.scene.Transformation;
+import de.jreality.scene.pick.PickAction;
+import de.jreality.scene.pick.PickPoint;
+import de.jreality.util.P3;
+import de.jreality.util.Rn;
+import junit.framework.TestCase;
+
+/**
+ * @author gunn
+ *
+ */
+public class TestWorldPick extends TestCase {
+
+	public void testWorldPick()	{
+		SceneGraphComponent root = new SceneGraphComponent();
+		root.setName("root");
+		SceneGraphComponent world = new SceneGraphComponent();
+		world.setName("world1");
+		world.setGeometry(new Sphere()); //Primitives.cube()); //
+		world.getGeometry().setName("sphere");
+		world.setTransformation(new Transformation());
+		world.getTransformation().setTranslation(0.0, 0.0, 1.0);
+		root.addChild(world);
+		world = new SceneGraphComponent();
+		world.setName("world2");
+		world.setGeometry(Primitives.cube()); //
+		world.getGeometry().setName("cube");
+		world.setTransformation(new Transformation());
+		world.getTransformation().setTranslation(0.0, 0.0, -1.0);
+		root.addChild(world);
+		InteractiveViewer iv = new InteractiveViewer(null, root);
+		PickAction pa = new PickAction(iv);
+		double[] p0 = {.5,.5,.5,1};
+		double[] p1 = {-.5, -.5, -.5,0};
+		pa.setPickSegment(p0, p1);
+		Vector hits = (Vector) pa.visit();
+		if (hits != null )	{
+			for (int i = 0; i< hits.size(); ++i)	{
+				PickPoint thisone = (PickPoint) hits.get(i);
+				System.out.println(thisone.toString());				
+			}
+		}
+	}
+}
