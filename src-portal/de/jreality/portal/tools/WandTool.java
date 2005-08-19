37cd35328c77eb03b69cb0354780d282f3fe9d41
Steffen Weissmann <steffen.weissmann@gmail.com>
2005-08-19 19:53:08 +0000

    did some cleanup
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@455 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-portal/de/jreality/portal/tools/WandTool.java
+++ src-portal/de/jreality/portal/tools/WandTool.java
@@ -6,12 +6,9 @@
  */
 package de.jreality.portal.tools;
 
-import javax.swing.text.NavigationFilter;
-
 import szg.framework.event.WandEvent;
 import szg.framework.event.WandListener;
 import szg.framework.event.WandMotionListener;
-import de.jreality.math.P3;
 import de.jreality.math.Rn;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.scene.Transformation;
