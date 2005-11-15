34775ec6f7544274f54bb3124c02a5b150441209
Steffen Weissmann <steffen.weissmann@gmail.com>
2005-11-15 12:52:39 +0000

    continued shader stuff
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@516 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- test/de/jreality/shader/SubEntityTest.java
+++ test/de/jreality/shader/SubEntityTest.java
@@ -1,6 +1,7 @@
 package de.jreality.shader;
 
 import java.awt.Color;
+import java.lang.reflect.UndeclaredThrowableException;
 import java.util.Arrays;
 
 import de.jreality.scene.Appearance;
@@ -8,6 +9,40 @@ import de.jreality.scene.data.AttributeEntityUtility;
 import junit.framework.TestCase;
 
 public class SubEntityTest extends TestCase {
+
+  public void testDefaultEntities() {
+    
+    Appearance a = new Appearance();
+    EffectiveAppearance ea = EffectiveAppearance.create().create(a);
+
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(DefaultGeometryShader.class, "", a));
+    assertTrue(AttributeEntityUtility.hasAttributeEntity(DefaultGeometryShader.class, "", ea));
+
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(PointShader.class, "", a));
+    assertTrue(AttributeEntityUtility.hasAttributeEntity(PointShader.class, "", ea));
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(DefaultPointShader.class, "", a));
+    assertTrue(AttributeEntityUtility.hasAttributeEntity(DefaultPointShader.class, "", ea));
+
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(LineShader.class, "", a));
+    assertTrue(AttributeEntityUtility.hasAttributeEntity(LineShader.class, "", ea));
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(DefaultLineShader.class, "", a));
+    assertTrue(AttributeEntityUtility.hasAttributeEntity(DefaultLineShader.class, "", ea));
+
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(PolygonShader.class, "", a));
+    assertTrue(AttributeEntityUtility.hasAttributeEntity(PolygonShader.class, "", ea));
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(DefaultPolygonShader.class, "", a));
+    assertTrue(AttributeEntityUtility.hasAttributeEntity(DefaultPolygonShader.class, "", ea));
+    
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(Texture2D.class, "", a));
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(Texture2D.class, "", ea));
+
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(CubeMap.class, "", a));
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(CubeMap.class, "", ea));
+
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(TwoSidePolygonShader.class, "", a));
+    assertFalse(AttributeEntityUtility.hasAttributeEntity(TwoSidePolygonShader.class, "", ea));
+  
+  }
 
   public void testDefaultGeometryShader() {
     Appearance a = new Appearance();
@@ -20,16 +55,14 @@ public class SubEntityTest extends TestCase {
     DefaultPolygonShader ps = (DefaultPolygonShader) gs.getPolygonShader();
     
     ps.setAmbientCoefficient(1.2);
-    ps.setSmoothShading(false);
-    
-    
+    ps.setSmoothShading(false);  
 
     DefaultPointShader dps = (DefaultPointShader)gs.getPointShader();
     
-    TwoSidePolygonShader tps = (TwoSidePolygonShader) dps.createPolygonShader("TwoSidePolygonShader");
+    TwoSidePolygonShader tps = (TwoSidePolygonShader) dps.createPolygonShader("twoSide");
     
-    DefaultPolygonShader back = (DefaultPolygonShader) tps.createBack("DefaultPolygonShader");
-    DefaultPolygonShader front = (DefaultPolygonShader) tps.createFront("DefaultPolygonShader");
+    DefaultPolygonShader back = (DefaultPolygonShader) tps.getBack();
+    DefaultPolygonShader front = (DefaultPolygonShader) tps.getFront();
     
     back.setDiffuseColor(Color.BLACK);
     front.setDiffuseColor(Color.WHITE);
@@ -41,4 +74,22 @@ public class SubEntityTest extends TestCase {
     System.out.println(a);
   }
   
+  public void testDefaultGeometryShaderRead() {
+    EffectiveAppearance ea = EffectiveAppearance.create();
+    
+    DefaultGeometryShader gs = (DefaultGeometryShader) AttributeEntityUtility.createAttributeEntity(DefaultGeometryShader.class, "", ea);
+    
+    DefaultPolygonShader ps = (DefaultPolygonShader) gs.getPolygonShader();
+    DefaultLineShader ls = (DefaultLineShader) gs.getLineShader();
+    DefaultPointShader pos = (DefaultPointShader) gs.getPointShader();
+    
+    System.out.println("\n***********");
+    
+    System.out.println(ps.getTexture2d());
+
+    System.out.println("\n***********");
+
+    
+    System.out.println(gs);
+  }
 }
