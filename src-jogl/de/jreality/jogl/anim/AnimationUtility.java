f63f3de434a07c292949e06407eb2e0dd258d5bd
Charles Gunn <gunn@math.tu-berlin.de>
2006-02-09 10:25:30 +0000

    Removed old method.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@695 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-jogl/de/jreality/jogl/anim/AnimationUtility.java
+++ src-jogl/de/jreality/jogl/anim/AnimationUtility.java
@@ -8,7 +8,7 @@ import de.jreality.math.FactoredMatrix;
 import de.jreality.math.Pn;
 import de.jreality.math.Quaternion;
 import de.jreality.scene.Camera;
-import de.jreality.scene.Transformation;
+
 
 /**
  * @author gunn
@@ -20,41 +20,6 @@ public class AnimationUtility {
 		super();
 	}
 	
-	// form the transformation so that when s = 0, dst = t1 and when s=1,  dst = t2
-	public static Transformation linearInterpolation(Transformation dst, Transformation t1, Transformation t2, double s ) {
-		if (dst == null) dst = new Transformation();
-		int sig = t1.getSignature();
-		if (sig != t2.getSignature())	{
-			// TODO signal error
-			t2.setSignature(sig);
-		}
-		dst.setSignature(sig);
-		
-		double[] trans1 = t1.getTranslation();
-		double[] trans2 = t2.getTranslation();
-		double[] trans = Pn.linearInterpolation(null,trans1, trans2, s, sig);
-		
-//		double[] center1 = t1.getCenter();
-//		double[] center2 = t2.getCenter();
-//		double[] center = null;
-//		if (center1 != null && center2 != null)
-//			center = Pn.linearInterpolation(null,center1, center2, s, sig);
-//		
-		double[] stretch1 = t1.getStretch();
-		double[] stretch2 = t2.getStretch();
-		double[] stretch = Pn.linearInterpolation(null,stretch1, stretch2, s, Pn.EUCLIDEAN);
-		
-		Quaternion rot1 = t1.getRotationQuaternion();
-		Quaternion rot2 = t2.getRotationQuaternion();
-		Quaternion rot = Quaternion.linearInterpolation(null, rot1, rot2, s);
-
-		dst.setTranslation(trans);
-//		if (center != null) dst.setCenter(center);
-		dst.setStretch(stretch);
-		dst.setRotation(rot);
-		return dst;
-	}
-
 	public static FactoredMatrix linearInterpolation(FactoredMatrix dst, FactoredMatrix t1, FactoredMatrix t2, double s ) {
 		if (dst == null) dst = new FactoredMatrix(t1.getSignature());
 		int sig = t1.getSignature();
@@ -87,7 +52,7 @@ public class AnimationUtility {
 		return dst;
 	}
 	
-	//	 form the transformation so that when s = 0, dst = t1 and when s=1,  dst = t2
+	//	 form the FactoredMatrix so that when s = 0, dst = t1 and when s=1,  dst = t2
 	public static Camera linearInterpolation(Camera dst, Camera t1, Camera t2, double s ) {
 		return dst;
 	}
