577598e3e0cefd69de650cc7b42d7f56715ccd20
Tim Hoffmann <tim.hoffmann@ma.tum.de>
2006-03-02 18:00:46 +0000

    *** empty log message ***
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@737 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-backends-share/de/jreality/backends/label/LabelUtility.java
+++ src-backends-share/de/jreality/backends/label/LabelUtility.java
@@ -1,7 +1,40 @@
 package de.jreality.backends.label;
 
-public class LabelUtility {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 
+import com.sun.media.sound.Toolkit;
+
+public class LabelUtility {
+	
+	private static FontRenderContext frc;
+	
+	//TODO is there a better way to get a FontRenderContext???
+	static {
+		BufferedImage bi = new BufferedImage(1,1,BufferedImage.TYPE_INT_ARGB);
+		frc = bi.createGraphics().getFontRenderContext();
+	}
   private LabelUtility() {}
 
+  
+  private static Color TRANSPARENT = new Color(0,0,0,0);
+  
+  public static Image createImageFromString(String s, Font f,Color color) {
+	  Rectangle r = f.getStringBounds(s,frc).getBounds();
+	  BufferedImage img = new BufferedImage(r.width,r.height,BufferedImage.TYPE_INT_ARGB);
+	  Graphics2D g = (Graphics2D) img.getGraphics();
+	  g.setBackground(TRANSPARENT);
+	  g.clearRect(0,0,r.width,r.height);
+	  g.setColor(color);
+	  g.setFont(f);
+	  float desc = f.getLineMetrics(s,frc).getDescent();
+	  g.drawString(s,0,r.height-desc);
+	  return img;
+  }
 }
---
+++ test-backends-share/de/jreality/backends/label/TestLabelImages.java
@@ -0,0 +1,30 @@
+package de.jreality.backends.label;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TestLabelImages  extends JPanel {
+
+	
+	public void paintComponent(Graphics g) {
+		Image i = LabelUtility.createImageFromString("hello World p g",
+				new Font("Times New Roman",Font.ITALIC,23), Color.RED );
+		g.drawImage(i,0,0,null);
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame f = new JFrame("test label Images");
+		f.add(new TestLabelImages());
+		f.setSize(300,300);
+		f.setVisible(true);
+
+	}
+
+}
