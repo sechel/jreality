eabb83cd81acc1cd0ea44e6a51124b08d4d77587
Steffen Weissmann <steffen.weissmann@gmail.com>
2006-03-03 21:16:17 +0000

    started caching
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@754 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-backends-share/de/jreality/backends/label/LabelUtility.java
+++ src-backends-share/de/jreality/backends/label/LabelUtility.java
@@ -9,6 +9,11 @@ import java.awt.font.FontRenderContext;
 import java.awt.font.LineMetrics;
 import java.awt.font.TextLayout;
 import java.awt.image.BufferedImage;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.WeakHashMap;
 
 import de.jreality.geometry.GeometryUtility;
 import de.jreality.geometry.IndexedFaceSetFactory;
@@ -16,12 +21,18 @@ import de.jreality.geometry.Primitives;
 import de.jreality.math.P3;
 import de.jreality.math.Pn;
 import de.jreality.scene.Appearance;
+import de.jreality.scene.Geometry;
 import de.jreality.scene.IndexedFaceSet;
+import de.jreality.scene.PointSet;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.scene.Transformation;
 import de.jreality.scene.data.Attribute;
 import de.jreality.scene.data.AttributeEntityUtility;
+import de.jreality.scene.data.DataList;
+import de.jreality.scene.data.DoubleArrayArray;
+import de.jreality.scene.data.IntArrayArray;
 import de.jreality.scene.data.StorageModel;
+import de.jreality.scene.data.StringArray;
 import de.jreality.shader.ImageData;
 import de.jreality.shader.Texture2D;
 
@@ -38,6 +49,43 @@ public class LabelUtility {
 
   
   private static Color TRANSPARENT = new Color(0,0,0,0);
+  
+  private static final ReferenceQueue refQueue = new ReferenceQueue();
+  private static final HashMap geometryToMaps = new HashMap();
+  
+  private static BufferedImage[] createImages(Geometry geom, int type, StringArray labels, Color color, Font font) {
+    BufferedImage[] ret = new BufferedImage[labels.getLength()];
+    for (Object ref=refQueue.poll(); ref != null; ref=refQueue.poll()) {
+      geometryToMaps.remove(ref);
+    }
+    HashMap keyToImageMap = (HashMap) geometryToMaps.get(geom);
+    if (keyToImageMap == null) {
+      keyToImageMap = new HashMap();
+      geometryToMaps.put(geom, keyToImageMap);
+    }
+    Key key = new Key(geom, type, font, color);
+    HashMap strToImages = (HashMap) keyToImageMap.get(key);
+    if (strToImages == null) {
+      strToImages = new HashMap();
+      keyToImageMap.put(key, strToImages);
+    }
+    for (int i = 0, n=labels.getLength(); i < n; i++) {
+      String str = labels.getValueAt(i);
+      ret[i] = (BufferedImage) strToImages.get(str);
+      if (ret[i] == null) {
+        ret[i] = createImageFromString(str, font, color);
+        strToImages.put(str, ret[i]);
+      }
+    }
+    return ret;
+  }
+  
+  public static BufferedImage[] createPointImages(PointSet ps, Font f, Color c) {
+    DataList dl = ps.getVertexAttributes(Attribute.LABELS);
+    if (dl == null) return null;
+    StringArray sa = dl.toStringArray();
+    return createImages(ps, Key.TYPE_POINTS, sa, c, f);
+  }
   
   public static BufferedImage createImageFromString(String s, Font f,Color color) {
 	  //Rectangle r = f.getStringBounds(s,frc).getBounds();
@@ -72,4 +120,34 @@ public class LabelUtility {
 
   		return sgc;
   	}
+    
+    private static class Key {
+      
+      final static int TYPE_POINTS=0;
+      final static int TYPE_EDGES=2;
+      final static int TYPE_FACES=3;
+      
+      private Geometry geometry;
+      private int type;
+      private Font font;
+      private Color color;
+
+      private final int hash;
+      
+      Key(Geometry g, int t, Font f, Color c) {
+        if (t<0 || t>3) throw new IllegalArgumentException("no such type");
+        geometry=g; type=t; font=f; color=c;
+        hash = 37*37*37*c.hashCode()+37*37*g.hashCode()+37*f.hashCode()+t;
+      }
+      
+      public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof Key)) return false;
+        Key k = (Key) obj;
+        return k.hashCode() == hashCode() && k.geometry.equals(geometry) && k.type == type && k.font.equals(font) && k.color.equals(color);
+      }
+      
+      public int hashCode() {
+        return hash;
+      }
+    }
   }
