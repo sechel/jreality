e5e669602b5d641394395ad35d716b4bbae0cd1f
Nils Bleicher <bleicher@math.tu-berlin.de>
2006-03-17 16:32:21 +0000

    *** empty log message ***
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@819 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ test/de/jreality/scene/pick/TestBruteForcePicking.java
@@ -0,0 +1,70 @@
+package de.jreality.scene.pick;
+
+import de.jreality.examples.CatenoidHelicoid;
+import de.jreality.math.MatrixBuilder;
+import de.jreality.scene.Cylinder;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.scene.Sphere;
+import de.jreality.ui.viewerapp.ViewerApp;
+
+public class TestBruteForcePicking implements Runnable{
+	
+	private CatenoidHelicoid ch;
+	double t=0;
+	double speed=0.001;
+	int sleeptime=25;
+
+	public static void main(String[] args) {
+
+		TestBruteForcePicking test=new TestBruteForcePicking();		
+		test.animate();
+	}
+	
+	public TestBruteForcePicking(){
+		AABBPickSystem.defaultBuildTree=false;
+		
+		SceneGraphComponent geoNode=new  SceneGraphComponent();
+		
+		SceneGraphComponent cmpCH = new SceneGraphComponent();
+		SceneGraphComponent cmpSp = new SceneGraphComponent();
+		SceneGraphComponent cmpCyl = new SceneGraphComponent();
+		
+		ch = new CatenoidHelicoid(20);
+		Sphere sp = new Sphere();
+		Cylinder cyl = new Cylinder();
+		
+		MatrixBuilder.euclidean().translate(1,1,1).scale(1.5).assignTo(cmpSp);
+		MatrixBuilder.euclidean().translate(-1,1,1).assignTo(cmpCyl);
+		
+		//cmpCH.setGeometry(ch);
+		cmpSp.setGeometry(sp);
+		cmpCyl.setGeometry(cyl);
+		
+		geoNode.addChild(cmpCH);
+		geoNode.addChild(cmpSp);
+		geoNode.addChild(cmpCyl);
+		
+		ViewerApp.display(geoNode);		
+	}
+
+	private void animate() {
+		Thread th = new Thread(this);
+		th.start();		
+	}
+	
+	public void run(){
+		
+		while (true) {
+			t=t+speed;
+			if(t>=1){ t=0;}					
+			ch.setAlpha(t*Math.PI*2);
+	    	try {
+	    		Thread.sleep(sleeptime);
+	    	} catch (InterruptedException e){
+	    	}
+	        
+	     }		
+	}
+	
+	
+}
