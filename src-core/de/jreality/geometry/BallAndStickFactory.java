d27d924be737466b763c451b45fc1929aa06bc77
Charles Gunn <gunn@math.tu-berlin.de>
2006-04-24 14:58:18 +0000

    make balls/ sticks optional
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@894 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-core/de/jreality/geometry/BallAndStickFactory.java
+++ src-core/de/jreality/geometry/BallAndStickFactory.java
@@ -22,13 +22,14 @@ public class BallAndStickFactory {
 	 IndexedLineSet ils;
 	 double stickRadius=.025, ballRadius=.05;
 	 Color stickColor = Color.YELLOW, ballColor=Color.GREEN, arrowColor = Color.RED;
-	 Appearance ballsAp, sticksAp, arrowsAp, topAp;
 	 int signature = Pn.EUCLIDEAN;
+	 boolean showBalls = true, showSticks = true;
 	 SceneGraphComponent theResult;
 	 boolean drawArrows = false;
 	 double arrowPosition = .5;		// where is tip of arrow placed?
 	 double arrowScale = .1;			// scale=1:  height of cone is length of edge
 	 double arrowSlope = 1.0;			// bigger scale: more pointy arrow profile
+	 Appearance ballsAp, sticksAp, arrowsAp, topAp;
 	 private static IndexedFaceSet urCone = null;
 		public static double[][] octagonalCrossSection = {{1,0,-1}, 
 			{.707, .707, -1}, 
@@ -52,61 +53,65 @@ public class BallAndStickFactory {
 	 public void update()	{
 		 	// create sticks on edges
 			SceneGraphComponent sticks = new SceneGraphComponent();
-			if (sticksAp == null) sticksAp =  new Appearance();
-			sticksAp.setAttribute(CommonAttributes.FACE_DRAW, true);
-			sticksAp.setAttribute(CommonAttributes.EDGE_DRAW, false);
-			sticksAp.setAttribute(CommonAttributes.VERTEX_DRAW, false);
-			if (stickColor != null) sticksAp.setAttribute(CommonAttributes.POLYGON_SHADER+"."+CommonAttributes.DIFFUSE_COLOR, stickColor);
-			sticks.setAppearance(sticksAp);
-			if (arrowsAp == null) arrowsAp = new Appearance();
-			arrowsAp.setAttribute(CommonAttributes.POLYGON_SHADER+"."+CommonAttributes.DIFFUSE_COLOR, arrowColor);
-			DataList vertices = ils.getVertexAttributes(Attribute.COORDINATES);
-			int n = ils.getNumEdges();
-			for (int i = 0; i<n; ++i)	{
-				int[] ed = ils.getEdgeAttributes(Attribute.INDICES).item(i).toIntArray(null);
-				int m = ed.length;
-				for (int j = 0; j<m-1; ++j)	{
-					int k = ed[j];
-					double[] p1 = vertices.item(k).toDoubleArray(null);	
-					k = ed[j+1];
-					double[] p2 = vertices.item(k).toDoubleArray(null);	
-					SceneGraphComponent cc = TubeUtility.tubeOneEdge(p1, p2, stickRadius, null, signature);
-					if (cc != null) sticks.addChild(cc);
-					if (drawArrows)		{
-						FactoredMatrix arrowM = new FactoredMatrix(signature);
-						double d;
-						if (p1.length == 3) d = Rn.euclideanDistance(p1, p2);
-						else d = Pn.distanceBetween(p1, p2, signature);
-						double flatten = arrowSlope/(d);
-						double stretch = arrowScale/stickRadius;
-						arrowM.setStretch(stretch, stretch, arrowScale*flatten);
-						arrowM.setTranslation(0,0,arrowPosition-.5);
-						SceneGraphComponent arrow = new SceneGraphComponent();
-						arrow.setAppearance(arrowsAp);
-						arrowM.update();
-						arrowM.assignTo(arrow);
-						arrow.setGeometry(urCone);
-						cc.addChild(arrow);
+			if (showSticks)	{
+				if (sticksAp == null) sticksAp =  new Appearance();
+				sticksAp.setAttribute(CommonAttributes.FACE_DRAW, true);
+				sticksAp.setAttribute(CommonAttributes.EDGE_DRAW, false);
+				sticksAp.setAttribute(CommonAttributes.VERTEX_DRAW, false);
+				if (stickColor != null) sticksAp.setAttribute(CommonAttributes.POLYGON_SHADER+"."+CommonAttributes.DIFFUSE_COLOR, stickColor);
+				sticks.setAppearance(sticksAp);
+				if (arrowsAp == null) arrowsAp = new Appearance();
+				arrowsAp.setAttribute(CommonAttributes.POLYGON_SHADER+"."+CommonAttributes.DIFFUSE_COLOR, arrowColor);
+				DataList vertices = ils.getVertexAttributes(Attribute.COORDINATES);
+				int n = ils.getNumEdges();
+				for (int i = 0; i<n; ++i)	{
+					int[] ed = ils.getEdgeAttributes(Attribute.INDICES).item(i).toIntArray(null);
+					int m = ed.length;
+					for (int j = 0; j<m-1; ++j)	{
+						int k = ed[j];
+						double[] p1 = vertices.item(k).toDoubleArray(null);	
+						k = ed[j+1];
+						double[] p2 = vertices.item(k).toDoubleArray(null);	
+						SceneGraphComponent cc = TubeUtility.tubeOneEdge(p1, p2, stickRadius, null, signature);
+						if (cc != null) sticks.addChild(cc);
+						if (drawArrows)		{
+							FactoredMatrix arrowM = new FactoredMatrix(signature);
+							double d;
+							if (p1.length == 3) d = Rn.euclideanDistance(p1, p2);
+							else d = Pn.distanceBetween(p1, p2, signature);
+							double flatten = arrowSlope/(d);
+							double stretch = arrowScale/stickRadius;
+							arrowM.setStretch(stretch, stretch, arrowScale*flatten);
+							arrowM.setTranslation(0,0,arrowPosition-.5);
+							SceneGraphComponent arrow = new SceneGraphComponent();
+							arrow.setAppearance(arrowsAp);
+							arrowM.update();
+							arrowM.assignTo(arrow);
+							arrow.setGeometry(urCone);
+							cc.addChild(arrow);
+						}
 					}
-				}
+				}				
 			}
-			// we should allow the user to specify "real" balls, not via the appearance.
 			SceneGraphComponent balls = new SceneGraphComponent();
-			balls.setGeometry(ils);
-			if (ballsAp == null) ballsAp =  new Appearance();
-			ballsAp.setAttribute(CommonAttributes.FACE_DRAW, false);
-			ballsAp.setAttribute(CommonAttributes.EDGE_DRAW, false);
-			ballsAp.setAttribute(CommonAttributes.VERTEX_DRAW, true);
-			ballsAp.setAttribute(CommonAttributes.POINT_SHADER+"."+CommonAttributes.SPHERES_DRAW, true);
-			ballsAp.setAttribute(CommonAttributes.POINT_SHADER+"."+CommonAttributes.POINT_RADIUS, ballRadius);
-			if (ballColor != null) ballsAp.setAttribute(CommonAttributes.POINT_SHADER+"."+CommonAttributes.DIFFUSE_COLOR, ballColor);
-			balls.setAppearance(ballsAp);
+			if (showBalls)	{
+				// we should allow the user to specify "real" balls, not via the appearance.
+				balls.setGeometry(ils);
+				if (ballsAp == null) ballsAp =  new Appearance();
+				ballsAp.setAttribute(CommonAttributes.FACE_DRAW, false);
+				ballsAp.setAttribute(CommonAttributes.EDGE_DRAW, false);
+				ballsAp.setAttribute(CommonAttributes.VERTEX_DRAW, true);
+				ballsAp.setAttribute(CommonAttributes.POINT_SHADER+"."+CommonAttributes.SPHERES_DRAW, true);
+				ballsAp.setAttribute(CommonAttributes.POINT_SHADER+"."+CommonAttributes.POINT_RADIUS, ballRadius);
+				if (ballColor != null) ballsAp.setAttribute(CommonAttributes.POINT_SHADER+"."+CommonAttributes.DIFFUSE_COLOR, ballColor);
+				balls.setAppearance(ballsAp);				
+			}
 			if (topAp == null) topAp =  new Appearance();
 			topAp.setAttribute(CommonAttributes.POLYGON_SHADER+"."+CommonAttributes.SMOOTH_SHADING, true);
 			theResult = new SceneGraphComponent();
 			theResult.setAppearance(topAp);
-			theResult.addChild(sticks);
-			theResult.addChild(balls);
+			if (showSticks) theResult.addChild(sticks);
+			if (showBalls) theResult.addChild(balls);
 	 }
 	 
 	 public void setStickRadius(double r)	{
@@ -191,5 +196,22 @@ public class BallAndStickFactory {
 	public void setIls(IndexedLineSet ils) {
 		this.ils = ils;
 	}
+
+	public boolean isShowBalls() {
+		return showBalls;
+	}
+
+	public void setShowBalls(boolean showBalls) {
+		this.showBalls = showBalls;
+	}
+
+	public boolean isShowSticks() {
+		return showSticks;
+	}
+
+	public void setShowSticks(boolean showSticks) {
+		this.showSticks = showSticks;
+	}
+
 
 }
