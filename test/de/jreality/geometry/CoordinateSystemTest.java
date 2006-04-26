b3b28b26aba8918339efd17f5e605e2dd06b39c8
Martin Sommer <msommer@math.tu-berlin.de>
2006-04-26 16:07:43 +0000

    CoordinateSystemTest
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@902 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- test/de/jreality/geometry/CoordinateSystemTest.java
+++ test/de/jreality/geometry/CoordinateSystemTest.java
@@ -1,6 +1,9 @@
 package de.jreality.geometry;
 
+import de.jreality.math.FactoredMatrix;
 import de.jreality.scene.SceneGraphComponent;
+import de.jreality.scene.SceneGraphPath;
+import de.jreality.scene.tool.ToolSystemViewer;
 import de.jreality.ui.viewerapp.ViewerApp;
 import de.jreality.util.SceneGraphUtility;
 
@@ -13,24 +16,42 @@ public class CoordinateSystemTest {
   public static void main(String[] args) {
     
     //create a component
-//    SceneGraphComponent component = SphereUtility.tessellatedCubeSphere(2);
-//    component.setName("Sphere");
+//  SceneGraphComponent component = SphereUtility.tessellatedCubeSphere(2); component.setName("Sphere");
     SceneGraphComponent component = parametricSurface();  //see below
+
+    FactoredMatrix trans = new FactoredMatrix();
+    trans.setRotation(Math.PI, 1, 1, 1);
+    //trans.setTranslation(1,0,0);
+    trans.assignTo(component);
     
     //create coordinate system
     CoordinateSystemFactory coords = new CoordinateSystemFactory(component);
     coords.setAxisScale(0.5);
-  
     
-    //add box as a child
-//    component.addChild(coords.getBox());
-    component.addChild(coords.getBox(new double[]{-1,-1,-1}));  //specify a direction
+    //display axes and box
+    coords.displayAxes();
+    coords.displayBox();
+        
+    //display component
+    ToolSystemViewer currViewer = (ToolSystemViewer)ViewerApp.display(component)[1];
     
-    //add axes as a child
-    component.addChild(coords.getAxes());
-      
-//    ViewerApp.display(coords.getBox());
-    ViewerApp.display(component);
+    
+    //--------------------------------------------------
+    //for testing the tool methods:
+    
+    //get paths of camera and object
+    SceneGraphPath cameraPath = currViewer.getCameraPath();
+    SceneGraphPath objectPath = new SceneGraphPath();
+    SceneGraphComponent root = currViewer.getSceneRoot();
+    SceneGraphComponent scene = root.getChildComponent(0);
+    objectPath.push(root);
+    objectPath.push(scene);
+    objectPath.push(component);
+
+    
+    coords.updateBox(new double[]{-1,-1,1});  //specify a direction
+//    coords.updateBox(cameraPath, objectPath);  //specify paths in scene graph
+//    not working yet
   }
   
   
