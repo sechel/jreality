e248d53ebc482bb2b46928722155d9e06ce40424
Martin Sommer <msommer@math.tu-berlin.de>
2006-05-31 15:02:22 +0000

    CoordinateSystemTest
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@982 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- test/de/jreality/geometry/CoordinateSystemTest.java
+++ test/de/jreality/geometry/CoordinateSystemTest.java
@@ -41,9 +41,10 @@ public class CoordinateSystemTest {
     //coords.setLabelColor(Color.MAGENTA);
     //coords.setGridColor(Color.GRAY);
     
-    //display axes/box
+    //display axes/box/grid
     coords.showAxes(false);
     coords.showBox(true);
+    coords.showGrid(true);
     
     
     //display component
