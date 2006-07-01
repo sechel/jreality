7c9410eca790fa54ab42bdeadaa94796c419f440
Steffen Weissmann <steffen.weissmann@gmail.com>
2006-07-01 14:26:34 +0000

    fixed due to refactoring
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@1057 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-proxies/de/jreality/scene/proxy/scene/SceneGraphComponent.java
+++ src-proxies/de/jreality/scene/proxy/scene/SceneGraphComponent.java
@@ -40,8 +40,7 @@
 
 package de.jreality.scene.proxy.scene;
 
-import de.jreality.scene.proxy.SgAdd;
-import de.jreality.scene.proxy.SgRemove;
+import de.jreality.util.SceneGraphUtility;
 
 /**
  * Note: all remote references passed in must be hosted on the same machine as
@@ -50,10 +49,10 @@ import de.jreality.scene.proxy.SgRemove;
 public class SceneGraphComponent extends de.jreality.scene.SceneGraphComponent implements RemoteSceneGraphComponent {
 
   public void add(RemoteSceneGraphNode newChild) {
-    new SgAdd().add(this, (de.jreality.scene.SceneGraphNode) newChild);
+    SceneGraphUtility.addChildNode(this, (de.jreality.scene.SceneGraphNode) newChild);
   }
 
   public void remove(RemoteSceneGraphNode newChild) {
-    new SgRemove().remove(this, (de.jreality.scene.SceneGraphNode) newChild);
+    SceneGraphUtility.removeChildNode(this, (de.jreality.scene.SceneGraphNode) newChild);
   }
 }
