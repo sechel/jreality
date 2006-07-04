62e20611846f3ab4f7f20d763b6c6dd21bc49485
Nils Bleicher <bleicher@math.tu-berlin.de>
2006-07-04 14:31:38 +0000

    *** empty log message ***
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@1065 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-tutorial/de/jreality/tutorial/PointDragExample.java
@@ -0,0 +1,50 @@
+package de.jreality.tutorial;
+
+import de.jreality.geometry.Primitives;
+import de.jreality.math.Rn;
+import de.jreality.scene.Appearance;
+import de.jreality.scene.PointSet;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.scene.data.Attribute;
+import de.jreality.scene.data.StorageModel;
+import de.jreality.scene.tool.PointDragEvent;
+import de.jreality.scene.tool.PointDragEventTool;
+import de.jreality.scene.tool.PointDragListener;
+import de.jreality.shader.CommonAttributes;
+import de.jreality.ui.viewerapp.ViewerApp;
+
+public class PointDragExample {
+
+	public static void main(String[] args) {
+		SceneGraphComponent cmp = new SceneGraphComponent();
+		PointDragEventTool t = new PointDragEventTool();
+		t.addPointDragListener(new PointDragListener() {
+
+			public void pointDragStart(PointDragEvent e) {
+				System.out.println("drag start of vertex no "+e.getIndex());				
+			}
+
+			public void pointDragged(PointDragEvent e) {
+				System.out.println("dragging vertex no "+e.getIndex()+"; new Position: "+Rn.toString(e.getPosition()));
+				PointSet pointSet = e.getPointSet();
+				double[][] points=new double[pointSet.getNumPoints()][];
+		        pointSet.getVertexAttributes(Attribute.COORDINATES).toDoubleArrayArray(points);
+		        points[e.getIndex()]=e.getPosition();  
+		        pointSet.setVertexAttributes(Attribute.COORDINATES,StorageModel.DOUBLE_ARRAY.array(3).createReadOnly(points));			
+			}
+
+			public void pointDragEnd(PointDragEvent e) {
+				System.out.println("drag end of vertex no "+e.getIndex());
+			}
+			
+		});
+		
+		cmp.addTool(t);
+		cmp.setGeometry(Primitives.icosahedron());
+		cmp.setAppearance(new Appearance());
+		cmp.getAppearance().setAttribute(CommonAttributes.VERTEX_DRAW,true);
+		cmp.getAppearance().setAttribute(CommonAttributes.SPHERES_DRAW,true);
+		cmp.getAppearance().setAttribute(CommonAttributes.POINT_RADIUS,0.05);
+		ViewerApp.display(cmp);
+	}
+}
