3fd5c04ba7406c0d2fea59a01c508536b81b42cc
Charles Gunn <gunn@math.tu-berlin.de>
2006-07-14 07:31:17 +0000

    Latest changes.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@1139 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-core/de/jreality/geometry/TubeFactory.java
+++ src-core/de/jreality/geometry/TubeFactory.java
@@ -1,43 +1,7 @@
-/**
- *
- * This file is part of jReality. jReality is open source software, made
- * available under a BSD license:
- *
- * Copyright (c) 2003-2006, jReality Group: Charles Gunn, Tim Hoffmann, Markus
- * Schmies, Steffen Weissmann.
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * - Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * - Neither the name of jReality nor the names of its contributors nor the
- *   names of their associated organizations may be used to endorse or promote
- *   products derived from this software without specific prior written
- *   permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+/*
+ * Created on Jul 2, 2004
  *
  */
-
-
 package de.jreality.geometry;
 
 import java.util.logging.Level;
@@ -329,7 +293,15 @@ public  class TubeFactory {
 			double[] B = new double[4];
 			for (int i = 1; i<n-1; ++i)	{
 				Pn.polarize(B, P3.planeFromPoints(null, polygon[i-1], polygon[i], polygon[i+1]),signature);	
-				if (Rn.euclideanNormSquared(B) > 10E-16) return B;
+				if (Rn.euclideanNormSquared(B) > 10E-16) {
+					Pn.dehomogenize(B,B);
+					boolean flip = false;
+					if (B[0] <0) flip = true;
+					else if (B[0] == 0.0 && B[1] < 0) flip = true;
+					else if (B[1] == 0.0 && B[2] < 0) flip = true;
+					if (flip) for (int j = 0; j<3; ++j) B[j] = -B[j];
+					return B;
+				}
 			}
 			B = new double[] {Math.random(), Math.random(), Math.random(), 1.0};
 			return Pn.polarizePlane(null, P3.planeFromPoints(null, B, polygon[1], polygon[2]),signature);
