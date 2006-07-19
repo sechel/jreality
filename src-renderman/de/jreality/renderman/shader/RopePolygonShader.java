471e4b4375abf724bbd3d128f3a3ba04bcad9eb5
Charles Gunn <gunn@math.tu-berlin.de>
2006-07-19 08:45:57 +0000

    Update rope shader to blend.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@1148 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-renderman/de/jreality/renderman/shader/RopePolygonShader.java
+++ src-renderman/de/jreality/renderman/shader/RopePolygonShader.java
@@ -50,7 +50,9 @@ public class RopePolygonShader extends DefaultPolygonShader {
 	Color band1color = new Color(1f,1f,1f), 
 		band2color = new Color(.8f, .8f, .8f), 
 		shadowcolor = new Color(0f,0f,0f), 
-		gapcolor = new Color(0f,0f,0f,0f);
+		gapcolor = new Color(0f,0f,0f,0f),
+		blendcolor = new Color(0f,0f,0f,0f);
+;
 	double bandwidth, shadowwidth, blendfactor, gapalpha;
 	double[] textureMatrix = Rn.identityMatrix(4);
 	
@@ -65,6 +67,7 @@ public class RopePolygonShader extends DefaultPolygonShader {
         band2color =(Color) eap.getAttribute(name+"."+"band2color", band2color);
         shadowcolor =(Color) eap.getAttribute(name+"."+"shadowcolor", shadowcolor);
         gapcolor =(Color) eap.getAttribute(name+"."+"gapcolor", gapcolor);
+        blendcolor =(Color) eap.getAttribute(name+"."+"blendcolor", blendcolor);
         textureMatrix =(double[]) eap.getAttribute(name+"."+"textureMatrix", textureMatrix);
         map.put("bandwidth",new Float((float) bandwidth));
         map.put("shadowwidth",new Float((float) shadowwidth));
@@ -74,6 +77,7 @@ public class RopePolygonShader extends DefaultPolygonShader {
         map.put("band2color", band2color);
         map.put("shadowcolor", shadowcolor);
         map.put("gapcolor", gapcolor);
+        map.put("blendcolor", blendcolor);
         map.put("textureMatrix", textureMatrix);
         map.remove("string texturename");
 	}
