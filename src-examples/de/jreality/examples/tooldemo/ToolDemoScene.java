f37f38f5aea69c6cae9a6db2e37f10c6c12cd33c
Steffen Weissmann <steffen.weissmann@gmail.com>
2006-08-02 07:52:00 +0000

    ...
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@1174 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-examples/de/jreality/examples/tooldemo/ToolDemoScene.java
+++ src-examples/de/jreality/examples/tooldemo/ToolDemoScene.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import javax.swing.JFrame;
 
 import de.jreality.geometry.GeometryUtility;
+import de.jreality.geometry.IndexedLineSetFactory;
 import de.jreality.geometry.Primitives;
 import de.jreality.math.MatrixBuilder;
+import de.jreality.math.Rn;
 import de.jreality.reader.Readers;
 import de.jreality.scene.Appearance;
 import de.jreality.scene.Camera;
@@ -15,7 +17,7 @@ import de.jreality.scene.DirectionalLight;
 import de.jreality.scene.IndexedFaceSet;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.scene.SceneGraphPath;
-import de.jreality.shader.CubeMap;
+import de.jreality.scene.data.Attribute;
 import de.jreality.shader.ImageData;
 import de.jreality.shader.Texture2D;
 import de.jreality.shader.TextureUtility;
@@ -27,6 +29,7 @@ import de.jreality.tools.ShipNavigationTool;
 import de.jreality.ui.viewerapp.ViewerApp;
 import de.jreality.util.Input;
 import de.jreality.util.PickUtility;
+import de.jreality.util.Rectangle3D;
 
 public class ToolDemoScene {
 
@@ -84,7 +87,7 @@ public class ToolDemoScene {
         
     // add tools
     ShipNavigationTool shipNavigationTool = new ShipNavigationTool();
-    shipNavigationTool.setGain(17);
+    shipNavigationTool.setGain(7);
     //shipNavigationTool.setGravity(100);
     //shipNavigationTool.setJumpSpeed(20);
     avatarNode.addTool(shipNavigationTool);
@@ -94,7 +97,9 @@ public class ToolDemoScene {
     //avatarNode.addTool(new PointerDisplayTool());
     
     sceneNode.addTool(new RotateTool());
-    sceneNode.addTool(new DraggingTool());
+    DraggingTool draggingTool = new DraggingTool();
+    draggingTool.setMoveChildren(true);
+    sceneNode.addTool(draggingTool);
 
   }
   
@@ -105,12 +110,12 @@ public class ToolDemoScene {
     // prepare terrain
     if (terrain) {
       terrainNode = Readers.read(Input.getInput("terrain.3ds")).getChildComponent(0);
-      MatrixBuilder.euclidean().translate(0,9,0).assignTo(terrainNode);
+      MatrixBuilder.euclidean().scale(1/3.).translate(0,9,0).assignTo(terrainNode);
       System.out.println(GeometryUtility.calculateBoundingBox(terrainNode).getMinY());
       } else {
       terrainNode = new SceneGraphComponent();
       MatrixBuilder.euclidean().rotateX(Math.PI/2).assignTo(terrainNode);
-      terrainNode.setGeometry(Primitives.plainQuadMesh(300, 300, 1, 1));
+      terrainNode.setGeometry(Primitives.plainQuadMesh(100, 100, 1, 1));
     }
     terrainNode.setName("terrain");
     IndexedFaceSet terrainGeom = (IndexedFaceSet) terrainNode.getGeometry();
@@ -130,6 +135,60 @@ public class ToolDemoScene {
     TextureUtility.createSkyBox(rootAppearance, imgs);
   }
   
+  public void setContent(SceneGraphComponent content, Rectangle3D bounds, boolean keepRatio) {
+    Rectangle3D contentBounds = GeometryUtility.calculateBoundingBox(content);
+    setContent(content, contentBounds, bounds, keepRatio);
+  }
+  
+  public void setContent(SceneGraphComponent content, Rectangle3D contentBounds, Rectangle3D bounds, boolean keepRatio) {
+    double[] exc = contentBounds.getExtent();
+    double[] exb = bounds.getExtent();
+    double[] scale = new double[3];
+    scale[0]=Math.abs(exb[0]/exc[0]);
+    scale[1]=Math.abs(exb[1]/exc[1]);
+    scale[2]=Math.abs(exb[2]/exc[2]);
+    if (keepRatio) {
+      if (scale[1]<scale[0]) scale[0]=scale[1];
+      else scale[1]=scale[0];
+      if (scale[1]<scale[2]) scale[2]=scale[1];
+      else scale[0]=scale[1]=scale[2];
+    }
+    MatrixBuilder mb = MatrixBuilder.euclidean().scale(scale);
+    mb.assignTo(content);
+    //contentBounds = GeometryUtility.calculateBoundingBox(content);
+    double[] contentCenter = contentBounds.getCenter();
+    contentCenter[0]*=scale[0]; contentCenter[1]*=scale[1]; contentCenter[2]*=scale[2];
+    double[] t = Rn.subtract(null, bounds.getCenter(), contentCenter);
+    t[0]/=scale[0]; t[1]/=scale[1]; t[2]/=scale[2];
+    mb.translate(t);
+    mb.assignTo(content);
+    sceneNode.addChild(content);
+    
+    contentBounds = GeometryUtility.calculateBoundingBox(content);
+    System.out.println("cb:\n"+contentBounds+"\n\nbb:\n"+bounds);
+    
+    rootNode.addChild(bounds(bounds));
+  }
+  
+  SceneGraphComponent bounds(Rectangle3D bounds) {
+    SceneGraphComponent ret = new SceneGraphComponent();
+    IndexedLineSetFactory ilsf = new IndexedLineSetFactory();
+    ilsf.setVertexCount(8);
+    ilsf.setLineCount(12);
+    IndexedFaceSet cube = Primitives.cube();
+    ilsf.setVertexCoordinates(cube.getVertexAttributes(Attribute.COORDINATES));
+    ilsf.setEdgeIndices(cube.getEdgeAttributes(Attribute.INDICES).toIntArrayArray(null));
+    ilsf.update();
+    ret.setGeometry(ilsf.getIndexedLineSet());
+    double[] scale = bounds.getExtent();
+    scale[0]/=2; scale[1]/=2; scale[2]/=2;
+    MatrixBuilder mb = MatrixBuilder.euclidean().scale(scale);
+    double[] t = bounds.getCenter();
+    t[0]/=scale[0]; t[1]/=scale[1]; t[2]/=scale[2];
+    mb.translate(t).assignTo(ret);
+    return ret;
+  }
+  
   public static void main(String[] args) throws IOException {
     System.setProperty("jreality.data", "/net/MathVis/data/testData3D");
     //System.setProperty("de.jreality.scene.Viewer", "de.jreality.soft.DefaultViewer");
@@ -137,9 +196,8 @@ public class ToolDemoScene {
     System.setProperty("de.jreality.ui.viewerapp.synchRender", "true");
     
     ToolDemoScene tds = new ToolDemoScene();
+    tds.terrain=false;
     tds.update();
-//    tds.setTerrainTexture(ImageData.load(Input.getInput("grid.jpeg")), 5);
-//    tds.setTerrainTexture(ImageData.load(Input.getInput("monkey.jpeg")), 1);
     ViewerApp.display(tds.rootNode, tds.camPath, tds.emptyPickPath, tds.avatarPath);
     
     Landscape l = new Landscape();
@@ -149,6 +207,19 @@ public class ToolDemoScene {
     f.getContentPane().add(l.selectionComponent);
     f.pack();
     f.setVisible(true);
+    
+    Rectangle3D bounds = new Rectangle3D(new double[][]{{-1,1,1},{1,14,-1}});
+    SceneGraphComponent schwarz1 = Readers.read(Input.getInput("3ds/schwarz.3ds"));
+    SceneGraphComponent schwarz2 = Readers.read(Input.getInput("3ds/schwarz.3ds"));
+    Appearance app = new Appearance();
+    app.setAttribute("showPoints", false);
+    schwarz1.setAppearance(app);
+    schwarz2.setAppearance(app);
+    tds.setContent(schwarz1, bounds, true);
+    bounds = new Rectangle3D(new double[][]{{-11,1,1},{-9,4,-1}});
+    tds.setContent(schwarz2, bounds, false);
+    
+    //ViewerApp.displayOld(schwarz1);
   }
 
 }
