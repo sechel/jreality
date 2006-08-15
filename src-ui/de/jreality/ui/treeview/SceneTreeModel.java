6fa4f9e026d4be48db1cc983f60f7c560b15c285
Martin Sommer <msommer@math.tu-berlin.de>
2006-08-15 15:48:24 +0000

    fixed memleak
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@1239 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-ui/de/jreality/ui/treeview/SceneTreeModel.java
+++ src-ui/de/jreality/ui/treeview/SceneTreeModel.java
@@ -46,7 +46,6 @@ import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.WeakHashMap;
@@ -66,17 +65,17 @@ import de.jreality.shader.RootAppearance;
 import de.jreality.shader.ShaderUtility;
 
 public class SceneTreeModel extends AbstractTreeModel {
-
+  
   private UpToDateSceneProxyBuilder builder;
   
-  WeakHashMap entities = new WeakHashMap();
-  WeakHashMap parents = new WeakHashMap();
-
+  WeakHashMap<Object, Object[]> entities = new WeakHashMap<Object, Object[]>();
+  WeakHashMap<Object, Object> parents = new WeakHashMap<Object, Object>();
+  
   public SceneTreeModel(SceneGraphComponent root) {
     super(null);
     setSceneRoot(root);
   }
-
+  
   public void dispose() {
     builder.dispose();
     builder = null;
@@ -96,7 +95,7 @@ public class SceneTreeModel extends AbstractTreeModel {
     });
     super.root = builder.createProxyTree();
   }
-
+  
   public Object getChild(Object parent, int index) {
     if (parent instanceof SceneTreeNode) {
       SceneTreeNode sn = (SceneTreeNode) parent;
@@ -105,13 +104,13 @@ public class SceneTreeModel extends AbstractTreeModel {
         int newInd = index-sn.getChildren().size();
         SceneGraphComponent comp = (SceneGraphComponent) sn.getNode();
         Tool t = (Tool) comp.getTools().get(newInd);
-        return TreeTool.getInstance(sn, t);
+        return new TreeTool(sn,t);
       }
     }
     Object[] childEntities = (Object[]) entities.get(parent);
     return childEntities[index];
   }
-
+  
   public int getChildCount(Object parent) {
     if (parent instanceof TreeTool) return 0;
     if (parent instanceof SceneTreeNode) {
@@ -133,42 +132,42 @@ public class SceneTreeModel extends AbstractTreeModel {
         }
         return ents.length;
       }
-	int ret = sn.getChildren().size(); 
-	if (sn.getNode() instanceof SceneGraphComponent) {
-	  ret += ((SceneGraphComponent)sn.getNode()).getTools().size();
-	}
-	return ret;
+      int ret = sn.getChildren().size(); 
+      if (sn.getNode() instanceof SceneGraphComponent) {
+        ret += ((SceneGraphComponent)sn.getNode()).getTools().size();
+      }
+      return ret;
     }
-	// entity
-      Object[] ents = (Object[]) entities.get(parent);
-      if (ents == null) {
-        BeanInfo bi=null;
+    // entity
+    Object[] ents = (Object[]) entities.get(parent);
+    if (ents == null) {
+      BeanInfo bi=null;
+      try {
+        bi = Introspector.getBeanInfo(parent.getClass());
+      } catch (IntrospectionException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+      PropertyDescriptor[] pd=bi.getPropertyDescriptors();
+      List<AttributeEntity> childEntities = new LinkedList<AttributeEntity>();
+      for (int i = 0; i < pd.length; i++) {
+        if (!AttributeEntity.class.isAssignableFrom(pd[i].getPropertyType())) continue;
         try {
-          bi = Introspector.getBeanInfo(parent.getClass());
-        } catch (IntrospectionException e) {
-          // TODO Auto-generated catch block
+          AttributeEntity ae = (AttributeEntity) pd[i].getReadMethod().invoke(parent, (Object[]) null);
+          if (ae != null) {
+            childEntities.add(ae);
+            parents.put(ae, parent);
+          }
+        } catch (Exception e) {
           e.printStackTrace();
         }
-        PropertyDescriptor[] pd=bi.getPropertyDescriptors();
-        List childEntities = new LinkedList();
-        for (int i = 0; i < pd.length; i++) {
-          if (!AttributeEntity.class.isAssignableFrom(pd[i].getPropertyType())) continue;
-          try {
-            AttributeEntity ae = (AttributeEntity) pd[i].getReadMethod().invoke(parent, null);
-            if (ae != null) {
-              childEntities.add(ae);
-              parents.put(ae, parent);
-            }
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
-        }
-        ents = childEntities.toArray();
-        entities.put(parent, ents);
       }
-      return ents.length;
+      ents = childEntities.toArray();
+      entities.put(parent, ents);
+    }
+    return ents.length;
   }
-
+  
   public Object getParent(Object o) {
     if (o instanceof SceneTreeNode )
       return ((SceneTreeNode)o).getParent();
@@ -178,43 +177,41 @@ public class SceneTreeModel extends AbstractTreeModel {
   
   public static class TreeTool {
     
-    static WeakHashMap map = new WeakHashMap();
-    
-    private final SceneTreeNode treeNode;
     private final Tool tool;
-
-    static TreeTool getInstance(SceneTreeNode n, Tool t) {
-      HashMap m = (HashMap) map.get(n);
-      if (m == null) {
-        m = new HashMap();
-        map.put(n, m);
-      }
-      HashMap m2 = (HashMap) m.get(t);
-      if (m2 == null) {
-        m2 = new HashMap();
-        m2.put(t, new TreeTool(n, t));
-      }
-      return (TreeTool) m2.get(t);
-    }
+    private SceneTreeNode node;
     
     private TreeTool(SceneTreeNode n, Tool t) {
-      this.treeNode = n;
-      this.tool = t;
+      node = n;
+      tool = t;
     }
-
+    
     public Tool getTool() {
       return tool;
     }
-
+    
     public SceneTreeNode getTreeNode() {
-      return treeNode;
+      return node;
+    }
+    
+    @Override
+    public int hashCode() {
+      return node.hashCode()^(tool.hashCode()*31);
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+      if (obj instanceof TreeTool) {
+        TreeTool tt = (TreeTool) obj;
+        return (node == tt.node && tool == tt.tool);
+      }
+      return false;
     }
   }
   
   private class SceneTreeNodeWithToolListener extends SceneTreeNode implements ToolListener {
     
     SceneGraphComponent cmp;
-    List tools=new LinkedList();
+    List<Tool> tools=new LinkedList<Tool>();
     
     protected SceneTreeNodeWithToolListener(SceneGraphNode node) {
       super(node);
@@ -240,13 +237,13 @@ public class SceneTreeModel extends AbstractTreeModel {
       if (EventQueue.isDispatchThread()) runner.run();
       else try {
         EventQueue.invokeAndWait(runner);
-//        EventQueue.invokeLater(runner);
+//      EventQueue.invokeLater(runner);
       } catch (Exception e) {
         throw new Error(";-(");
       }
       return ret[0];
     }
-
+    
     protected int removeChild(final SceneTreeNode prevChild) {
       final int[] ret = new int[1];
       Runnable runner = new Runnable(){
@@ -258,25 +255,25 @@ public class SceneTreeModel extends AbstractTreeModel {
       if (EventQueue.isDispatchThread()) runner.run();
       else try {
         EventQueue.invokeAndWait(runner);
-//        EventQueue.invokeLater(runner);
+//      EventQueue.invokeLater(runner);
       } catch (Exception e) {
         throw new Error(";-(");
       }
       return ret[0];
     }
-
+    
     public void toolAdded(final ToolEvent ev) {
       Runnable runner = new Runnable(){
         public void run() {
           int idx = getChildren().size()+tools.size();
           tools.add(ev.getTool());
-          fireNodesAdded(SceneTreeNodeWithToolListener.this, new int[]{idx}, new Object[]{TreeTool.getInstance(SceneTreeNodeWithToolListener.this, ev.getTool())});
+          fireNodesAdded(SceneTreeNodeWithToolListener.this, new int[]{idx}, new Object[]{new TreeTool(SceneTreeNodeWithToolListener.this,ev.getTool())});
         }
       };
       if (EventQueue.isDispatchThread()) runner.run();
       else try {
         EventQueue.invokeAndWait(runner);
-//          EventQueue.invokeLater(runner);
+//      EventQueue.invokeLater(runner);
       } catch (Exception e) {
         throw new Error(";-(");
       }
@@ -288,13 +285,13 @@ public class SceneTreeModel extends AbstractTreeModel {
           int idx = getChildren().size();
           int tind = tools.indexOf(ev.getTool());
           tools.remove(tind);
-          fireNodesRemoved(SceneTreeNodeWithToolListener.this, new int[]{idx+tind}, new Object[]{TreeTool.getInstance(SceneTreeNodeWithToolListener.this, ev.getTool())});
+          fireNodesRemoved(SceneTreeNodeWithToolListener.this, new int[]{idx+tind}, new Object[]{new TreeTool(SceneTreeNodeWithToolListener.this,ev.getTool())});
         }
       };
       if (EventQueue.isDispatchThread()) runner.run();
       else try {
         EventQueue.invokeAndWait(runner);
-//        EventQueue.invokeLater(runner);
+//      EventQueue.invokeLater(runner);
       } catch (Exception e) {
         throw new Error(";-(");
       }
