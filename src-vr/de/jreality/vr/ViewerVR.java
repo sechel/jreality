33122c6f3feb41c1d62054d151d61bb930caed1d
Steffen Weissmann <steffen.weissmann@gmail.com>
2006-11-14 12:53:38 +0000

    ...
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@1754 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-vr/de/jreality/vr/ViewerVR.java
+++ src-vr/de/jreality/vr/ViewerVR.java
@@ -427,7 +427,7 @@ public class ViewerVR {
  
 		// terrain
 		terrainNode = Readers
-				.read(Input.getInput(this.getClass().getResource("terrain.3ds")))
+				.read(Input.getInput(ViewerVR.class.getResource("terrain.3ds")))
 				.getChildComponent(0);
 		terrain = (IndexedFaceSet) terrainNode.getGeometry();
 		MatrixBuilder.euclidean().scale(1 / 3.).translate(0, 7, 0).assignTo(terrainNode);
