f327cbfffd778bc7b2dac4126345bfcfd46f8895
Charles Gunn <gunn@math.tu-berlin.de>
2006-12-18 15:38:37 +0000

    Test new thick surface factory.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2020 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ test/de/jreality/geometry/ThickenedSurfaceFactoryTest.java
@@ -0,0 +1,57 @@
+package de.jreality.geometry;
+
+import java.awt.Color;
+
+import de.jreality.scene.IndexedFaceSet;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.shader.CommonAttributes;
+import de.jreality.ui.viewerapp.ViewerApp;
+import de.jreality.util.SceneGraphUtility;
+
+public class ThickenedSurfaceFactoryTest {
+	SceneGraphComponent sgc1, root;
+	IndexedFaceSet surface, thickSurface;
+	int refineLevel = 1, steps = 4;
+	double holeSize = 1.0, thickness = .1;
+	boolean makeHoles = true;
+	ThickenedSurfaceFactory tsf;
+	
+	public  SceneGraphComponent makeWorld() {
+		sgc1 = SceneGraphUtility.createFullSceneGraphComponent();
+		surface = SphereUtility.tessellatedIcosahedronSphere(refineLevel); //Primitives.cube(); // DiscreteGroupUtility.archimedeanSolid("3.4.3.4");////
+		tsf = new ThickenedSurfaceFactory(surface);
+		tsf.setThickness(thickness);
+		tsf.setMakeHoles(makeHoles);
+		tsf.setHoleFactor(holeSize);
+		tsf.setStepsPerEdge(steps);
+		tsf.setKeepFaceColors(true);
+		tsf.setProfileCurve(new double[][]{{0,0},{.02, .25}, {.333,.5},{.666, .5},{.98, .25},{1,0}});
+		updateGeometry();
+		root = SceneGraphUtility.createFullSceneGraphComponent();
+		root.getAppearance().setAttribute(CommonAttributes.POLYGON_SHADER+"."+CommonAttributes.DIFFUSE_COLOR,	Color.yellow);
+		root.getAppearance().setAttribute(CommonAttributes.LINE_SHADER+"."+CommonAttributes.TUBES_DRAW, false);
+		root.getAppearance().setAttribute(CommonAttributes.VERTEX_DRAW, false);
+		root.addChild(sgc1);
+		return root;
+	}
+
+	private void updateGeometry() {
+		tsf.update();
+		thickSurface = tsf.getThickenedSurface();
+		//thickSurface = GeometryUtilityOverflow.thicken(surface, thickness, true, holeSize, steps, );
+		sgc1.setGeometry(thickSurface);
+	}
+
+	  public static void main(String[] args) {
+		  ThickenedSurfaceFactoryTest tsft = new ThickenedSurfaceFactoryTest();
+		  ViewerApp va = new ViewerApp(tsft.makeWorld());
+		  va.setAttachBeanShell(true);
+		  va.setAttachNavigator(true);
+		  va.setShowMenu(true);
+		  va.update();
+		  va.display();
+		  //ViewerApp.display(tsft.makeWorld());
+		  
+	  }
+
+}
