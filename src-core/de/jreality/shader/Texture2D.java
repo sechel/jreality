8d8833ca8cbe1e9dccdffae1c8e6e0583c0a7cda
Charles Gunn <gunn@math.tu-berlin.de>
2006-12-22 10:04:05 +0000

    add support for multitexturing
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2060 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-core/de/jreality/shader/Texture2D.java
+++ src-core/de/jreality/shader/Texture2D.java
@@ -42,6 +42,8 @@ package de.jreality.shader;
 
 import java.awt.Color;
 
+import javax.media.opengl.GL;
+
 import de.jreality.math.Matrix;
 import de.jreality.scene.data.AttributeEntity;
 
@@ -63,6 +65,8 @@ public interface Texture2D extends AttributeEntity {
   public static final int GL_LUMINANCE_ALPHA = 0x190A;
   public static final int GL_INTENSITY = 0x8049;
   // types of application of texture
+  
+  public static final int GL_TEXTURE = 0x1702;
   public static final int GL_DECAL = 0x2101;
   public static final int GL_MODULATE = 0x2100;
   public static final int GL_REPLACE = 0x1E01;
@@ -130,9 +134,22 @@ public interface Texture2D extends AttributeEntity {
   public static final int MAG_FILTER_DEFAULT = GL_LINEAR;
   public static final int MIN_FILTER_DEFAULT = GL_LINEAR_MIPMAP_LINEAR;
   public static final int COMBINE_MODE_DEFAULT = GL_INTERPOLATE;
-  //public static final int COMBINE_MODE_DEFAULT = GL_MODULATE;
+  public static final int COMBINE_MODE_COLOR_DEFAULT = GL_INTERPOLATE;
+  public static final int COMBINE_MODE_ALPHA_DEFAULT = GL_INTERPOLATE;
 
-  public static final Matrix TEXTURE_MATRIX_DEFAULT=new Matrix();
+  public static final int SOURCE0_COLOR_DEFAULT = GL_TEXTURE;
+  public static final int SOURCE1_COLOR_DEFAULT = GL_PREVIOUS;
+  public static final int SOURCE2_COLOR_DEFAULT = GL_CONSTANT;
+  public static final int SOURCE0_ALPHA_DEFAULT = GL_TEXTURE;
+  public static final int SOURCE1_ALPHA_DEFAULT = GL_PREVIOUS;
+  public static final int SOURCE2_ALPHA_DEFAULT = GL_CONSTANT;
+  public static final int OPERAND0_COLOR_DEFAULT = GL_SRC_COLOR;
+  public static final int OPERAND1_COLOR_DEFAULT = GL_SRC_COLOR;
+  public static final int OPERAND2_COLOR_DEFAULT = GL_SRC_ALPHA;
+  public static final int OPERAND0_ALPHA_DEFAULT = GL_SRC_ALPHA;
+  public static final int OPERAND1_ALPHA_DEFAULT = GL_SRC_ALPHA;
+  public static final int OPERAND2_ALPHA_DEFAULT = GL_SRC_ALPHA;
+ public static final Matrix TEXTURE_MATRIX_DEFAULT=new Matrix();
   public static final Color BLEND_COLOR_DEFAULT=Color.WHITE;
   public static final String EXTERNAL_SOURCE_DEFAULT=null;
   
@@ -203,9 +220,45 @@ public interface Texture2D extends AttributeEntity {
 
   public abstract void setBlendColor(Color blendColor);
 
-  public abstract Integer getCombineMode();
+  /**
+   * @deprecated	Use {@link #getCombineModeColor()}.
+   */public abstract Integer getCombineMode();
 
-  public abstract void setCombineMode(Integer combineMode);
+  /**
+   * @deprecated    Use {@link #setCombineModeColor(Integer)}.
+   * @param combineMode
+   */public abstract void setCombineMode(Integer combineMode);
+  
+  public abstract Integer getSource0Color();
+  public abstract Integer getSource1Color();
+  public abstract Integer getSource2Color();
+  public abstract Integer getSource0Alpha();
+  public abstract Integer getSource1Alpha();
+  public abstract Integer getSource2Alpha();
+  public abstract Integer getOperand0Color();
+  public abstract Integer getOperand1Color();
+  public abstract Integer getOperand2Color();
+  public abstract Integer getOperand0Alpha();
+  public abstract Integer getOperand1Alpha();
+  public abstract Integer getOperand2Alpha();
+  public abstract Integer getCombineModeColor();
+  public abstract Integer getCombineModeAlpha();
+  
+  public abstract void setSource0Color(Integer i);
+  public abstract void setSource1Color(Integer i);
+  public abstract void setSource2Color(Integer i);
+  public abstract void setSource0Alpha(Integer i);
+  public abstract void setSource1Alpha(Integer i);
+  public abstract void setSource2Alpha(Integer i);
+  public abstract void setOperand0Color(Integer i);
+  public abstract void setOperand1Color(Integer i);
+  public abstract void setOperand2Color(Integer i);
+  public abstract void setOperand0Alpha(Integer i);
+  public abstract void setOperand1Alpha(Integer i);
+  public abstract void setOperand2Alpha(Integer i);
+  public abstract void setCombineModeColor(Integer i);
+  public abstract void setCombineModeAlpha(Integer i);
+  
   
   public abstract void setImage(ImageData image);
   
