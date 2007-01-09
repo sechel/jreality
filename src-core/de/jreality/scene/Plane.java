3cec80133023cbb70c8e5a63559b5d8b1898ef10
Ulrich Pinkall <pinkall@math.tu-berlin.de>
2007-01-09 09:10:31 +0000

    new primitive
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2095 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-core/de/jreality/scene/Plane.java
@@ -0,0 +1,70 @@
+/**
+ *
+ * This file is part of jReality. jReality is open source software, made
+ * available under a BSD license:
+ *
+ * Copyright (c) 2003-2006, jReality Group: Charles Gunn, Tim Hoffmann, Markus
+ * Schmies, Steffen Weissmann.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of jReality nor the names of its contributors nor the
+ *   names of their associated organizations may be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ** @author pinkall
+ */
+
+package de.jreality.scene;
+
+/**
+ * Plane centered at (0, 0, 0) and spanned by the vectors (1, 0, 0) centered at (0, 1, 0).
+ * Use scene graph transformations to change orientation and position. The center and
+ * the spanning vectors provide natural texture coordinates.
+ */
+public class Plane extends Geometry {
+	private static int UNNAMED_ID;
+
+	public Plane(String name) {
+		super(name);
+	}
+
+	public Plane() {
+		this("sphere " + (UNNAMED_ID++));
+	}
+
+	public void accept(SceneGraphVisitor v) {
+		v.visit(this);
+	}
+
+	static void superAccept(Plane u, SceneGraphVisitor v) {
+		u.superAccept(v);
+	}
+
+	private void superAccept(SceneGraphVisitor v) {
+		super.accept(v);
+	}
+}
