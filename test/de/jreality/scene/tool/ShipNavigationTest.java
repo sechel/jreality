fb13cb2152478a42f33447f8f9f13192f27b4482
Steffen Weissmann <steffen.weissmann@gmail.com>
2007-03-15 18:38:07 +0000

    simple tests added
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2440 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ test/de/jreality/scene/tool/ShipNavigationTest.java
@@ -0,0 +1,42 @@
+package de.jreality.scene.tool;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+
+import de.jreality.geometry.GeometryUtility;
+import de.jreality.geometry.Primitives;
+import de.jreality.math.MatrixBuilder;
+import de.jreality.reader.Readers;
+import de.jreality.scene.Camera;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.tools.HeadTransformationTool;
+import de.jreality.tools.ShipNavigationTool;
+import de.jreality.ui.viewerapp.ViewerApp;
+import de.jreality.util.Input;
+import de.jreality.util.PickUtility;
+import de.jreality.util.Rectangle3D;
+
+public class ShipNavigationTest {
+
+	public static void main(String[] args) throws Exception {
+		ShipNavigationTool snt = new ShipNavigationTool();
+		SceneGraphComponent earth = Primitives.sphere(2, null); //Readers.read(Input.getInput("/net/MathVis/data/testData3D/jrs/earth.jrs"));
+		PickUtility.assignFaceAABBTrees(earth);
+		ViewerApp va = new ViewerApp(earth);
+		va.update();
+		Rectangle3D bb = GeometryUtility.calculateBoundingBox(earth);
+		snt.setCenter(true);
+		snt.setCenter(bb.getCenter());
+		System.out.println(Arrays.toString(bb.getCenter()));
+		
+		va.getViewer().getAvatarPath().getLastComponent().addTool(snt);
+		va.getViewer().getCameraPath().getLastComponent().addTool(new HeadTransformationTool());
+		MatrixBuilder.euclidean().translate(0, 2, 0).assignTo(va.getViewer().getAvatarPath().getLastComponent());
+		MatrixBuilder.euclidean().translate(0, 1.7, 0).assignTo(va.getViewer().getCameraPath().getLastComponent());
+		Camera c = (Camera) va.getViewer().getCameraPath().getLastElement();
+		c.setNear(0.1);
+		c.setFar(1000);
+		va.display();
+	}
+}
