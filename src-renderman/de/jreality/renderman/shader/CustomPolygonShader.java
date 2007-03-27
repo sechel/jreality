ecb944293e48382b57b78728a76de5499c0be04f
Charles Gunn <gunn@math.tu-berlin.de>
2007-03-27 13:59:28 +0000

    rename
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2486 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-renderman/de/jreality/renderman/shader/CustomPolygonShader.java
+++ src-renderman/de/jreality/renderman/shader/CustomPolygonShader.java
@@ -8,14 +8,14 @@ import de.jreality.scene.Appearance;
 import de.jreality.shader.CommonAttributes;
 import de.jreality.shader.EffectiveAppearance;
 
-public class FreePolygonShader extends AbstractRendermanShader {
+public class CustomPolygonShader extends AbstractRendermanShader {
 	SLShader orig = new SLShader("orig");
 	@Override
 	public void setFromEffectiveAppearance(RIBVisitor ribv,
 			EffectiveAppearance eap, String name) {
        Object foo = eap.getAttribute(name+"."+CommonAttributes.RMAN_SURFACE_SHADER,orig);
        if (foo == Appearance.DEFAULT || !(foo instanceof SLShader) || foo == orig)
-    	   throw new IllegalStateException("FreePolygonShader called without SLShader");
+    	   throw new IllegalStateException("CustomPolygonShader called without SLShader");
        SLShader sls = (SLShader) foo;
        map = sls.getParameters();
        shaderName = sls.getName();
