91ada51b821acaa3c4c9139463afaa0019889fc3
Steffen Weissmann <steffen.weissmann@gmail.com>
2007-03-28 11:57:00 +0000

    removed event queue synch
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2496 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-portal/de/jreality/toolsystem/PortalToolSystemImpl.java
+++ src-portal/de/jreality/toolsystem/PortalToolSystemImpl.java
@@ -1,20 +1,11 @@
 package de.jreality.toolsystem;
 
-import java.awt.Toolkit;
-
 import de.jreality.jogl.Viewer;
-import de.jreality.portal.awt.SynchEventQueue;
 import de.jreality.toolsystem.config.ToolSystemConfiguration;
 
 public class PortalToolSystemImpl extends ToolSystem implements PortalToolSystem {
 
-	private static final SynchEventQueue SYNCH_EVENT_QUEUE = new SynchEventQueue();
-	private static final boolean DO_SYNCH=false;
 	Viewer joglViewer;
-	
-	static {
-		if (DO_SYNCH) Toolkit.getDefaultToolkit().getSystemEventQueue().push(SYNCH_EVENT_QUEUE);
-	}
 	
 	  public PortalToolSystemImpl(Viewer joglViewer, ToolSystemConfiguration config) {
 		  super(joglViewer, config, null);
@@ -25,7 +16,6 @@ public class PortalToolSystemImpl extends ToolSystem implements PortalToolSystem
 	 * @see de.jreality.toolsystem.PortalToolSystem#render()
 	 */
 	public void render() {
-		if (DO_SYNCH) SYNCH_EVENT_QUEUE.unlock();
 		joglViewer.render();
 	  }
 	  
