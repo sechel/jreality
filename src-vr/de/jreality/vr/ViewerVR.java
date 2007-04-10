509264fb46749c129ecd7572fba6a28a1c086d5f
Steffen Weissmann <steffen.weissmann@gmail.com>
2007-04-10 15:36:47 +0000

    added hack for change in viewerapp (menu is null until display is called...)
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2563 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-vr/de/jreality/vr/ViewerVR.java
+++ src-vr/de/jreality/vr/ViewerVR.java
@@ -642,8 +642,15 @@ public class ViewerVR {
 	 */
 	public ViewerApp initialize() {
 		restorePreferences(getPreferences());
-		ViewerApp viewerApp = new ViewerApp(sceneRoot, cameraPath, emptyPickPath, avatarPath);
-		tweakMenu(viewerApp);
+		// HACK:
+		ViewerApp viewerApp = new ViewerApp(sceneRoot, cameraPath, emptyPickPath, avatarPath) {
+			@Override
+			public JFrame display() {
+				JFrame f = super.display();
+				tweakMenu(this);
+				return f;
+			}
+		};
 		return viewerApp;
 	}
 
