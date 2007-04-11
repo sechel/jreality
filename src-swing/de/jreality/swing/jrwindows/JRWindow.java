3dde9e0bed38e3ac44876b8fb4e615f074afd5be
Nils Bleicher <bleicher@math.tu-berlin.de>
2007-04-11 16:11:31 +0000

    tubes update fix (see TODO)
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2566 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-swing/de/jreality/swing/jrwindows/JRWindow.java
+++ src-swing/de/jreality/swing/jrwindows/JRWindow.java
@@ -289,6 +289,11 @@ public class JRWindow {
 		borderSgc.getAppearance().setAttribute(CommonAttributes.POINT_RADIUS,cornerRadius);
 		borderSgc.getAppearance().setAttribute(CommonAttributes.TUBE_RADIUS,borderRadius);
 		borderSgc.getAppearance().setAttribute(CommonAttributes.LIGHTING_ENABLED, true);
+		
+		/*TODO:
+		 * when bug in the tubes-shader-update is fixed, erease this:
+		 */
+		borderSgc.getAppearance().setAttribute(CommonAttributes.ANY_DISPLAY_LISTS,false);
 
 		if(enableVertexPopUpTool){
 			vertexPopUpTool=new VertexPopUpTool();
@@ -350,22 +355,7 @@ public class JRWindow {
 		decoControlFace.setVertexAttributes(Attribute.COORDINATES,StorageModel.DOUBLE_ARRAY.array(3).createReadOnly(decoControlCorners));  
 		calculateDecoDragCorners(decoDragCorners,cornerPos);
 		decoDragFace.setVertexAttributes(Attribute.COORDINATES,StorageModel.DOUBLE_ARRAY.array(3).createReadOnly(decoDragCorners));    
-		
-		
-		/**TODO:
-		 * Why are the tubes not updated correctly by this?:
-		 */
-		//borders.setVertexAttributes(Attribute.COORDINATES,StorageModel.DOUBLE_ARRAY.array(3).createReadOnly(cornerPos));
-		
-	    IndexedLineSetFactory borders=new IndexedLineSetFactory();
-		borders.setVertexCount(4);
-		borders.setVertexCoordinates(cornerPos);
-		borders.setLineCount(4);
-		borders.setEdgeIndices(new int[][] {{0,1},{1,2},{2,3},{3,0}});
-		borders.update();
-		this.borders=borders.getIndexedLineSet();
-		borderSgc.setGeometry(this.borders);
-		
+		borders.setVertexAttributes(Attribute.COORDINATES,StorageModel.DOUBLE_ARRAY.array(3).createReadOnly(cornerPos));
 		updateFrameTitle();
 	}    
 
