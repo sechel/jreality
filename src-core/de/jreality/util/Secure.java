a6bfcce5df6e1a2699bcc8722ea8c06547416370
Tim Hoffmann <tim.hoffmann@ma.tum.de>
2007-05-08 12:12:19 +0000

    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2595 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-core/de/jreality/util/Secure.java
+++ src-core/de/jreality/util/Secure.java
@@ -6,19 +6,31 @@ import java.security.PrivilegedAction;
 public class Secure {
 	
     public static String getProperty(final String name, final String def) {
-    	return AccessController.doPrivileged(new PrivilegedAction<String>() {
-    		public String run() {
-    			return System.getProperty(name, def);
-    		}
-    	});
+    	String result = null;
+        try{
+            result = AccessController.doPrivileged(new PrivilegedAction<String>() {           
+                public String run() {
+                    return System.getProperty(name, def);
+                }
+            });
+        } catch (Throwable t) {
+            result = def;
+        }
+        return result;
     }
 
     public static String getProperty(final String name) {
-    	return AccessController.doPrivileged(new PrivilegedAction<String>() {
+        String result = null;
+        try {
+    	result =  AccessController.doPrivileged(new PrivilegedAction<String>() {
     		public String run() {
     			return System.getProperty(name);
     		}
     	});
+        } catch (Throwable t) {
+            
+        }
+        return result;
     }
 
 	public static String setProperty(final String name, final String value) {
