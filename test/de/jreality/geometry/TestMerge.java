aff0421a685c09dbafcf225dc1e59e99dc54fd85
Bernd Gonska <gonska@math.fu-berlin.de>
2007-05-10 07:55:43 +0000

    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2608 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- test/de/jreality/geometry/TestMerge.java
+++ test/de/jreality/geometry/TestMerge.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import de.jreality.math.MatrixBuilder;
 import de.jreality.scene.Appearance;
 import de.jreality.scene.IndexedFaceSet;
+import de.jreality.scene.IndexedLineSet;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.shader.CommonAttributes;
 import de.jreality.ui.viewerapp.ViewerApp;
@@ -21,13 +22,12 @@ public class TestMerge {
 		IndexedFaceSet box= Primitives.box(10, .5, .5, true);
 		IndexedFaceSet box2= Primitives.box(10, .6, 0.4, true);
 		IndexedFaceSet zyl= Primitives.cylinder(20,1,0,.5,5);
-
+		IndexedLineSet arr=Primitives.arrow(0, 0, 1, 1, 1, false);
 		SceneGraphComponent root= new SceneGraphComponent();
 		Appearance app=new Appearance();
 		app.setAttribute(CommonAttributes.POLYGON_SHADER+"."+CommonAttributes.DIFFUSE_COLOR, new Color(255,255,0));
 		app.setAttribute(CommonAttributes.VERTEX_SHADER+"."+CommonAttributes.DIFFUSE_COLOR, new Color(0,255,255));
 		root.setAppearance(app);
-
 		SceneGraphComponent A= new SceneGraphComponent();
 		SceneGraphComponent B= new SceneGraphComponent();
 
@@ -47,15 +47,15 @@ public class TestMerge {
 		B.addChild(B2);
 		root.addChild(C);
 
-		A1.setGeometry(box);
-		A11.setGeometry(box2);
-		B1.setGeometry(zyl);
-		B2.setGeometry(ico);
-		//C.setGeometry(ico2);
+		//A1.setGeometry(box);
+		//A11.setGeometry(box2);
+		//B1.setGeometry(zyl);
+		//B2.setGeometry(ico);
+		C.setGeometry(arr);
 
-		IndexedFaceSet[] list= new IndexedFaceSet[]{ico};
-		//PointSet[] list= new PointSet[]{box,box2,zyl,ico2};
-		//IndexedLineSet[] list= new IndexedLineSet[]{box,box2,zyl,ico2};
+		IndexedLineSet[] list= new IndexedLineSet[]{arr};
+		//PointSet[] list= new PointSet[]{zyl,box2,box,ico2};
+		//IndexedLineSet[] list= new IndexedLineSet[]{ico,box};
 		//IndexedFaceSet i=mergeIndexedFaceSets(list,new Attribute[]{Attribute.COLORS},new double[][][]{{{0,1,1}}},null,null,null,null );
 
 		GeometryMergeFactory t= new GeometryMergeFactory();
@@ -64,11 +64,40 @@ public class TestMerge {
 		//t.generateVertexNormals=false;
 		//t.respectEdges=false;
 		
-		IndexedFaceSet i=t.mergeGeometrySets(root);
-		//PointSet i=t.mergeIndexedLineSets(list);
+		//IndexedFaceSet i=t.mergeGeometrySets(root);
+		IndexedFaceSet i=t.mergeIndexedFaceSets(list);
 		//PointSet i=t.mergeIndexedFaceSets(list);
 		//System.out.println("Report:"+i);
-		vApp.display(i);
+		vApp.display(arr);
+		
+//		// little Scene (two colored boxes and a bangle)
+//		IndexedFaceSet box= Primitives.box(10, .5, .5, true);
+//		IndexedFaceSet box2= Primitives.box(10, .6, 0.4, true);
+//		
+//		IndexedFaceSet zyl= Primitives.cylinder(20,1,0,.5,5);
+//		SceneGraphComponent root= new SceneGraphComponent();
+//		SceneGraphComponent A= new SceneGraphComponent();
+//		SceneGraphComponent B= new SceneGraphComponent();
+//		
+//		root.addChild(A);
+//		root.addChild(B);
+//		//root.setGeometry(box);
+//		A.setGeometry(zyl);
+//		B.setGeometry(box);
+//		// the factory :
+//		
+//		GeometryMergeFactory mergeFact= new GeometryMergeFactory();		
+//		//mergeFact.setRespectEdges(false);
+//		// play with the following 3 optional settings (by default they are true)
+//		//mergeFact.setRespectFaces(true);
+//		//mergeFact.setRespectEdges(true);
+//		//mergeFact.setGenerateVertexNormals(true);				
+//		
+//		//IndexedFaceSet[] list= new IndexedFaceSet[]{box,ico};
+//		IndexedFaceSet result=mergeFact.mergeGeometrySets(root);
+//		//IndexedFaceSet result=mergeFact.mergeIndexedFaceSets(list);
+//		System.out.println(result);
+//		vApp.display(result);
 	}
 
 }
