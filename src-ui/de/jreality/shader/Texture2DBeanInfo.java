80c866d7178d64bcba33599a5f3b419bdba988ed
Martin Sommer <msommer@math.tu-berlin.de>
2007-06-25 12:07:52 +0000

    added scale property setting the textureMatrix
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2772 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-ui/de/jreality/shader/Texture2DBeanInfo.java
+++ src-ui/de/jreality/shader/Texture2DBeanInfo.java
@@ -4,7 +4,10 @@ import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.beans.SimpleBeanInfo;
 
+import de.jreality.math.Matrix;
+import de.jreality.math.MatrixBuilder;
 import de.jtem.beans.ChoiceEditor;
+import de.jtem.beans.DoubleEditor;
 
 public class Texture2DBeanInfo extends SimpleBeanInfo {
 	
@@ -33,6 +36,33 @@ public class Texture2DBeanInfo extends SimpleBeanInfo {
 		}
 	}
 	
+	public static class TextureMatrixEditor extends DoubleEditor {
+		private Matrix unit = MatrixBuilder.euclidean().getMatrix();
+		private double d = 1.0;
+		public TextureMatrixEditor() {
+			super();
+			setValue(unit);
+		}
+		@Override
+		public String getAsText() {
+//			if (getValue() == null) return "";
+//			else 
+			return String.valueOf(d);
+		}
+		@Override
+		public void setAsText(String text) {
+			if (text == null) setValue(unit);  //null
+			else {
+				try {
+					d = Double.parseDouble(text);
+					setValue(MatrixBuilder.euclidean().scale(d).getMatrix());
+				} catch (NumberFormatException nfe) {
+					setValue(unit);  //null
+				}
+			}
+		}
+	}
+	
 //	 GL_MIRRORED_REPEAT =  0x8370;
 //	  public static final int GL_REPEAT = 0x2901;
 //	  public static final int GL_CLAMP = 0x2900;
@@ -58,8 +88,14 @@ public class Texture2DBeanInfo extends SimpleBeanInfo {
 				new PropertyDescriptor("blendColor", beanClass);
 			PropertyDescriptor combineMode =
 				new PropertyDescriptor("combineMode", beanClass);
+			
 			PropertyDescriptor textureMatrix =
 				new PropertyDescriptor("textureMatrix", beanClass);
+			textureMatrix.setPropertyEditorClass(
+					TextureMatrixEditor.class
+			);
+			textureMatrix.setDisplayName("scale");
+			
 			PropertyDescriptor repeatS =
 				new PropertyDescriptor("repeatS", beanClass);
 			repeatS.setPropertyEditorClass(
