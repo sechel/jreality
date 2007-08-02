6fb2d95c36c609c120c6f866e394557ba5a246b8
Charles Gunn <gunn@math.tu-berlin.de>
2007-08-02 10:58:29 +0000

    add "fast and dirty" attribute
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2874 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-core/de/jreality/shader/CommonAttributes.java
+++ src-core/de/jreality/shader/CommonAttributes.java
@@ -86,6 +86,8 @@ import de.jreality.scene.Appearance;
 	public final static String Z_BUFFER_ENABLED = 		"zBufferEnabled";
 	public final static String LEVEL_OF_DETAIL = 			"levelOfDetail";
 	public final static double LEVEL_OF_DETAIL_DEFAULT = 			1.0;
+	public final static String FAST_AND_DIRTY = "fastAndDirty";
+	public final static boolean FAST_AND_DIRTY_DEFAULT = false;
 	public final static String OPAQUE_TUBES_AND_SPHERES = "opaqueTubesAndSpheres";
 	public final static boolean OPAQUE_TUBES_AND_SPHERES_DEFAULT = false;
 	public final static String CENTER_ON_BOUNDING_BOX = "centerOnBoundingBox";
