c89060e32ad37ef1532a1874ffc6abfbbbda9142
Charles Gunn <gunn@math.tu-berlin.de>
2007-08-20 10:50:59 +0000

    throw exception if current pick is null
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@2927 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tool/de/jreality/tools/DragEventTool.java
+++ src-tool/de/jreality/tools/DragEventTool.java
@@ -62,7 +62,9 @@ public class DragEventTool extends AbstractTool {
     root2cam.setColumn(3,new double[]{0,0,0,1});  
     distDir[0]=Rn.normalize(null,root2cam.multiplyVector(dir2ScaleZDrag[0]));
     distDir[1]=Rn.normalize(null,root2cam.multiplyVector(dir2ScaleZDrag[1]));
-          
+    if (tc.getCurrentPick() == null )
+    	throw new IllegalStateException("null pick");
+    
     if (tc.getCurrentPick().getPickType() == PickResult.PICK_TYPE_POINT) {
       if (pointDragListener == null) {
         active=false;
