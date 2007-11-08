11af2fb2bb30dcaad51198f0e8ba93d83d70edb6
Martin Sommer <msommer@math.tu-berlin.de>
2007-11-08 16:44:40 +0000

    started support for orthographic projection
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3011 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-sunflow/de/jreality/sunflow/core/camera/OrthogonalLens.java
+++ src-sunflow/de/jreality/sunflow/core/camera/OrthogonalLens.java
@@ -8,18 +8,29 @@ import org.sunflow.core.Ray;
 public class OrthogonalLens implements CameraLens {
 
 	private float aspect, fov;
-    
+
+	public OrthogonalLens() {
+		fov = 90;
+		aspect = 1;
+		update();
+	}
+
 	public boolean update(ParameterList pl, SunflowAPI api) {
 		// get parameters
-        fov = pl.getFloat("fov", fov);
-        aspect = pl.getFloat("aspect", aspect); //?
-        return true;
-    }
+		fov = pl.getFloat("fov", fov);
+		aspect = pl.getFloat("aspect", aspect);
+		update();
+		return true;
+	}
 
-    public Ray getRay(float x, float y, int imageWidth, int imageHeight, double lensX, double lensY, double time) {
-    	System.out.println("x="+x+" y="+y);
-    	x-=(imageWidth/2);
-    	y-=(imageHeight/2);
-        return new Ray(x/700, y/700, 0, 0, 0, -1);
-    }
+	private void update() {
+		fov = (float) Math.tan(Math.toRadians(fov * 0.5f));
+	}
+
+	public Ray getRay(float x, float y, int imageWidth, int imageHeight, double lensX, double lensY, double time) {
+//		System.out.println("x="+x+" y="+y);
+		x= (x-imageWidth/2) / (imageWidth*fov);
+		y= (y-imageHeight/2) / (imageHeight*fov*aspect);
+		return new Ray(x, y, 0, 0, 0, -1);
+	}
 }
