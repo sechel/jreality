1ee2dadbf8ee55f03511aa15fb8927bce1a8d607
Charles Gunn <gunn@math.tu-berlin.de>
2008-01-07 08:15:54 +0000

    support for tiff writing w/alpha channel
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3075 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-core/de/jreality/util/ImageUtility.java
+++ src-core/de/jreality/util/ImageUtility.java
@@ -2,7 +2,10 @@ package de.jreality.util;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
+import java.awt.image.renderable.ParameterBlock;
+import java.beans.Statement;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.logging.Level;
@@ -13,13 +16,29 @@ import javax.imageio.ImageIO;
 public class ImageUtility {
 
 	private ImageUtility() {}
-	
+
 	public static void writeBufferedImage(File file, BufferedImage img) {
 		String suffix = getFileSuffix(file);
+		System.err.println("Suffix is "+suffix);
 		if ("tiff".equals(suffix) || "tif".equals(suffix)) {
 			try {
-				Method cm = Class.forName("javax.media.jai.JAI").getMethod("create", new Class[]{String.class, RenderedImage.class, Object.class, Object.class});
-				cm.invoke(null, new Object[]{"filestore", img, file.getPath(), "tiff"});
+				  Class encParamClass = Class.forName("com.sun.media.jai.codec.TIFFEncodeParam");
+				  
+			      Object encodeParam = encParamClass.newInstance();
+			      Object compField = encParamClass.getField("COMPRESSION_DEFLATE").get(null);
+			      
+			      new Statement(encodeParam, "setCompression", new Object[]{compField}).execute();
+			      new Statement(encodeParam, "setDeflateLevel", new Object[]{9}).execute();
+			      
+			      ParameterBlock pb = new ParameterBlock();
+			      pb.addSource(img);
+			      pb.add(new FileOutputStream(file));
+			      pb.add("tiff");
+			      pb.add(encodeParam);
+			      
+				  new Statement(Class.forName("javax.media.jai.JAI"), "create", new Object[]{"encode", pb}).execute();
+//			Method cm = Class.forName("javax.media.jai.JAI").getMethod("create", new Class[]{String.class, RenderedImage.class, Object.class, Object.class});
+//				cm.invoke(null, new Object[]{"filestore", img, file.getPath(), "tiff"});
 			} catch(Throwable e) {
 				throw new RuntimeException("need JAI for tiff writing", e);
 			}
