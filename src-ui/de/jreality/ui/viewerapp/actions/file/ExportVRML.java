3952181df3946126569ff224c95b39ac0e8be736
Martin Sommer <msommer@math.tu-berlin.de>
2008-03-25 14:22:55 +0000

    clean up
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3199 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-ui/de/jreality/ui/viewerapp/actions/file/ExportVRML.java
+++ src-ui/de/jreality/ui/viewerapp/actions/file/ExportVRML.java
@@ -1,25 +1,19 @@
 package de.jreality.ui.viewerapp.actions.file;
 
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 import javax.swing.AbstractAction;
-import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JCheckBox;
 import javax.swing.JComponent;
 
 import de.jreality.scene.Viewer;
-import de.jreality.softviewer.PSRenderer;
 import de.jreality.ui.viewerapp.FileLoaderDialog;
 import de.jreality.ui.viewerapp.actions.AbstractJrAction;
 import de.jreality.writer.WriterVRML;
-import de.jtem.beans.DimensionPanel;
 
 public class ExportVRML extends AbstractJrAction {
 
@@ -38,9 +32,6 @@ public class ExportVRML extends AbstractJrAction {
 		setShortDescription("Export the current scene as VRML file");
 	}
 
-//	public ExportPS(String name, ViewerApp v) {
-//	this(name, v.getViewerSwitch(), v.getFrame());
-//	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -50,22 +41,19 @@ public class ExportVRML extends AbstractJrAction {
 		if (file == null) return;  //dialog cancelled
 
 		if (options == null) options = createAccessory();
-		// try {
-		Dimension d = viewer.getViewingComponentSize();
-		PSRenderer rv;
+
 		try {
 //			WriterVRML.write(viewer.getSceneRoot(), new FileOutputStream(file));
 			WriterVRML writer = new WriterVRML(new FileOutputStream(file));
 			writer.setWritePath(file.getParent()+"/");
 			writer.setWriteTextureFiles(writeTextureFiles);
 			writer.write(viewer.getSceneRoot());
-		} catch (FileNotFoundException exc) {
+		} catch (Exception exc) {
 			exc.printStackTrace();
-		} catch (IOException ev) {
-			// TODO Auto-generated catch block
-			ev.printStackTrace();
 		}			
 	}
+	
+	
 	JCheckBox checkbox;
 	private JComponent createAccessory() {
 		
