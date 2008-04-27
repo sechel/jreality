c4a6a968e82537f816d28e31a8618f8b7130e044
Martin Sommer <msommer@math.tu-berlin.de>
2008-04-27 21:27:41 +0000

    cleanup
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3234 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-ui/de/jreality/ui/viewerapp/actions/file/ExportSTL.java
+++ src-ui/de/jreality/ui/viewerapp/actions/file/ExportSTL.java
@@ -1,27 +1,16 @@
 package de.jreality.ui.viewerapp.actions.file;
 
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 
-import javax.swing.AbstractAction;
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.JCheckBox;
 import javax.swing.JComponent;
 
 import de.jreality.scene.Viewer;
-import de.jreality.softviewer.PSRenderer;
 import de.jreality.ui.viewerapp.FileLoaderDialog;
 import de.jreality.ui.viewerapp.actions.AbstractJrAction;
 import de.jreality.writer.WriterSTL;
-import de.jreality.writer.WriterVRML;
-import de.jtem.beans.DimensionPanel;
 
 public class ExportSTL extends AbstractJrAction {
 
@@ -39,10 +28,6 @@ public class ExportSTL extends AbstractJrAction {
 		setShortDescription("Export the current scene as STL file");
 	}
 
-//	public ExportPS(String name, ViewerApp v) {
-//	this(name, v.getViewerSwitch(), v.getFrame());
-//	}
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
@@ -52,11 +37,8 @@ public class ExportSTL extends AbstractJrAction {
 		try {
 //			WriterVRML.write(viewer.getSceneRoot(), new FileOutputStream(file));
 			WriterSTL.write(viewer.getSceneRoot(), new FileOutputStream(file));
-		} catch (FileNotFoundException exc) {
+		} catch (Exception exc) {
 			exc.printStackTrace();
-		} catch (IOException ev) {
-			// TODO Auto-generated catch block
-			ev.printStackTrace();
 		}			
 	}
 }
