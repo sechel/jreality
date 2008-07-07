57c255f60e3ddec1152d33ad95abdd40e6b3af92
Charles Gunn <gunn@math.tu-berlin.de>
2008-07-07 08:27:32 +0000

    get rid of this!
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3350 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-core/de/jreality/scene/newpick/HitFilter.java
+++
@@ -1,8 +0,0 @@
-package de.jreality.scene.newpick;
-
-import de.jreality.scene.pick.PickResult;
-
-public interface HitFilter {
-
-	public boolean accept(double[] from, double[] to, PickResult h);
-}
