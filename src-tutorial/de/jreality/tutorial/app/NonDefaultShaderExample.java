2d58e01a4d1743b87a2af1ded00e9f66708389f5
Charles Gunn <gunn@math.tu-berlin.de>
2008-07-22 14:55:36 +0000

    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3379 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/app/NonDefaultShaderExample.java
+++ src-tutorial/de/jreality/tutorial/app/NonDefaultShaderExample.java
@@ -8,7 +8,9 @@ import de.jreality.scene.SceneGraphComponent;
 import de.jreality.shader.CommonAttributes;
 import de.jreality.shader.DefaultGeometryShader;
 import de.jreality.shader.DefaultLineShader;
+import de.jreality.shader.DefaultPolygonShader;
 import de.jreality.shader.ShaderUtility;
+import de.jreality.shader.TwoSidePolygonShader;
 import de.jreality.ui.viewerapp.ViewerApp;
 import de.jreality.util.SceneGraphUtility;
 
@@ -22,10 +24,15 @@ public class NonDefaultShaderExample {
    			ShaderUtility.createDefaultGeometryShader(ap, true);
 		dgs.setShowLines(true);
 		dgs.setShowPoints(false);
-		// setting the nondefault shaders via shader interfaces is currently broken (gunn, 21.07.08)
+		// setting the nondefault shaders via shader interfaces is currently broken in jogl backend(gunn, 21.07.08)
+//		TwoSidePolygonShader tsps = (TwoSidePolygonShader) dgs.createPolygonShader("twoSide");
+//		DefaultPolygonShader dps = (DefaultPolygonShader) tsps.createFront("default");
+//		DefaultPolygonShader dps2 = (DefaultPolygonShader) tsps.createBack("default");
 		ap.setAttribute(CommonAttributes.POLYGON_SHADER+"name", "twoSide");
 		ap.setAttribute(CommonAttributes.POLYGON_SHADER+".front."+CommonAttributes.DIFFUSE_COLOR, new Color(0,204,204));
 		ap.setAttribute(CommonAttributes.POLYGON_SHADER+".back."+CommonAttributes.DIFFUSE_COLOR, new Color(204,204,0));
+//		dps.setDiffuseColor(new Color(0,204,204));
+//		dps2.setDiffuseColor(new Color(204,204,0));
 		DefaultLineShader dls = (DefaultLineShader) dgs.getLineShader();
 		dls.setTubeDraw(true);
 		dls.setTubeRadius(.005);
