01eb878c60139daf37a28a80c5e9c66d66f1399c
Stefan Sechelmann <sechel@math.tu-berlin.de>
2008-09-10 17:03:49 +0000

    NEW - bug 16: JReality PDF Writer http://arnold.math.tu-berlin.de:8080/tasks/show_bug.cgi?id=16
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3431 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-io/de/jreality/writer/pdf/viewerVRPrefs.js
+++ src-io/de/jreality/writer/pdf/viewerVRPrefs.js
@@ -1,36 +1,4 @@
-//------------------------
-// include a ViewerVR scene, that feels like the original ViewerVR.
-// That is to move with curser keys or aswd, jump with space
-// and turn around with right click on the mouse
-//------------------------
-/*
-var JUMP_HEIGHT = 20;
-var speed = 0.2;
-var goVector = new Vector3(1, 0, 0);
-var jumpVector = new Vector3(0,JUMP_HEIGHT,0);
-var jumpAnim = scene.animations.getByIndex(0);
-*/
-/*
-keyEventHandler = new KeyEventHandler();
-keyEventHandler.onEvent = function(event){
-	canvas = event.canvas;
-	camera = scene.cameras.getByName("##cam##");
-	var camDir = camera.targetPosition.subtract(camera.position);
-	camDir.scale(speed); 
-	
-	if(event.characterCode == 32){
-		scene.activateAnimation(jumpAnim);
-	}
-	if(event.characterCode == 31 || event.characterCode == 119){
-		//vrCamera.positionLocal.set(vrCamera.positionLocal.add(goVector));
-	}
-	if(event.characterCode == 30 || event.characterCode == 115){
-		//vrCamera.positionLocal.set(vrCamera.position.subtract(goVector));
-	}
-}
-runtime.addEventHandler(keyEventHandler);
-*/
-
+var canvas = null;
 var myEventHdlr = new RenderEventHandler();
 myEventHdlr.onEvent = function(event)
 {
