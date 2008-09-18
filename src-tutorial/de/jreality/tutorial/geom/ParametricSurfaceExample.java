e3d053f7a4133485129e74bdfa73d7f829e33454
Charles Gunn <gunn@math.tu-berlin.de>
2008-09-18 14:46:20 +0000

    Rename to be more explanatory
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3455 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/geom/ParametricSurfaceExample.java
+++ src-tutorial/de/jreality/tutorial/geom/ParametricSurfaceExample.java
@@ -1,19 +1,23 @@
 package de.jreality.tutorial.geom;
 
+import java.awt.Color;
+
 import de.jreality.geometry.ParametricSurfaceFactory;
 import de.jreality.geometry.ParametricSurfaceFactory.Immersion;
+import de.jreality.scene.Appearance;
 import de.jreality.scene.SceneGraphComponent;
+import de.jreality.shader.CommonAttributes;
 import de.jreality.ui.viewerapp.ViewerApp;
 import de.jreality.vr.ViewerVR;
 
-public class Swallowtail {
+public class ParametricSurfaceExample {
 	// to use the ParametricSurfaceFactory one needs an instance of immersion
 	// That is, a function that maps  (u,v) values into a 3- or 4-space
 	static Immersion myImmersion = new Immersion() {
 		public void evaluate(double u, double v, double[] xyz, int index) {
-			xyz[index]=(u-v*v);
-			xyz[index+1]=u*v;
-			xyz[index+2]=(u*u-4*u*v*v);
+			xyz[index]= 10*(u-v*v);
+			xyz[index+1]= 10*u*v;
+			xyz[index+2]= 10*(u*u-4*u*v*v);
 		}
 		// how many dimensions in the image space?
 		public int getDimensionOfAmbientSpace() { return 3;	}
@@ -36,12 +40,19 @@ public class Swallowtail {
 		
 		SceneGraphComponent sgc = new SceneGraphComponent("Swallowtail");
 		sgc.setGeometry(psf.getIndexedFaceSet());
-
-		ViewerVR vvr=ViewerVR.createDefaultViewerVR(null);
-		vvr.setContent(sgc);
-		
-		ViewerApp vapp=vvr.initialize();
-		vapp.update();
-		vapp.display();			
+		Appearance ap = new Appearance();
+		ap.setAttribute(CommonAttributes.POLYGON_SHADER+"."+CommonAttributes.DIFFUSE_COLOR, Color.yellow);
+		sgc.setAppearance(ap);
+		boolean useViewerVR = false;
+		if (useViewerVR)	{
+			ViewerVR vvr=ViewerVR.createDefaultViewerVR(null);
+			vvr.setContent(sgc);
+			
+			ViewerApp vapp=vvr.initialize();
+			vapp.update();
+			vapp.display();			
+		} else {
+			ViewerApp.display(sgc);		
+		}
 	}
 }
