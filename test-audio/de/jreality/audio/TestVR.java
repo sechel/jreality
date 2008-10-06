8cb29e702c0528e63597d1410c1e37b10f248a45
Steffen Weissmann <steffen.weissmann@gmail.com>
2008-10-06 17:42:44 +0000

    ...
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3476 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- test-audio/de/jreality/audio/TestVR.java
+++ test-audio/de/jreality/audio/TestVR.java
@@ -2,8 +2,10 @@ package de.jreality.audio;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 import de.jreality.audio.SignalSource;
+import de.jreality.audio.javasound.AudioInputStreamSource;
 import de.jreality.audio.javasound.CachedAudioInputStreamSource;
 import de.jreality.audio.javasound.JavaAmbisonicsStereoDecoder;
 import de.jreality.geometry.Primitives;
@@ -11,6 +13,7 @@ import de.jreality.math.MatrixBuilder;
 import de.jreality.scene.AudioSource;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.scene.Sphere;
+import de.jreality.scene.data.RingBuffer.Reader;
 import de.jreality.tools.ActionTool;
 import de.jreality.tools.DraggingTool;
 import de.jreality.ui.viewerapp.ViewerApp;
@@ -30,11 +33,12 @@ public class TestVR {
 		SceneGraphComponent cmp = new SceneGraphComponent();
 		DraggingTool dragtool = new DraggingTool();
 
+		
 		Input wavWaterdrops = Input.getInput("data/waterdrop.wav");
 		final AudioSource s1 = new CachedAudioInputStreamSource("wavnode", wavWaterdrops, true);
 		SceneGraphComponent cmp1 = new SceneGraphComponent();
 		cmp1.setGeometry(new Sphere());
-		MatrixBuilder.euclidean().translate(-2, 0, 0).assignTo(cmp1);
+		MatrixBuilder.euclidean().translate(-4, 0, 0).assignTo(cmp1);
 		cmp.addChild(cmp1);
 		cmp1.setAudioSource(s1);
 		ActionTool at1 = new ActionTool("PanelActivation");
@@ -67,7 +71,7 @@ public class TestVR {
 		};
 		
 		cmp2.setGeometry(Primitives.icosahedron());
-		MatrixBuilder.euclidean().translate(2, 0, 0).assignTo(cmp2);
+		MatrixBuilder.euclidean().translate(0, 0, 0).assignTo(cmp2);
 		cmp2.setAudioSource(sin);
 		ActionTool at2 = new ActionTool("PanelActivation");
 		at2.addActionListener(new ActionListener() {
@@ -79,6 +83,41 @@ public class TestVR {
 		cmp2.addTool(at2);
 		cmp2.addTool(dragtool);
 		sin.start();
+
+		
+		SceneGraphComponent cmp3 = new SceneGraphComponent();
+		cmp.addChild(cmp3);
+		
+		// works with mp3spi from javazoom in classpath:
+		URL url = new URL("http://www.br-online.de/imperia/md/audio/podcast/import/2008_09/2008_09_29_16_33_02_podcastdienasawird50_a.mp3");
+		Input input = Input.getInput(url); 
+		final AudioSource s3 = new AudioInputStreamSource("podcast", input, false) {
+			@Override
+			public int readSamples(Reader reader, float[] buffer,
+					int initialIndex, int samples) {
+				// TODO Auto-generated method stub
+				int ret = super.readSamples(reader, buffer, initialIndex, samples);
+				for (int i=0; i<buffer.length; i++) {
+					buffer[i]*=0.3f;
+				}
+				return ret;
+			}
+		};
+			
+		cmp3.setGeometry(Primitives.cube());
+		MatrixBuilder.euclidean().translate(4, 0, 0).assignTo(cmp3);
+		cmp3.setAudioSource(s3);
+		ActionTool at3 = new ActionTool("PanelActivation");
+		at3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (s3.getState() == AudioSource.State.RUNNING) s3.pause();
+				else s3.start();
+			}
+		});
+		cmp3.addTool(at3);
+		cmp3.addTool(dragtool);
+		s3.start();
+
 		
 		ViewerVR vr = ViewerVR.createDefaultViewerVR(null);
 		// ViewerApp va = ViewerApp.display(cmp);
