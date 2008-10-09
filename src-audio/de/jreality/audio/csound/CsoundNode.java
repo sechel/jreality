d3f066b78e47e77d3fd815d240d3cfe7dfd2858b
Peter Brinkmann <peter.brinkmann@gmail.com>
2008-10-09 03:57:45 +0000

    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3513 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-audio/de/jreality/audio/csound/CsoundNode.java
@@ -0,0 +1,30 @@
+package de.jreality.audio.csound;
+
+import de.jreality.scene.AudioSource;
+
+public class CsoundNode extends AudioSource {
+
+	public CsoundNode(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void setParameterImpl(String name, Object value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void writeSamples(int n) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
