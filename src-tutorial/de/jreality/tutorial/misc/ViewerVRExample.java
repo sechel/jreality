8780acf81b031a2c27bc1e4a99a1f06452bb4c6e
Charles Gunn <gunn@math.tu-berlin.de>
2008-10-23 14:17:53 +0000

    reuse ParametricSurfaceExample featuring its use of ViewerVR
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3557 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-tutorial/de/jreality/tutorial/misc/ViewerVRExample.java
@@ -0,0 +1,19 @@
+package de.jreality.tutorial.misc;
+
+import de.jreality.tutorial.geom.ParametricSurfaceExample;
+
+/**
+ * This is a placeholder class which refers to another
+ * tutorial class {@link ParametricSurfaceExample}. It provides a way of cross-indexing the themes 
+ * without copying the code.
+ * 
+ * @author Charles Gunn
+ *
+ */
+public class ViewerVRExample {
+	public static void main(String[] args) {
+		ParametricSurfaceExample pse = new ParametricSurfaceExample();
+		pse.doIt();
+	}
+
+}
