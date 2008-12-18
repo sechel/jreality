b23b32c319ffe04811025586262805515d692fa0
Charles Gunn <gunn@math.tu-berlin.de>
2008-12-18 14:15:35 +0000

    add default
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3714 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-core/de/jreality/shader/DefaultPointShader.java
+++ src-core/de/jreality/shader/DefaultPointShader.java
@@ -56,7 +56,7 @@ import java.awt.Color;
 //  public final static boolean NORMALS_DRAW_DEFAULT = false;
   public final static double POINT_RADIUS_DEFAULT = 0.025;
   public final static double POINT_SIZE_DEFAULT = 3.0;
-  
+  public final static boolean ATTENUATE_POINT_SIZE_DEFAULT = true;
 	public abstract Color getDiffuseColor();
 	public abstract void setDiffuseColor(Color c);
   
