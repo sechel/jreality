18b8481fdf6f5cfc5b22d6bba467fa31337f8ca0
Charles Gunn <gunn@math.tu-berlin.de>
2009-01-04 16:04:46 +0000

    moved these to their own package: viewer
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3732 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-tutorial/de/jreality/tutorial/viewer/SimpleViewer.java
@@ -0,0 +1,39 @@
+package de.jreality.tutorial.viewer;
+
+import java.awt.Component;
+
+import javax.swing.JFrame;
+
+import de.jreality.geometry.Primitives;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.tools.DraggingTool;
+import de.jreality.tools.RotateTool;
+import de.jreality.ui.viewerapp.ViewerApp;
+import de.jreality.util.Secure;
+import de.jreality.util.SystemProperties;
+
+/**
+ * A simple class showing how to use a {@link ViewerApp} to get a viewing component 
+ * which is then packed into another frame.
+ * @author Charles Gunn
+ *
+ */
+public class SimpleViewer {
+
+	public static void main(String[] args)	{
+		SceneGraphComponent world = new SceneGraphComponent();
+		world.setGeometry(Primitives.sharedIcosahedron);
+		// if you don't want ViewerApp to provide lights and camera and some tools
+		// then use the constructor 	
+		// public ViewerApp(SceneGraphComponent root, SceneGraphPath cameraPath, SceneGraphPath emptyPick, SceneGraphPath avatar) {
+
+		ViewerApp va = new ViewerApp(world);
+        JFrame f = new JFrame();
+        f.getContentPane().add((Component) va.getViewingComponent());
+
+        f.setSize(512, 512);
+        f.validate();
+        f.setVisible(true);
+	}
+ 
+}
