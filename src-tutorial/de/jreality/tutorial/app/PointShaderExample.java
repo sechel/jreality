eee92d74a8ef73e602041c32f44d345965b13715
Charles Gunn <gunn@math.tu-berlin.de>
2009-01-04 16:03:29 +0000

    work on point shader example, add line shader example
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3729 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/app/PointShaderExample.java
+++ src-tutorial/de/jreality/tutorial/app/PointShaderExample.java
@@ -6,7 +6,10 @@ import static de.jreality.shader.CommonAttributes.POINT_SPRITE;
 import java.awt.Color;
 import java.io.IOException;
 
+import javax.swing.SwingConstants;
+
 import de.jreality.backends.label.LabelUtility;
+import de.jreality.geometry.BallAndStickFactory;
 import de.jreality.geometry.PointSetFactory;
 import de.jreality.math.MatrixBuilder;
 import de.jreality.scene.Appearance;
@@ -17,6 +20,7 @@ import de.jreality.scene.data.StorageModel;
 import de.jreality.shader.DefaultGeometryShader;
 import de.jreality.shader.DefaultPointShader;
 import de.jreality.shader.DefaultPolygonShader;
+import de.jreality.shader.DefaultTextShader;
 import de.jreality.shader.ImageData;
 import de.jreality.shader.RenderingHintsShader;
 import de.jreality.shader.ShaderUtility;
@@ -28,8 +32,34 @@ import de.jreality.util.SceneGraphUtility;
 public class PointShaderExample {
   
   
-public static void main(String[] args) throws IOException {
-	PointSetFactory psf = new PointSetFactory();
+/**
+ * Features shown:
+ * 	 flat (points)
+ * 		1. with a sphere sprite
+ * 	 	2. with a text sprite
+ *   with true spheres
+ *   	3. color from appearance, opaque
+ *   	4. color from appearance, transparency enabled
+ *   	5. color from vertices of point set
+ *   	6. ditto, with relative radii allowing varying sizes of spheres
+ *   
+ * Notes:
+ * 	1. These examples do not use point attenuation (see {@link DefaultPointShader#setAttenuatePointSize(Boolean)}.
+ * 		When this is set to true, the sprites appear more realistic, but it is more difficult
+ * 		to control and in this example, I didn't have time to fine tube it.
+ *   2. Size of points (Examples 1 and 2) and spheres (the rest) are set independently
+ *   	since "pointSize" is specified in pixels, and "tubeRadius" in object coordinates.
+ *   	This is obviously inconvenient.  A good reliable fix is welcome.
+ *   3. Colors specified in the shader are ALWAYS RGB values; alpha (transparency) values
+ *   	must be set using the separate "transparency" attribute.  Vertex colors however,
+ *   	are RGBA values -- see Example 5.
+ *   4. This example has been tested on the JOGL backend; other backends may not yield the
+ *   	same behavior, particularly where rendering hints are involved.
+ * @author gunn
+ *
+ */
+public static void main(String[] args) {
+	// prepare a circle of points to be used as the example geometry
 	int numPoints = 20;
 	double[][] verts = new double[numPoints][], 
 		vcolors = new double[numPoints][];
@@ -37,43 +67,55 @@ public static void main(String[] args) throws IOException {
 	for (int i=0; i<numPoints; ++i)	{ 
 		double angle = (Math.PI*2*i)/numPoints;
 		verts[i] = new double[]{Math.cos(angle), Math.sin(angle), 0};
-		vcolors[i] = new double[]{verts[i][0]*.5+.5, verts[i][1]*.5+.5,0.0};
+		vcolors[i] = new double[]{verts[i][0]*.5+.5, verts[i][1]*.5+.5,0.0, 0.5};
 		relrad[i] = verts[i][0]+1.5;
 	}
-	psf.setVertexCount(numPoints);
-	psf.setVertexCoordinates(verts);
-	psf.update();
-	PointSet ps = psf.getPointSet();
 	
+	// each sample gets a numeric label at its geometric center point.  Prepare the appearance.
+	Appearance labelAp = new Appearance();
+    DefaultGeometryShader dgs = ShaderUtility.createDefaultGeometryShader(labelAp, false);
+	dgs.setShowPoints(true);
+    DefaultPointShader pointShader = (DefaultPointShader)dgs.getPointShader();
+    pointShader.setPointRadius(.0001);
+    pointShader.setSpheresDraw(true);
+    pointShader.setDiffuseColor(Color.black);
+	DefaultTextShader pts = (DefaultTextShader) (pointShader).getTextShader();
+	pts.setScale(.005);
+	pts.setOffset(new double[]{0,0,0});
+	pts.setAlignment(SwingConstants.CENTER);
+
 	SceneGraphComponent world = SceneGraphUtility.createFullSceneGraphComponent("world");
 	int numSamples = 6;
 	for (int i = 0; i<numSamples; ++i)	{
-		SceneGraphComponent child = SceneGraphUtility.createFullSceneGraphComponent("world");
+		SceneGraphComponent child = SceneGraphUtility.createFullSceneGraphComponent("child"+i);
 		world.addChild(child);
+		// create the label at the center of the example
+		SceneGraphComponent label = SceneGraphUtility.createFullSceneGraphComponent("label"+i);
+		PointSetFactory labelFac = new PointSetFactory();
+		labelFac.setVertexCount(1);
+		labelFac.setVertexCoordinates(new double[]{0,0,0});
+		labelFac.setVertexLabels(new String[]{""+(i+1)});
+		labelFac.update();
+		label.setGeometry(labelFac.getPointSet());
+		label.setAppearance(labelAp);
+		child.addChild(label);
+
+		// prepare the geometry
+		PointSetFactory psf = new PointSetFactory();
+		psf.setVertexCount(numPoints);
+		psf.setVertexCoordinates(verts);
 		if (i == 4 || i == 5)	{
-			psf = new PointSetFactory();
-			psf.setVertexCount(numPoints);
-			psf.setVertexCoordinates(verts);
 			psf.setVertexColors(vcolors);
-			if (i == 5) psf.setVertexAttribute(Attribute.RELATIVE_RADII, 
-					// aahhg! Don't look at this without some form of protective covering!
-					StorageModel.DOUBLE_ARRAY.createReadOnly(relrad));
-			psf.update();
-			ps = psf.getPointSet();			
 		}
-		else if (i == 5)	{
-			psf = new PointSetFactory();
-			psf.setVertexCount(numPoints);
-			psf.setVertexCoordinates(verts);
-			psf.setVertexColors(vcolors);
-			psf.update();
-			ps = psf.getPointSet();			
-		}
+		if (i == 5) 
+			psf.setVertexAttribute(Attribute.RELATIVE_RADII, relrad);
+		psf.update();
+		PointSet ps = psf.getPointSet();
 		child.setGeometry(ps);
 		Appearance ap = child.getAppearance();
-		DefaultGeometryShader dgs = (DefaultGeometryShader) ShaderUtility.createDefaultGeometryShader(ap, true);
+		dgs = ShaderUtility.createDefaultGeometryShader(ap, true);
 		dgs.setShowPoints(true);
-		RenderingHintsShader rhs = (RenderingHintsShader) ShaderUtility.createDefaultRenderingHintsShader(ap, true);
+		RenderingHintsShader rhs = ShaderUtility.createDefaultRenderingHintsShader(ap, true);
 		DefaultPointShader dps = (DefaultPointShader) dgs.createPointShader("default");
 		DefaultPolygonShader dpls = (DefaultPolygonShader) dps.createPolygonShader("default");
 		MatrixBuilder.euclidean().translate(0,0,-i*.5).assignTo(child);
@@ -86,18 +128,17 @@ public static void main(String[] args) throws IOException {
 				dps.setDiffuseColor(Color.yellow);
 				if (i == 1)	{
 					ImageData id = new ImageData(LabelUtility.createImageFromString("SPR", null, Color.white));
-					Texture2D tex = TextureUtility.createTexture(ap, POINT_SHADER+"."+POINT_SPRITE,id);					
+					TextureUtility.createTexture(ap, POINT_SHADER+"."+POINT_SPRITE,id);					
 				}
 				break;
 			case 2:		// transparency activated, but not for spheres coming from appearances, like these
 			case 3:		// transparency activated, and extends to the spheres coming from appearances. like these
 			case 4:		// opaque spheres with vertex colors
-			case 5:		// opaque spheres with vertex colors
+			case 5:		// opaque spheres with vertex colors, relative radii
 				dps.setSpheresDraw(true);		// the default
-				dpls.setTransparency(.75);
+				dpls.setTransparency(.6);
 				rhs.setTransparencyEnabled(true);
-				rhs.setOpaqueTubesAndSpheres(i != 3);
-				dps.setSpheresDraw(true);
+				rhs.setOpaqueTubesAndSpheres(i != 3 && i != 5);
 				dps.setPointRadius(.05);
 				break;
 		}
