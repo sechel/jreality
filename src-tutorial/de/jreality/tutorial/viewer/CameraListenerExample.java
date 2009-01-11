1861b201b750c558210aec512c731481860cb399
Charles Gunn <gunn@math.tu-berlin.de>
2009-01-11 17:57:24 +0000

    document, polish
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3757 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/viewer/CameraListenerExample.java
+++ src-tutorial/de/jreality/tutorial/viewer/CameraListenerExample.java
@@ -12,6 +12,7 @@ import java.awt.Color;
 import de.jreality.geometry.Primitives;
 import de.jreality.geometry.SphereUtility;
 import de.jreality.jogl.JOGLSphereHelper;
+import de.jreality.math.P3;
 import de.jreality.math.Rn;
 import de.jreality.scene.IndexedFaceSet;
 import de.jreality.scene.SceneGraphComponent;
@@ -19,6 +20,7 @@ import de.jreality.scene.SceneGraphPath;
 import de.jreality.scene.Viewer;
 import de.jreality.scene.event.CameraEvent;
 import de.jreality.scene.event.CameraListener;
+import de.jreality.scene.pick.Graphics3D;
 import de.jreality.shader.DefaultGeometryShader;
 import de.jreality.shader.DefaultLineShader;
 import de.jreality.shader.DefaultPolygonShader;
@@ -27,83 +29,109 @@ import de.jreality.ui.viewerapp.ViewerApp;
 import de.jreality.util.CameraUtility;
 import de.jreality.util.SceneGraphUtility;
 
-public class CameraListenerExample {
+/**
+ * A simple demo showing how to use a camera listener.  It implements a simple level of
+ * detail algorithm to display different resolutions of a tessellated icosahedra based
+ * on the apparent size of the object.
+ * 
+ * To run: execute the main method, and in the graphics window activate the camera zoom tool 
+ * via the mouse scroll wheel.  The camera listener calculates the expected screen extent
+ * of the sphere and chooses a tessellation of the sphere which keeps the size of the
+ * triangles roughly constant.
+ * 
+ * @author gunn
+ *
+ */public class CameraListenerExample {
 
-	public static void main(String[] args) {
-		
-		CameraListenerExample cle = new CameraListenerExample();
-		cle.doIt();
-	}
-	
-	SceneGraphComponent world = SceneGraphUtility.createFullSceneGraphComponent("sphere");
+	SceneGraphComponent sphereSGC = SceneGraphUtility.createFullSceneGraphComponent("sphere");
 	private DefaultPolygonShader dps;
 	private DefaultLineShader dls;
 	private Viewer viewer;
 	private SceneGraphPath camPath;
 	private SceneGraphPath spherePath;
 	private IndexedFaceSet[] levelsOfDetailSpheres;
-	private double[] levels;
 	private Color[] colors;
 	private int numLevels = 6, lastLevel = -1;
 	public void doIt() {
 		
-		world.setGeometry(Primitives.icosahedron());
-		DefaultGeometryShader dgs = ShaderUtility.createDefaultGeometryShader(world.getAppearance(), true);
+		DefaultGeometryShader dgs = ShaderUtility.createDefaultGeometryShader(sphereSGC.getAppearance(), true);
 		dgs.setShowFaces(true);
 		dgs.setShowLines(true);
 		dgs.setShowPoints(false);
 		dps = (DefaultPolygonShader) dgs.createPolygonShader("default");
 		dps.setDiffuseColor(Color.blue);
+		dps.setSmoothShading(false);		// I like it this way ...
 		dls = (DefaultLineShader) dgs.createLineShader("default");
 		dls.setTubeDraw(true);
 		dls.setLineWidth(2.0);		// in case you want no tubes...
 		dls.setDiffuseColor(Color.white);
 
-		world.getAppearance().setAttribute(POLYGON_SHADER+"."+DIFFUSE_COLOR, Color.white);
-		ViewerApp va = new ViewerApp(world);
+		sphereSGC.getAppearance().setAttribute(POLYGON_SHADER+"."+DIFFUSE_COLOR, Color.white);
+		ViewerApp va = new ViewerApp(sphereSGC);
 		viewer = va.getCurrentViewer();
 		va.update();
 		va.display();
+		// set up two scene graph paths, one to the camera and one to the sphere
 		camPath = viewer.getCameraPath();
 		spherePath = SceneGraphUtility.getPathsToNamedNodes(
 				viewer.getSceneRoot(), 
 				"sphere").get(0);
+		// set up a level-of-detail structure representing a sphere
 		levelsOfDetailSpheres = new IndexedFaceSet[numLevels];
-		levels = new double[numLevels];
 		colors = new Color[numLevels];
-		double base = .15;
 		for (int i = 0; i<numLevels; ++i) {
 			levelsOfDetailSpheres[i] = SphereUtility.tessellatedIcosahedronSphere(i);
-			levels[i] = base * (i+1);
 			colors[i] = new Color(i/(numLevels-1f), 0, ((numLevels-1)-i)/(numLevels-1f));
 		}
+		// set up geometry based on initial conditions
+		update();
+		// add the listener, which just calls this class's update() method
 		CameraListener camListener = new CameraListener() {
 			public void cameraChanged(CameraEvent ev) {
 				update();
 			}
 			
 		};
-		update();
 		CameraUtility.getCamera(viewer).addCameraListener(camListener);
 	}
 	
+	/**
+	 * Use the path to the object and to the sphere, plus other information from the
+	 * Camera, to calculate the net object to NDC (normalized device coordinates) transformation.
+	 * Use this to estimate how large the object appears.
+	 * 
+	 * Note: the matrix calculations here could be done somewhat more conveniently using
+	 * an instance of {@link Graphics3D} using the constructor {@link Graphics3D#Graphics3D(Viewer)}
+	 * and setting {@link Graphics3D#setCurrentPath(SceneGraphPath)} to <i>spherePath</i>, then
+	 * this method has only to call {@link Graphics3D#getObjectToNDC()}.  But it's probably good to see how to do it
+	 * by hand.
+	 */
 	public void update()	{
 		double[] s2w = spherePath.getMatrix(null);
 		double[] w2c = camPath.getInverseMatrix(null);
 		double[] c2ndc = CameraUtility.getCameraToNDC(viewer);
+		// the net transformation object to normalized device coordinates is the 
+		// product of the three matrices above.
 		double[] s2ndc = Rn.times(null, Rn.times(null, c2ndc, w2c), s2w);
-//			System.err.println("s2ndc = "+Rn.matrixToString(s2ndc));
-		double size = JOGLSphereHelper.getNDCExtent(s2ndc);
-		double logs = Math.log(size)+Math.log(40);
+		double size = P3.getNDCExtent(s2ndc);
+		// lowest resolution (0) corresponds to an ndc extent of exp(-4) ~= .02
+		// each doubling of this extent moves up to the next tessellation, so
+		// approximate screen size of triangles remains roughly constant.
+		double logs = Math.log(size)/Math.log(2.0)+4;
 		if (logs < 0) logs = 0;
 		if (logs > (numLevels-1)) logs = (numLevels-1);
 		int which = (int) logs;
-		System.err.println("size = "+size+" which = "+which);
+//		System.err.println("size = "+size+" which = "+which);
 		if (which == lastLevel) return;
-		world.setGeometry(levelsOfDetailSpheres[which]);
+		sphereSGC.setGeometry(levelsOfDetailSpheres[which]);
 		dls.setTubeRadius(.033*Math.pow(.5, which));
 		dps.setDiffuseColor(colors[which]);
 		lastLevel = which;
+	}
+	
+	public static void main(String[] args) {	
+		CameraListenerExample cle = new CameraListenerExample();
+		cle.doIt();
 	}
 	
 
