64bba058e2968352ff3c9f9f73730a6e32bdd53b
Peter Brinkmann <peter.brinkmann@gmail.com>
2009-01-26 07:22:54 +0000

    Added exponential attenuation for negatively curved spaces.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3791 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-audio/de/jreality/audio/Attenuation.java
+++ src-audio/de/jreality/audio/Attenuation.java
@@ -1,7 +1,7 @@
 package de.jreality.audio;
 
 /**
- * Simple interface for encapsulating different kinds of volume attenuation, to be used as an appearance
+ * Simple interface for encapsulating different kinds of volume attenuation, to be used as an appearance property
  * 
  * @author brinkman
  *
@@ -18,6 +18,12 @@ public interface Attenuation {
 		public float attenuate(float v, float r) {
 			return v/Math.max(r, 1);
 		}
+	};
+	
+	public static final Attenuation EXPONENTIAL = new Attenuation() {
+		public float attenuate(float v, float r) {
+			return v/(float) Math.exp(r);
+		}	
 	};
 	
 	float attenuate(float v, float r);
