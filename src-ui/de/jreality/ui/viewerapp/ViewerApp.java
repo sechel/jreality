84bb667a7bb00f984a82dc64f0330f24a8ee4eb8
Steffen Weissmann <steffen.weissmann@gmail.com>
2009-02-05 19:40:50 +0000

    improved error handling
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3851 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-ui/de/jreality/ui/viewerapp/ViewerApp.java
+++ src-ui/de/jreality/ui/viewerapp/ViewerApp.java
@@ -486,8 +486,8 @@ public class ViewerApp {
 			public ToolSystem run() {
 				try {
 					return createToolSystem();
-				} catch (Exception exc) { exc.printStackTrace(); }
-				return null;
+				} catch (Exception exc) { throw new RuntimeException(exc); }
+				//return null;
 			}
 		});
 		if (toolSystem.getPickSystem() == null)
