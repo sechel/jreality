5bc61c315f2814a9f1b599d03155bbe9ac507483
Peter Brinkmann <peter.brinkmann@gmail.com>
2009-02-05 23:40:47 +0000

    For future reference...
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3858 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-audio/de/jreality/audio/AudioException.java
@@ -0,0 +1,13 @@
+package de.jreality.audio;
+
+public class AudioException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	AudioException() {
+		super();
+	}
+	
+	public AudioException(String msg) {
+		super(msg);
+	}
+}
