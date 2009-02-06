316c463037a80299a850c9e1782a962b02439542
Charles Gunn <gunn@math.tu-berlin.de>
2009-02-06 11:33:53 +0000

    Restore as deprecated to avoid breaking lots of code.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3866 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-audio/de/jreality/audio/AudioLauncher.java
@@ -0,0 +1,14 @@
+package de.jreality.audio;
+
+import de.jreality.scene.Viewer;
+
+/* Removing this class broke too much code in my students' projects.
+ * 
+ */
+public class AudioLauncher {
+	
+	@Deprecated
+	public static void launch(Viewer v) 	{
+		de.jreality.audio.util.AudioLauncher.launch(v);
+	}
+}
