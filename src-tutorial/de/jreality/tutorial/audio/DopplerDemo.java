7346b5c8d82dd7fa76e837eb64aa21466ba6d2c4
Steffen Weissmann <steffen.weissmann@gmail.com>
2009-02-10 14:43:37 +0000

    .
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3902 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/audio/DopplerDemo.java
+++ src-tutorial/de/jreality/tutorial/audio/DopplerDemo.java
@@ -1,11 +1,15 @@
 package de.jreality.tutorial.audio;
 
+import java.io.IOException;
+
 import de.jreality.audio.SynthSource;
 import de.jreality.geometry.Primitives;
 import de.jreality.math.MatrixBuilder;
+import de.jreality.reader.Readers;
 import de.jreality.scene.AudioSource;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.ui.viewerapp.ViewerApp;
+import de.jreality.util.Input;
 import de.jreality.vr.ViewerVR;
 
 public class DopplerDemo {
@@ -25,18 +29,34 @@ public class DopplerDemo {
 	
 	public DopplerDemo() {
 		audioCmp.setAudioSource(asrc);
-		audioCmp.setGeometry(Primitives.icosahedron());
+		try {
+			SceneGraphComponent bear = Readers.read(Input.getInput("jrs/baer.jrs"));
+			MatrixBuilder.euclidean().rotateZ(Math.PI).translate(0,0,2).scale(0.002).assignTo(bear);
+			audioCmp.addChild(bear);
+		} catch (IOException ioe) {
+			// no bear available
+			audioCmp.setGeometry(Primitives.icosahedron());
+		}
 		centerCmp.addChild(audioCmp);
 		setLocationForTime(0);
 	}
-
+	
 	double omega = 1;
+	
+	double[] gamma = new double[3];
+	double[] gammaDot = new double[3];
+	
+	double[] gammaDot0 = new double[]{0,1,0};
 	
 	void setLocationForTime(double t) {
 		double angle = t*omega;
-		double x = a*Math.cos(angle);
-		double y = b*Math.sin(angle);
-		MatrixBuilder.euclidean().translate(x, y, 1.0).rotateZ(angle).assignTo(audioCmp);
+		gamma[0] = a*Math.cos(angle);
+		gamma[1] = b*Math.sin(angle);
+		
+		gammaDot[0] = -omega*a*Math.sin(t*omega);
+		gammaDot[1] = omega*b*Math.cos(t*omega);
+		
+		MatrixBuilder.euclidean().translate(gamma).rotateFromTo(gammaDot0, gammaDot).assignTo(audioCmp);
 	}
 	
 	public static void main(String[] args) {
@@ -53,10 +73,9 @@ public class DopplerDemo {
 		
 		de.jreality.audio.util.AudioLauncher.launch(va.getCurrentViewer());
 		
-		long st = System.currentTimeMillis();
-		
 		dd.asrc.start();
 		
+		long st = System.currentTimeMillis();
 		while (true) {
 			try {
 				Thread.sleep(1);
@@ -64,7 +83,8 @@ public class DopplerDemo {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			double t = (System.currentTimeMillis()-st)/1000.;
+			long ct = System.currentTimeMillis();
+			double t = (ct-st)*0.001;
 			dd.setLocationForTime(t);
 		}
 	}
