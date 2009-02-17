947794faf76c1db097aafe2f971cdfc6982f4ff7
Peter Brinkmann <peter.brinkmann@gmail.com>
2009-02-17 03:43:40 +0000

    Early preparations for the addition of OSC tools, not terribly useful yet...
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@3973 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-audio/de/jreality/audio/osc/OSCPool.java
@@ -0,0 +1,86 @@
+package de.jreality.audio.osc;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+
+import de.sciss.net.OSCChannel;
+import de.sciss.net.OSCClient;
+import de.sciss.net.OSCServer;
+
+/**
+ * 
+ * Simple class for managing a pool of OSC clients and servers, so  that many OSC tools may share the same OSC connection.
+ * Pretty straightforward, except that users of this class should not call stop() on any of the clients/servers received from
+ * this class; just rely on the garbage collector to clean things up if necessary.
+ * 
+ * Depends on the NetUtil OSC library (http://www.sciss.de/netutil/).
+ * 
+ * @author brinkman
+ *
+ */
+public class OSCPool {
+
+	private static final Map<InetSocketAddress, WeakReference<OSCClient>> udpClients = new HashMap<InetSocketAddress, WeakReference<OSCClient>>();
+	private static final Map<InetSocketAddress, WeakReference<OSCClient>> tcpClients = new HashMap<InetSocketAddress, WeakReference<OSCClient>>();
+
+	private static final Map<Integer, WeakReference<OSCServer>> udpServers = new HashMap<Integer, WeakReference<OSCServer>>();
+	private static final Map<Integer, WeakReference<OSCServer>> tcpServers = new HashMap<Integer, WeakReference<OSCServer>>();
+
+
+	public static OSCClient getUDPClient(InetSocketAddress addr) throws IOException {
+		return getClient(addr, udpClients, OSCChannel.UDP);
+	}
+
+	public static OSCClient getTCPClient(InetSocketAddress addr) throws IOException {
+		return getClient(addr, tcpClients, OSCChannel.TCP);
+	}
+
+	public static OSCServer getUDPServer(int port) throws IOException {
+		return getUDPServer(port, false);
+	}
+
+	public static OSCServer getUDPServer(int port, boolean loopback) throws IOException {
+		return getServer(port, udpServers, OSCChannel.UDP, loopback);
+	}
+
+	public static OSCServer getTCPServer(int port) throws IOException {
+		return getTCPServer(port, false);
+	}
+
+	public static OSCServer getTCPServer(int port, boolean loopback) throws IOException {
+		return getServer(port, tcpServers, OSCChannel.TCP, loopback);
+	}
+
+	
+	private static OSCClient getClient(InetSocketAddress addr, Map<InetSocketAddress, WeakReference<OSCClient>> clients, String protocol) throws IOException {
+		synchronized (clients) {
+			WeakReference<OSCClient> ref = clients.get(addr);
+			OSCClient client = (ref != null) ? ref.get() : null;
+			if (client == null) {
+				client = OSCClient.newUsing(protocol);
+				client.setTarget(addr);
+				client.start();
+
+				clients.put(addr, new WeakReference<OSCClient>(client));
+			}
+			return client;
+		}
+	}
+	
+	private static OSCServer getServer(int port, Map<Integer, WeakReference<OSCServer>> servers, String protocol, boolean loopback) throws IOException {
+		synchronized (servers) {
+			WeakReference<OSCServer> ref = servers.get(port);
+			OSCServer server = (ref != null) ? ref.get() : null;
+			if (server == null) {
+				server = OSCServer.newUsing(protocol, port, loopback);
+				server.start();
+
+				servers.put(port, new WeakReference<OSCServer>(server));
+			}
+			return server;
+		}
+	}
+}
