666f38d6a98ecdadcca850b5ee7176a7f58c6fc1
Peter Brinkmann <peter.brinkmann@gmail.com>
2009-02-23 18:58:56 +0000

    Got rid of @Override tags to resolve errors in Java 1.5
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4029 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-plugin/de/jreality/plugin/view/JRealityPagePlugin.java
+++ src-plugin/de/jreality/plugin/view/JRealityPagePlugin.java
@@ -8,42 +8,34 @@ import de.varylab.jrworkspace.plugin.flavor.HelpFlavor;
 
 public class JRealityPagePlugin extends Plugin implements HelpFlavor {
 
-	@Override
 	public PluginInfo getPluginInfo() {
 		return new PluginInfo("JReality Home Page");
 	}
 
-	@Override
 	public String getHelpDocument() {
 		return "";
 	}
 
-	@Override
 	public Class<?> getHelpHandle() {
 		return getClass();
 	}
 
-	@Override
 	public Icon getHelpIcon() {
 		return null;
 	}
 
-	@Override
 	public String getHelpPath() {
 		return "http://www3.math.tu-berlin.de/jreality/";
 	}
 
-	@Override
 	public String getHelpStyleSheet() {
 		return "files/main.css";
 	}
 
-	@Override
 	public String getHelpTitle() {
 		return "jReality Home Page";
 	}
 
-	@Override
 	public void setHelpListener(HelpListener l) {
 		
 	}
