5e73ad9aa4fbfca3550f956e81d534b4fade12f7
Charles Gunn <gunn@math.tu-berlin.de>
2009-03-17 16:13:18 +0000

    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4109 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/gjnlp.pl
+++ src-tutorial/de/jreality/tutorial/gjnlp.pl
@@ -1,16 +1,22 @@
 #!/usr/bin/perl
-$codebase="http://www3.math.tu-berlin.de/jreality/download/webstart/";
-while ($dir = <*>) {
+$jrealitybase="/net/www3/pub/jreality/";
+$codebase="http://www3.math.tu-berlin.de/jreality/download/webstart/jnlp/";
+$outdir=$jrealitybase."download/webstart/jnlp/";
+#$dirname = $jrealitybase."download/src-tutorial/de/jreality/tutorial/";
+#print $dirname,"\n";
+#opendir( ALLDIR, $dirname) || die "Error in opening $dirname\n";
+#while (($dir = readdir(ALLDIR))) {
+while ( $dir = <*>) {
     if (-d $dir) {
         print $dir,"\n";
-        opendir(DIR, $dir);
+        opendir(DIR, $dirname.$dir);
         @files = readdir(DIR);
         foreach $file (@files)  {
             $end = ".java";
             if ($file =~ /.*$end\b/) {
                 $file =~ s/$end//;
                 open(IN, "Template.jnlp");
-                $outfile = $file.".jnlp";
+                $outfile = $outdir.$file.".jnlp";
                 print "\t",$outfile,"\n";
                 open(OUT, ">$outfile");
                 while (<IN>)    {
