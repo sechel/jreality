e044afe860dca8a38f0c2803f368a98a13576f3c
Charles Gunn <gunn@math.tu-berlin.de>
2009-03-20 12:45:32 +0000

    small changes
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4118 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/app/GLShadingLangExample03.java
+++ src-tutorial/de/jreality/tutorial/app/GLShadingLangExample03.java
@@ -1,6 +1,7 @@
 package de.jreality.tutorial.app;
 
 import static de.jreality.shader.CommonAttributes.POLYGON_SHADER;
+import static de.jreality.shader.CommonAttributes.TEXTURE_2D_1;
 
 import java.awt.Color;
 import java.io.IOException;
@@ -9,6 +10,8 @@ import de.jreality.geometry.SphereUtility;
 import de.jreality.math.MatrixBuilder;
 import de.jreality.scene.Appearance;
 import de.jreality.scene.SceneGraphComponent;
+import de.jreality.scene.data.AttributeEntityUtility;
+import de.jreality.shader.CommonAttributes;
 import de.jreality.shader.DefaultGeometryShader;
 import de.jreality.shader.DefaultPolygonShader;
 import de.jreality.shader.GlslProgram;
@@ -17,6 +20,7 @@ import de.jreality.shader.ShaderUtility;
 import de.jreality.shader.Texture2D;
 import de.jreality.shader.TextureUtility;
 import de.jreality.tutorial.util.SimpleTextureFactory;
+import de.jreality.tutorial.util.SimpleTextureFactory.TextureType;
 import de.jreality.ui.viewerapp.ViewerApp;
 import de.jreality.util.CameraUtility;
 import de.jreality.util.Input;
@@ -43,14 +47,24 @@ public class GLShadingLangExample03 {
 //		ap.setAttribute("useVertexArrays", false);
 		ap.setAttribute("useGLSL", true);
 		GlslProgram brickProg = null;		
-		world.addChild(SphereUtility.tessellatedCubeSphere(3)); 
-		SimpleTextureFactory stf = new SimpleTextureFactory();
-		stf.setColor(0, new Color(0,0,0,0));	// gap color in weave pattern is totally transparent
-		stf.setColor(1, new Color(255,0,100));
-		stf.update();
-		ImageData id = stf.getImageData();
-		Texture2D tex = TextureUtility.createTexture(ap, POLYGON_SHADER,id);
-		tex.setTextureMatrix(MatrixBuilder.euclidean().scale(5).getMatrix());
+		world.setGeometry(SphereUtility.sphericalPatch(0, 0, 90, 90, 30, 30, 1)); 
+		MatrixBuilder.euclidean().rotateY(-Math.PI/2).assignTo(world);
+		byte[] imageRGBAdata = new byte[256 * 4]; 
+		for (int x = 0; x <256; x++) 
+		{ 
+		   imageRGBAdata[x*4] = (byte) (x>>1); 
+		   imageRGBAdata[x*4+1] = (byte) x; 
+		   imageRGBAdata[x*4+2] = (byte) x; 
+		   imageRGBAdata[x*4+3] = ~0; 
+		} 
+		       
+		ImageData id = new de.jreality.shader.ImageData(imageRGBAdata, 256, 1); 
+		Texture2D tex = TextureUtility.createTexture(ap, POLYGON_SHADER, 0, id);
+		// rotate this texture by 90 degrees
+		tex.setTextureMatrix(MatrixBuilder.euclidean().scale(4).rotateZ(Math.PI/2).getMatrix());
+
+		Texture2D gradTexture = (Texture2D) TextureUtility.createTexture(ap, POLYGON_SHADER, 1, id); 
+		gradTexture.setTextureMatrix(MatrixBuilder.euclidean().scale(3).getMatrix());
 		try {
 			brickProg = new GlslProgram(ap, "polygonShader",   
 					null,
@@ -59,7 +73,17 @@ public class GLShadingLangExample03 {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		brickProg.setUniform("sampler", Texture2D.GL_TEXTURE0);
+// sampler.frag:
+//		uniform sampler2D  sampler;
+//		uniform sampler2D sampler2;
+//		void main(void)
+//		{
+//		    vec4 currentSample = texture2D(sampler,gl_TexCoord[0].st); 
+//		    vec4 currentSample2 = texture2D(sampler2,gl_TexCoord[1].st); 
+//		    gl_FragColor = currentSample*currentSample2 * gl_Color; 
+//		}
+		brickProg.setUniform("sampler",0);
+		brickProg.setUniform("sampler2",1);		
 		ViewerApp va = ViewerApp.display(world);
 		CameraUtility.encompass(va.getCurrentViewer());
 	}
