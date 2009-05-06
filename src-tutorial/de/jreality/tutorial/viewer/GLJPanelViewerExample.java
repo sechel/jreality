02fa4dc6e0b4460e9469715e67a28cd774561c6b
Charles Gunn <gunn@math.tu-berlin.de>
2009-05-06 07:09:03 +0000

    example of jogl GLPanelViewer
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4331 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-tutorial/de/jreality/tutorial/viewer/GLJPanelViewerExample.java
@@ -0,0 +1,60 @@
+package de.jreality.tutorial.viewer;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics2D;
+import java.util.EventObject;
+
+import javax.swing.JFrame;
+
+import de.jreality.geometry.Primitives;
+import de.jreality.jogl.GLJPanelViewer;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.ui.viewerapp.ViewerApp;
+import de.jreality.util.SystemProperties;
+
+/**
+ * A simple class showing how to use a {@link ViewerApp} to get a viewing component 
+ * which is then packed into another frame.
+ * @author Charles Gunn
+ *
+ */
+public class GLJPanelViewerExample {
+
+	public static void main(String[] args)	{
+		SceneGraphComponent world = new SceneGraphComponent();
+		world.setGeometry(Primitives.sharedIcosahedron);
+		// if you don't want ViewerApp to provide lights and camera and some tools
+		// then use the constructor 	
+		// public ViewerApp(SceneGraphComponent root, SceneGraphPath cameraPath, SceneGraphPath emptyPick, SceneGraphPath avatar) {
+	    System.setProperty(SystemProperties.VIEWER, "de.jreality.jogl.GLJPanelViewer"); // de.jreality.portal.DesktopPortalViewer");
+	      
+		ViewerApp va = new ViewerApp(world);
+		va.getViewer().getSceneRoot().getAppearance().setAttribute("backgroundColor", new Color(0,255,0,128));
+		final GLJPanelViewer glpv = (GLJPanelViewer) va.getCurrentViewer();
+		glpv.addRenderListener(new de.jreality.jogl.AbstractViewer.RenderListener() {
+
+			public void postRender(EventObject e) {
+				Graphics2D g2 = glpv.getG2d();
+				if (g2 == null) return;
+				g2.setColor(Color.pink);
+				g2.fillRect(0, 0, 50, 50);
+			}
+
+			public void preRender(EventObject e) {
+				Graphics2D g2 = glpv.getG2d();
+				if (g2 == null) return;
+				g2.setColor(Color.blue);
+				g2.fillRect(0, 50, 50, 50);		
+			}
+			
+		});
+        JFrame f = new JFrame();
+        f.getContentPane().add((Component) va.getViewingComponent());
+
+        f.setSize(512, 512);
+        f.validate();
+        f.setVisible(true);
+	}
+ 
+}
