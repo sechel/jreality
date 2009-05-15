84e6dc38e947ec220264286597ad0da24a803dd9
Peter Brinkmann <peter.brinkmann@gmail.com>
2009-05-15 05:34:30 +0000

    Updated for new version of plugins
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4453 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/audio/MinimalExample.java
+++ src-tutorial/de/jreality/tutorial/audio/MinimalExample.java
@@ -6,20 +6,13 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import de.jreality.audio.csound.CsoundNode;
-import de.jreality.geometry.Primitives;
-import de.jreality.plugin.audio.AudioLauncher;
-import de.jreality.plugin.audio.AudioOptions;
-import de.jreality.plugin.view.CameraStand;
-import de.jreality.plugin.view.Inspector;
-import de.jreality.plugin.view.Lights;
-import de.jreality.plugin.view.View;
-import de.jreality.plugin.view.ViewMenuBar;
-import de.jreality.plugin.vr.Avatar;
+import de.jreality.plugin.JRViewer;
+import de.jreality.reader.Readers;
 import de.jreality.scene.AudioSource;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.tools.ActionTool;
+import de.jreality.tools.DraggingTool;
 import de.jreality.util.Input;
-import de.varylab.jrworkspace.plugin.simplecontroller.SimpleController;
 
 /**
  * 
@@ -29,53 +22,30 @@ import de.varylab.jrworkspace.plugin.simplecontroller.SimpleController;
  *
  */
 public class MinimalExample {
-	public static void main(String[] args) 
-	throws IOException {
-		SimpleController c = 
-			new SimpleController();
 
-		// set up graphics
-		c.registerPlugin(new ViewMenuBar());
-		c.registerPlugin(new View());
-		c.registerPlugin(new CameraStand());
-		c.registerPlugin(new Lights());
-		c.registerPlugin(new Inspector());
-		c.registerPlugin(new Avatar());
-
-		// set up audio
-		c.registerPlugin(new AudioLauncher());
-		c.registerPlugin(new AudioOptions());
-
-		// create visual content
-		SceneGraphComponent content = new SceneGraphComponent("content");
-		content.setGeometry(Primitives.sphere(20));
-		
-		// create audio content
-		final AudioSource source = new CsoundNode("csound node", Input.getInput(MinimalExample.class.getResource("trapped.csd")));
+	public static void main(String[] args) throws IOException {
+		SceneGraphComponent audioComponent = Readers.read(Input.getInput("jrs/bear.jrs"));
+		Input input = Input.getInput(MinimalExample.class.getResource("trapped.csd"));
+		final AudioSource source = new CsoundNode("csound node", input);
 		source.start();
-		content.setAudioSource(source);
-
-		// create simple tool for pausing/starting audio
-		ActionTool tool = 
-			new ActionTool("PanelActivation");
-		tool.addActionListener(new ActionListener() {
-			public void actionPerformed(
-					ActionEvent e) {
-				if (source.getState() ==
-					AudioSource.State.RUNNING)
+		audioComponent.setAudioSource(source);
+	
+		ActionTool actionTool = new ActionTool("PanelActivation");
+		actionTool.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (source.getState() == AudioSource.State.RUNNING) {
 					source.pause();
-				else
+				} else {
 					source.start();
+				}
 			}
 		});
-		content.addTool(tool);
+		audioComponent.addTool(actionTool);
+		audioComponent.addTool(new DraggingTool());
 
-		// attach content to scene
-		SceneGraphComponent parent = 
-			c.getPlugin(View.class).getSceneRoot();
-		parent.addChild(content);
-
-		// launch viewer
-		c.startup();
+		JRViewer v = JRViewer.createViewerVRWithAudio();
+		v.setPropertiesFile("MinimalExamle.jrw");
+		v.setContent(audioComponent);
+		v.startup();
 	}
 }
