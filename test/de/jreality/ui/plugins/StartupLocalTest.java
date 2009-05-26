809c53c18de917515f3799c4ef4a34483ef4ab16
Stefan Sechelmann <sechel@math.tu-berlin.de>
2009-05-26 16:12:50 +0000

    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4508 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- test/de/jreality/ui/plugins/StartupLocalTest.java
+++ test/de/jreality/ui/plugins/StartupLocalTest.java
@@ -3,27 +3,24 @@ package de.jreality.ui.plugins;
 import java.awt.GridLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JRootPane;
 
 import de.jreality.geometry.Primitives;
 import de.jreality.plugin.JRViewer;
-import de.jreality.plugin.view.CameraStand;
-import de.jreality.plugin.view.ContentTools;
-import de.jreality.plugin.view.Lights;
+import de.jreality.plugin.JRViewer.ContentType;
 
 public class StartupLocalTest {
 
 	public static void main(String[] args) {
-		JRViewer v = JRViewer.createEmptyViewer();
-		v.registerPlugin(new CameraStand());
-		v.registerPlugin(new Lights());
-		v.registerPlugin(new ContentTools());
-		v.setContent(Primitives.sharedIcosahedron);
-		v.startupLocal();
+		JRViewer v = new JRViewer();
+		v.addBasicUI();
+		v.setContent(Primitives.sharedIcosahedron, ContentType.CenteredAndScaled);
+		JRootPane p = v.startupLocal();
 		
 		JFrame f = new JFrame();
 		f.setSize(800, 600);
 		f.setLayout(new GridLayout());
-		f.add(v.getView().getContentPanel());
+		f.add(p);
 		f.setVisible(true);
 	}
 
--- test/de/jreality/ui/plugins/ViewerTest.java
+++ test/de/jreality/ui/plugins/ViewerTest.java
@@ -2,14 +2,14 @@ package de.jreality.ui.plugins;
 
 import de.jreality.geometry.Primitives;
 import de.jreality.plugin.JRViewer;
-import de.jreality.plugin.view.Export;
+import de.jreality.plugin.JRViewer.ContentType;
 
 public class ViewerTest {
 
 	public static void main(String[] args) {
-		JRViewer v = JRViewer.createViewerApp();
-		v.setContent(Primitives.coloredCube());
-		v.registerPlugin(new Export());
+		JRViewer v = new JRViewer();
+		v.addBasicUI();
+		v.setContent(Primitives.coloredCube(), ContentType.CenteredAndScaled);
 		v.startup();
 	}
 
