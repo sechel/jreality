d16d4008326d5161f0cc3d731c97039aaca0c388
Stefan Sechelmann <sechel@math.tu-berlin.de>
2009-05-26 10:38:23 +0000

    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4489 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- test/de/jreality/ui/plugins/PluginHierarchyTest.java
+++ test/de/jreality/ui/plugins/PluginHierarchyTest.java
@@ -1,6 +1,8 @@
 package de.jreality.ui.plugins;
 
+import de.jreality.plugin.view.ContentLoader;
 import de.jreality.plugin.view.Lights;
+import de.jreality.plugin.view.View;
 import de.jreality.plugin.vr.Avatar;
 import de.jreality.plugin.vr.Terrain;
 import de.varylab.jrworkspace.plugin.simplecontroller.SimpleController;
@@ -9,6 +11,8 @@ public class PluginHierarchyTest {
 
 	public static void main(String[] args) {
 		SimpleController c = new SimpleController();
+		c.registerPlugin(new View());
+		c.registerPlugin(new ContentLoader());
 		c.registerPlugin(new Terrain());
 		c.registerPlugin(new Avatar());
 		c.registerPlugin(new Lights());
