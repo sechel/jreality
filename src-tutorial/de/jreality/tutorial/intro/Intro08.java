8abc0f6837f654222be06c42edb7ae1ad7a33afd
Charles Gunn <gunn@math.tu-berlin.de>
2009-05-27 08:38:03 +0000

    use JRViewer
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4516 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/intro/Intro08.java
+++ src-tutorial/de/jreality/tutorial/intro/Intro08.java
@@ -2,6 +2,7 @@ package de.jreality.tutorial.intro;
 
 import de.jreality.geometry.Primitives;
 import de.jreality.math.MatrixBuilder;
+import de.jreality.plugin.JRViewer;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.ui.viewerapp.ViewerApp;
 import de.jreality.util.SceneGraphUtility;
@@ -22,18 +23,8 @@ public class Intro08 {
 	public static void main(String[] args)	{
 		SceneGraphComponent myscene = SceneGraphUtility.createFullSceneGraphComponent("myscene");
 		myscene.setGeometry(Primitives.coloredCube());
-		ViewerApp va = myViewerApp(myscene);
-		va.update();
 		MatrixBuilder.euclidean().scale(2,.8,1).assignTo(myscene);
+		JRViewer.display(myscene);
 	}
-
-	private static ViewerApp myViewerApp(SceneGraphComponent myscene) {
-		ViewerApp va = ViewerApp.display(myscene);
-		va.setAttachNavigator(true);
-		va.setExternalNavigator(false);
-		return va;
-	}
-
-
 
 }
