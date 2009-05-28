7c667fe47b03c4dcb9ffb7d1d909730b90fcd51c
Stefan Sechelmann <sechel@math.tu-berlin.de>
2009-05-28 15:21:39 +0000

    web content loader experimental plug-in
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4554 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-plugin/de/jreality/plugin/experimental/WebContentLoader.java
@@ -0,0 +1,235 @@
+package de.jreality.plugin.experimental;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.AbstractListModel;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.HTML.Attribute;
+import javax.swing.text.html.HTML.Tag;
+
+import de.jreality.plugin.JRViewer;
+import de.jreality.plugin.PluginUtility;
+import de.jreality.plugin.JRViewer.ContentType;
+import de.jreality.plugin.basic.Content;
+import de.jreality.plugin.basic.View;
+import de.jreality.reader.Readers;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.util.Input;
+import de.varylab.jrworkspace.plugin.Controller;
+import de.varylab.jrworkspace.plugin.PluginInfo;
+import de.varylab.jrworkspace.plugin.sidecontainer.SideContainerPerspective;
+import de.varylab.jrworkspace.plugin.sidecontainer.template.ShrinkPanelPlugin;
+
+public class WebContentLoader extends ShrinkPanelPlugin implements ActionListener {
+
+	private Content 
+		contentPlugin = null;
+	private JTextField
+		locationField = new JTextField("http://www.sechel.de/models/");
+	private JList
+		contentList = new JList(new ContentModel());
+	private JScrollPane
+		contentScroller = new JScrollPane(contentList);
+	private HTMLEditorKit
+		htmlEditorKit = new HTMLEditorKit();
+	private List<WebModel>
+		content = new LinkedList<WebModel>();
+	private JCheckBox
+		encompassChecker = new JCheckBox("Encompass After Load", true);
+	private JButton
+		loadButton = new JButton("Load Model");
+	
+	
+	public WebContentLoader() {
+		shrinkPanel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1.0;
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		c.insets = new Insets(2,2,2,2);
+		
+		
+		contentScroller.setPreferredSize(new Dimension(10, 150));
+		shrinkPanel.add(locationField, c);
+		c.weighty = 1.0;
+		c.fill = GridBagConstraints.BOTH;
+		shrinkPanel.add(contentScroller, c);
+		c.weighty = 0.0;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		shrinkPanel.add(encompassChecker, c);
+		shrinkPanel.add(loadButton, c);
+		
+		locationField.addActionListener(this);
+		loadButton.addActionListener(this);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (locationField == e.getSource()) {
+			updateContent();
+		}
+		if (loadButton == e.getSource()) {
+			Object[] models = contentList.getSelectedValues();
+			SceneGraphComponent root = new SceneGraphComponent("Web Models");
+			for (Object model : models) {
+				WebModel wm = (WebModel)model;
+				try {
+					Input in = Input.getInput(wm.uri.toURL());
+					SceneGraphComponent c = Readers.read(in);
+					root.addChild(c);
+				} catch (Exception ex) {
+					System.out.println("Cannot load model " + wm.name + ": " + ex.getMessage());
+				}
+			}
+			contentPlugin.setContent(root);
+			if (encompassChecker.isSelected()) {
+				contentPlugin.encompassEuclidean();
+			}
+			
+		}
+	}
+	
+	private void updateContent() {
+		content.clear();
+		URI siteUrl = null;
+		try {
+			siteUrl = new URI(locationField.getText());
+		} catch (URISyntaxException e) {
+			content.add(new WebModel(null, "Invalid URI"));
+			contentList.revalidate();
+			return;
+		}
+		InputStream in = null; 
+		try {
+			in = siteUrl.toURL().openStream();
+		} catch (IOException e) {
+			content.add(new WebModel(null, "Cannot open URL"));
+			contentList.revalidate();
+			return;
+		}
+		HTMLDocument doc =(HTMLDocument) htmlEditorKit.createDefaultDocument();
+		try {
+			htmlEditorKit.read(in, doc, 0);
+		} catch (Exception e) {
+			content.add(new WebModel(null, "Cannot read web page"));
+			contentList.revalidate();
+			return;
+		}
+		HTMLDocument.Iterator linkIt = doc.getIterator(Tag.A);
+		for (; linkIt.isValid(); linkIt.next()) {
+			AttributeSet aSet = linkIt.getAttributes();
+			String href = (String)aSet.getAttribute(Attribute.HREF);
+			try {
+				URI uriRef = new URI(href);
+				if (!checkFileExtension(uriRef)) {
+					continue;
+				}
+				uriRef = siteUrl.resolve(uriRef);
+				String name = uriRef.toURL().getFile();
+				WebModel model = new WebModel(uriRef, name);
+				content.add(model);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		contentList.revalidate();
+	}
+	
+	
+	protected boolean checkFileExtension(URI uri) {
+		String name = uri.toString().toLowerCase();
+		boolean r = name.endsWith(".obj");
+		r |= name.endsWith(".3ds");
+		r |= name.endsWith(".jrs");
+		return r;
+	}
+	
+	
+	
+	
+	private class WebModel {
+		
+		public URI
+			uri = null;
+		public String
+			name = "NoName";
+
+		public WebModel() {
+		}
+
+		public WebModel(URI url, String name) {
+			this.uri = url;
+			this.name = name;
+		}
+		
+		@Override
+		public String toString() {
+			return name;
+		}
+		
+	}
+	
+	private class ContentModel extends AbstractListModel {
+
+		private static final long 
+			serialVersionUID = 1L;
+
+		@Override
+		public Object getElementAt(int index) {
+			return content.get(index);
+		}
+
+		@Override
+		public int getSize() {
+			return content.size();
+		}
+		
+	}
+	
+	
+	@Override
+	public void install(Controller c) throws Exception {
+		super.install(c);
+		contentPlugin = PluginUtility.getContentPlugin(c);
+		updateContent();
+	}
+	
+	
+	@Override
+	public Class<? extends SideContainerPerspective> getPerspectivePluginClass() {
+		return View.class;
+	}
+
+	@Override
+	public PluginInfo getPluginInfo() {
+		return new PluginInfo("Web Content Loader", "Stefan Sechelmann");
+	}
+
+	
+	public static void main(String[] args) {
+		JRViewer v = new JRViewer();
+		v.addBasicUI();
+		v.addContentSupport(ContentType.CenteredAndScaled);
+		v.registerPlugin(new WebContentLoader());
+		v.startup();
+	}
+	
+}
