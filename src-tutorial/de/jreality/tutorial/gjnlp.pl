e876355f9be5f53c960fb3b11e8aecdf0527ca2c
Steffen Weissmann <steffen.weissmann@gmail.com>
2009-06-08 16:20:58 +0000

    moved tutorial locations to jrwebroot/tutorial
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4631 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/gjnlp.pl
+++ src-tutorial/de/jreality/tutorial/gjnlp.pl
@@ -1,18 +1,20 @@
 #!/usr/bin/perl
 $jrealitybase="/net/www3/pub/jreality/";
-$codebase="http://www3.math.tu-berlin.de/jreality/download/webstart/jnlp/";
-$outdir=$jrealitybase."download/webstart/jnlp/";
-#$dirname = $jrealitybase."download/src-tutorial/de/jreality/tutorial/";
+$codebase="http://www3.math.tu-berlin.de/jreality/tutorial/jnlp/";
+$outdir=$jrealitybase."tutorial/jnlp/";
+$dirname = $jrealitybase."tutorial/src/de/jreality/tutorial/";
 #print $dirname,"\n";
 #opendir( ALLDIR, $dirname) || die "Error in opening $dirname\n";
 #while (($dir = readdir(ALLDIR))) {
 while ( $dir = <*>) {
     if (-d $dir) {
         print $dir,"\n";
+        print $dirname.$dir,"\n";
         opendir(DIR, $dirname.$dir);
         @files = readdir(DIR);
         foreach $file (@files)  {
             $end = ".java";
+            print $file,"\n";
             if ($file =~ /.*$end\b/) {
                 $file =~ s/$end//;
                 open(IN, "Template.jnlp");
