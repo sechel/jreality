1766b63f9ccbb90d09fffb3aa261b078f6fb131f
Charles Gunn <gunn@math.tu-berlin.de>
2009-06-29 09:40:01 +0000

    fix glBlendFuncSeparate() calls
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4811 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-jogl/de/jreality/jogl/JOGLRendererHelper.java
+++ src-jogl/de/jreality/jogl/JOGLRendererHelper.java
@@ -752,8 +752,10 @@ public class JOGLRendererHelper {
 		gl.glEnable(GL.GL_BLEND);
 		gl.glDisable(GL.GL_LIGHTING);
 		gl.glDepthMask(true);
-//		gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
-		gl.glBlendFuncSeparate(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA, GL.GL_ONE, GL.GL_ONE_MINUS_SRC_ALPHA);
+		if (JOGLConfiguration.hasBlendFuncSeparate) 
+			gl.glBlendFuncSeparate(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA, GL.GL_ONE, GL.GL_ONE_MINUS_SRC_ALPHA);
+		else 
+			gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
 		gl.glColor3d(1, 1, 1);
 		double[] c2o = jr.getContext().getCameraToObject();
 		gl.glActiveTexture(GL.GL_TEXTURE0);
