7ff8d4a51fc7af1ab8490c7b1b0af4f804547b60
Paul Peters <peters@math.tu-berlin.de>
2009-07-22 06:30:42 +0000

    comments which explain things from ParametricSurfaceExample removed for better readablity
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4890 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/gui/BeanInspectorExample.java
+++ src-tutorial/de/jreality/tutorial/gui/BeanInspectorExample.java
@@ -24,40 +24,27 @@ import de.jtem.beans.InspectorPanel;
  *
  */
 public class BeanInspectorExample {
-	// to use the ParametricSurfaceFactory one needs an instance of immersion
-	// That is, a function that maps  (u,v) values into a 3- or 4-space
 	public static Immersion swallowtailImmersion = new Immersion() {
 		public void evaluate(double u, double v, double[] xyz, int index) {
 			xyz[index]= 10*(u-v*v);
 			xyz[index+1]= 10*u*v;
 			xyz[index+2]= 10*(u*u-4*u*v*v);
 		}
-		// how many dimensions in the image space?
 		public int getDimensionOfAmbientSpace() { return 3;	}
-		// Does evaluate() always put the same value into xyz for a given pair (u,v)?
-		// If the immersion has parameters that affect the result of evaluate() then isImmutable()
-		// should return false.
 		public boolean isImmutable() { return true; }
 	};
 	
 	public static void main(String[] args) {
-		//initialize the parametric surface factory
 		final ParametricSurfaceFactory psf = new ParametricSurfaceFactory(swallowtailImmersion);
-		//uv-extension of the domain
 		psf.setUMin(-.3);psf.setUMax(.3);psf.setVMin(-.4);psf.setVMax(.4);
-		//subdivisions of th domain
 		psf.setULineCount(20);psf.setVLineCount(20);
-		//generate edges and normals
 		psf.setGenerateEdgesFromFaces(true);
 		psf.setGenerateVertexNormals(true);
-		//generate the IndexFaceSet
 		psf.update();
 		
-		//put the generated IndexedFaceSet into the SceneGraphComponent sgc
 		SceneGraphComponent sgc = new SceneGraphComponent("Swallowtail");
 		sgc.setGeometry(psf.getIndexedFaceSet());
 		
-		// Finally show the example in the JRViewer with virtual reality enabled, i.e., JRViewerVR
 		JRViewer v = new JRViewer();
 		v.addBasicUI();
 		v.addVRSupport();
