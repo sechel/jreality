80f94b177cc96cb7751990887ea87dc5ed43169f
Paul Peters <peters@math.tu-berlin.de>
2009-07-22 05:44:25 +0000

    code clean up, unrelated code removed.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4886 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/geom/ParametricSurfaceExample.java
+++ src-tutorial/de/jreality/tutorial/geom/ParametricSurfaceExample.java
@@ -8,12 +8,11 @@ import de.jreality.plugin.JRViewer.ContentType;
 import de.jreality.plugin.content.ContentAppearance;
 import de.jreality.plugin.content.ContentTools;
 import de.jreality.scene.SceneGraphComponent;
-import de.jreality.util.Secure;
 
 public class ParametricSurfaceExample {
 	// to use the ParametricSurfaceFactory one needs an instance of immersion
 	// That is, a function that maps  (u,v) values into a 3- or 4-space
-	Immersion myImmersion = new Immersion() {
+	public static Immersion swallowtailImmersion = new Immersion() {
 		public void evaluate(double u, double v, double[] xyz, int index) {
 			xyz[index]= 10*(u-v*v);
 			xyz[index+1]= 10*u*v;
@@ -27,45 +26,35 @@ public class ParametricSurfaceExample {
 		public boolean isImmutable() { return true; }
 	};
 	
-	public void doIt(boolean useViewerVR)	{		
+	public static void main(String[] args) {
 		//initialize the parametric surface factory
-		ParametricSurfaceFactory psf = new ParametricSurfaceFactory(myImmersion);
-		psf.setUMin(-.3);
-		psf.setUMax(.3);
-		psf.setVMin(-.4);
-		psf.setVMax(.4);
-		psf.setULineCount(20);
-		psf.setVLineCount(20); 
+		final ParametricSurfaceFactory psf = new ParametricSurfaceFactory(swallowtailImmersion);
+		//uv-extension of the domain
+		psf.setUMin(-.3);psf.setUMax(.3);psf.setVMin(-.4);psf.setVMax(.4);
+		//subdivisions of th domain
+		psf.setULineCount(20);psf.setVLineCount(20);
+		//generate edges and normals
 		psf.setGenerateEdgesFromFaces(true);
 		psf.setGenerateVertexNormals(true);
+		//generate the IndexFaceSet
 		psf.update();
 		
-		//put the result of the factory into the SceneGraphComponent sgc
+		//put the generated IndexedFaceSet into the SceneGraphComponent sgc
 		SceneGraphComponent sgc = new SceneGraphComponent("Swallowtail");
 		sgc.setGeometry(psf.getIndexedFaceSet());
 		
-		// Finally show the example in the Viewer or ViewerVR.
-		// to run the application inside of ViewerVR, set the VM argument 
-		// -DuseViewerVR=true  
-		// In eclipse: Run->Run..., click on "Arguments", enter this in "VM Arguments" text field
-		String foo = Secure.getProperty("useViewerVR");
-		if (useViewerVR || (foo != null && foo.equals("true")))	{
-			JRViewer v = new JRViewer();
-			v.addBasicUI();
-			v.addVRSupport();
-			v.addContentSupport(ContentType.TerrainAligned);
-			v.setPropertiesFile("vrprefsParametricExample.xml");
-			v.registerPlugin(new ContentAppearance());
-			v.registerPlugin(new ContentTools());
-			v.setContent(sgc);
-			v.startup();
-		} else {
-			JRViewer.display(sgc);		
-		}
-	}
+		// Finally show the example in the JRViewer with virtual reality enabled, i.e., JRViewerVR
+		JRViewer v = new JRViewer();
+		v.addBasicUI();
+		v.addVRSupport();
+		v.addContentSupport(ContentType.TerrainAligned);
+		v.registerPlugin(new ContentAppearance());
+		v.registerPlugin(new ContentTools());
+		v.setContent(sgc);
+
 	
-	public static void main(String[] args) {
-		ParametricSurfaceExample pse = new ParametricSurfaceExample();
-		pse.doIt(true);
-	}
+		//Start the viewer
+		v.startup();
+		
+	}	
 }
