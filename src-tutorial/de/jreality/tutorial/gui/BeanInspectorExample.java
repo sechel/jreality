d6201f4aec7dfaad0cd4cc8a6a2be09851a1459f
Paul Peters <peters@math.tu-berlin.de>
2009-07-22 04:54:37 +0000

    made independent of tutorial.util.BeanInspectorPlugin instead uses new ViewShrinkPanelPlugin
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4883 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/gui/BeanInspectorExample.java
+++ src-tutorial/de/jreality/tutorial/gui/BeanInspectorExample.java
@@ -1,14 +1,21 @@
 package de.jreality.tutorial.gui;
 
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
 import de.jreality.geometry.ParametricSurfaceFactory;
 import de.jreality.geometry.ParametricSurfaceFactory.Immersion;
 import de.jreality.plugin.JRViewer;
 import de.jreality.plugin.JRViewer.ContentType;
+import de.jreality.plugin.basic.ViewShrinkPanelPlugin;
 import de.jreality.plugin.content.ContentAppearance;
 import de.jreality.plugin.content.ContentTools;
 import de.jreality.scene.SceneGraphComponent;
-import de.jreality.tutorial.util.BeanInspectorPlugin;
+import de.jtem.beans.InspectorPanel;
 
 public class BeanInspectorExample {
 	// to use the ParametricSurfaceFactory one needs an instance of immersion
@@ -29,7 +36,7 @@ public class BeanInspectorExample {
 	
 	public static void main(String[] args) {
 		//initialize the parametric surface factory
-		ParametricSurfaceFactory psf = new ParametricSurfaceFactory(swallowtailImmersion);
+		final ParametricSurfaceFactory psf = new ParametricSurfaceFactory(swallowtailImmersion);
 		//uv-extension of the domain
 		psf.setUMin(-.3);psf.setUMax(.3);psf.setVMin(-.4);psf.setVMax(.4);
 		//subdivisions of th domain
@@ -49,13 +56,29 @@ public class BeanInspectorExample {
 		v.addBasicUI();
 		v.addVRSupport();
 		v.addContentSupport(ContentType.TerrainAligned);
-		v.setPropertiesFile("vrprefsParametricExample.xml");
 		v.registerPlugin(new ContentAppearance());
 		v.registerPlugin(new ContentTools());
 		v.setContent(sgc);
-		
+
 		//add an Inspector for the domain
-		v.getController().registerPlugin(new BeanInspectorPlugin("Domain",psf,null));
+		InspectorPanel inspector = new InspectorPanel();
+		inspector.setObject(psf, null);
+		JButton updateButton=new JButton("update");
+		updateButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				psf.update();
+			}
+		});		
+		inspector.add(updateButton,BorderLayout.SOUTH);
+		inspector.revalidate();
+
+		
+		ViewShrinkPanelPlugin plugin = new ViewShrinkPanelPlugin("Domain");
+		plugin.getShrinkPanel().add(inspector);
+		v.registerPlugin(plugin);
+		
+		//Start the viewer
 		v.startup();
+		
 	}	
 }
