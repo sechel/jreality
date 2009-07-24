4ef6558a54d54d6960ba50d5dbf782df018c35f2
Paul Peters <peters@math.tu-berlin.de>
2009-07-24 08:28:05 +0000

    ViewShringPanelPlugin should better be abstract, because the plugin system only allows any plugin.class to appear once. This also renders the problem with the constructor of ShrinkPanelPlugin calling getPluginInfo irrelevant.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4894 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-plugin/de/jreality/plugin/basic/ViewShrinkPanelPlugin.java
+++ src-plugin/de/jreality/plugin/basic/ViewShrinkPanelPlugin.java
@@ -2,43 +2,27 @@ package de.jreality.plugin.basic;
 
 import de.jreality.plugin.JRViewer;
 import de.varylab.jrworkspace.plugin.Plugin;
-import de.varylab.jrworkspace.plugin.PluginInfo;
 import de.varylab.jrworkspace.plugin.sidecontainer.SideContainerPerspective;
 import de.varylab.jrworkspace.plugin.sidecontainer.template.ShrinkPanelPlugin;
 
 /** Use this class to get a  {@link ShrinkPanelPlugin} that belongs to the 
- * main {@link Plugin}, i.e., {@link View}, of {@link JRViewer}. Add this
- * to the JRViewer with {@link JRViewer#registerPlugin()};
+ * main {@link Plugin}, i.e., {@link View}, of {@link JRViewer}. Extend this class#
+ * and add it to the JRViewer with {@link JRViewer#registerPlugin()};
+ * 
+ * Extension of the class is necessary, because only one plugin of each class is allowed.
  * 
  * @author G. Paul Peters, 22.07.2009
  *
  */
-public class ViewShrinkPanelPlugin extends ShrinkPanelPlugin {
-	//TODO: pinfoDummy hack because ShrinkPanelPlugin needs a PluginInfo with non-null name!
-	static private PluginInfo pinfoDummy=new PluginInfo("");
-	
-	private PluginInfo pinfo;
-	
-	public ViewShrinkPanelPlugin(String name) {
-		this(new PluginInfo(name));
-	}
+abstract public class ViewShrinkPanelPlugin extends ShrinkPanelPlugin {
 
-	public ViewShrinkPanelPlugin(PluginInfo pinfo) {
+	public ViewShrinkPanelPlugin() {
 		super();
-		//TODO: pinfoDummy hack because ShrinkPanelPlugin needs a PluginInfo with non-null name!
-		shrinkPanel.setTitle(pinfo.name);
-		this.pinfo=pinfo;
 	}
 	
 	@Override
 	public Class<? extends SideContainerPerspective> getPerspectivePluginClass() {
 		return View.class;
-	}
-
-	@Override
-	public PluginInfo getPluginInfo() {
-		//TODO: pinfoDummy hack because ShrinkPanelPlugin needs a PluginInfo with non-null name!
-		return pinfo==null?pinfoDummy:pinfo;
 	}
 
 }
