0df6d96504c8af27f124aea54ac9936bfa433af0
Paul Peters <peters@math.tu-berlin.de>
2009-10-28 20:23:50 +0000

    searches now for a default help file
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@4985 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-plugin/de/jreality/plugin/basic/ViewShrinkPanelPlugin.java
+++ src-plugin/de/jreality/plugin/basic/ViewShrinkPanelPlugin.java
@@ -6,16 +6,23 @@ import de.jtem.jrworkspace.plugin.sidecontainer.SideContainerPerspective;
 import de.jtem.jrworkspace.plugin.sidecontainer.template.ShrinkPanelPlugin;
 
 /** Use this class to get a  {@link ShrinkPanelPlugin} that belongs to the 
- * main {@link Plugin}, i.e., {@link View}, of {@link JRViewer}. Extend this class#
+ * main {@link Plugin}, i.e., {@link View}, of {@link JRViewer}. Extend this class
  * and add it to the JRViewer with {@link JRViewer#registerPlugin()};
  * 
- * Extension of the class is necessary, because only one plugin of each class is allowed.
+ * <p>If a file <code>clazz.getSimpleName</code>.html is found as a resource of <code>clazz</code>, then this is
+ * attached as the help file of this plugin, where <code>clazz</code> is the top level 
+ * enclosing class of the runtime class of this object.
  * 
  * @author G. Paul Peters, 22.07.2009
  *
  */
 abstract public class ViewShrinkPanelPlugin extends ShrinkPanelPlugin {
 
+	protected String helpDocument;
+	protected String helpPath;
+	protected Class<?> helpHandle;
+	private boolean helpResourceChecked=false;
+	
 	public ViewShrinkPanelPlugin() {
 		super();
 	}
@@ -24,5 +31,40 @@ abstract public class ViewShrinkPanelPlugin extends ShrinkPanelPlugin {
 	public Class<? extends SideContainerPerspective> getPerspectivePluginClass() {
 		return View.class;
 	}
+	
+	@Override
+	public String getHelpDocument() {
+		checkHelpResource();
+		return helpDocument==null ? super.getHelpDocument() : helpDocument;
+	}
+
+	@Override
+	public String getHelpPath() {
+		checkHelpResource();
+		return helpPath==null ? super.getHelpPath() : helpPath;
+	}
+
+	@Override
+	public Class<?> getHelpHandle() {
+		checkHelpResource();
+		return helpHandle==null ? super.getHelpHandle() : helpHandle;
+	}
+	
+	
+	private void checkHelpResource() {
+		if (helpResourceChecked) return;
+		Class<?> clazz = getClass();
+		while (null != clazz.getEnclosingClass()) {
+			clazz = clazz.getEnclosingClass();
+		}
+		String filename = clazz.getSimpleName()+".html";
+		if (null!=clazz.getResource(filename)) {
+			helpDocument=filename;
+			helpPath="";
+			helpHandle=clazz;
+		}
+		helpResourceChecked=true;
+	}
+	
 
 }
