e9a5623e1ef7505b7852fa43cce37ad22eeadb05
Stefan Sechelmann <sechel@math.tu-berlin.de>
2010-03-18 10:40:10 +0000

    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5119 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-ui/de/jreality/ui/viewerapp/actions/file/ExportPDF.java
+++ src-ui/de/jreality/ui/viewerapp/actions/file/ExportPDF.java
@@ -46,16 +46,21 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JSpinner;
+import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 
 import de.jreality.io.JrScene;
@@ -64,7 +69,10 @@ import de.jreality.toolsystem.ToolSystem;
 import de.jreality.ui.viewerapp.FileLoaderDialog;
 import de.jreality.ui.viewerapp.actions.AbstractJrAction;
 import de.jreality.writer.pdf.WriterPDF;
-import de.jreality.writer.pdf.WriterPDF.PDF3DPreferences;
+import de.jreality.writer.pdf.WriterPDF.PDF3DGridMode;
+import de.jreality.writer.pdf.WriterPDF.PDF3DLightingScene;
+import de.jreality.writer.pdf.WriterPDF.PDF3DRenderMode;
+import de.jreality.writer.pdf.WriterPDF.PDF3DTool;
 
 /**
  * Saves the current scene.
@@ -77,6 +85,8 @@ public class ExportPDF extends AbstractJrAction {
 		serialVersionUID = 1L;
 	private Viewer 
 		viewer = null;
+	private File
+		userFile = null;
 	private PDFExportAccessory
 		accessory = new PDFExportAccessory();
 
@@ -93,15 +103,20 @@ public class ExportPDF extends AbstractJrAction {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		File file = FileLoaderDialog.selectTargetFile(parentComp, accessory, "pdf",
-				"PDF files");
+		File file = FileLoaderDialog.selectTargetFile(parentComp, accessory, "pdf", "PDF Files");
 		if (file == null)
 			return; // dialog canceled
 
 		try {
 			WriterPDF writer = new WriterPDF();
-			writer.setPreferences(accessory.getPreferences());
+			writer.setTool(accessory.getTool());
 			writer.setSize(accessory.getPDFSize());
+			writer.setLighting(accessory.getLighting());
+			writer.setUserScriptFile(userFile);
+			writer.setRenderMode(accessory.getRenderMode());
+			writer.setGridMode(accessory.getGridMode());
+			writer.setShowGrid(accessory.isUseGrid());
+			writer.setShowAxes(accessory.isUseAxes());
 			JrScene s = new JrScene(viewer.getSceneRoot());
 			s.addPath("cameraPath", viewer.getCameraPath());
 			ToolSystem ts = ToolSystem.toolSystemForViewer(viewer);
@@ -118,30 +133,54 @@ public class ExportPDF extends AbstractJrAction {
 		}
 	}
 
-	private class PDFExportAccessory extends JPanel {
+	private class PDFExportAccessory extends JPanel implements ActionListener {
 
 		private static final long 
 			serialVersionUID = 1L;
 		private JComboBox 
-			prefsCombo = new JComboBox(PDF3DPreferences.values());
+			lightingCombo = new JComboBox(PDF3DLightingScene.values()),
+			toolCombo = new JComboBox(PDF3DTool.values()),
+			renderModeCombo = new JComboBox(PDF3DRenderMode.values()),
+			gridModeCombo = new JComboBox(PDF3DGridMode.values());
 		private SpinnerNumberModel
 			widthModel = new SpinnerNumberModel(800, 1, 10000, 10),
 			heightModel = new SpinnerNumberModel(600, 1, 10000, 10);
 		private JSpinner
 			widthSpinner = new JSpinner(widthModel),
 			heightSpinner = new JSpinner(heightModel);
+		private JTextField
+			userFileField = new JTextField();
+		private JButton
+			browseButton= new JButton("User Script...");
+		private JCheckBox
+			useGridChecker = new JCheckBox("Grid"),
+			useAxesChecker = new JCheckBox("Show Axes");
 		
 		public PDFExportAccessory() {
 			setBorder(BorderFactory.createTitledBorder("PDF Options"));
 			setLayout(new GridBagLayout());
 			GridBagConstraints c = new GridBagConstraints();
-			c.insets = new Insets(0, 5, 0, 0);
+			c.insets = new Insets(2,2,2,2);
 			c.weightx = 1.0;
 			c.fill = GridBagConstraints.HORIZONTAL;
 			c.gridwidth = GridBagConstraints.RELATIVE;
-			add(new JLabel("Tool Set:"), c);
+			add(new JLabel("Tool:"), c);
 			c.gridwidth = GridBagConstraints.REMAINDER;
-			add(prefsCombo, c);
+			add(toolCombo, c);
+			c.gridwidth = GridBagConstraints.RELATIVE;
+			add(new JLabel("Lighting:"), c);
+			c.gridwidth = GridBagConstraints.REMAINDER;
+			add(lightingCombo, c);
+			c.gridwidth = GridBagConstraints.RELATIVE;
+			add(new JLabel("Mode:"), c);
+			c.gridwidth = GridBagConstraints.REMAINDER;
+			add(renderModeCombo, c);
+			c.gridwidth = GridBagConstraints.RELATIVE;
+			add(useGridChecker, c);
+			c.gridwidth = GridBagConstraints.REMAINDER;
+			add(gridModeCombo, c);
+			c.gridwidth = GridBagConstraints.REMAINDER;
+			add(useAxesChecker, c);
 			c.gridwidth = GridBagConstraints.RELATIVE;
 			add(new JLabel("Size:"), c);
 			c.gridwidth = GridBagConstraints.REMAINDER;
@@ -151,18 +190,53 @@ public class ExportPDF extends AbstractJrAction {
 			sizePanel.add(new JLabel("X"));
 			sizePanel.add(heightSpinner);
 			add(sizePanel, c);
+			add(new JSeparator(), c);
+			c.gridwidth = GridBagConstraints.REMAINDER;
+			add(browseButton, c);
+			c.gridwidth = GridBagConstraints.REMAINDER;
+			add(userFileField, c);
+
+			toolCombo.setSelectedItem(PDF3DTool.ROTATE);
+			lightingCombo.setSelectedItem(PDF3DLightingScene.DAY);
+			renderModeCombo.setSelectedItem(PDF3DRenderMode.SOLID);
+			gridModeCombo.setSelectedItem(PDF3DGridMode.GRID_MODE_OFF);
+			
+			userFileField.setEditable(false);
+			browseButton.addActionListener(this);
 		}
 
-		public PDF3DPreferences getPreferences() {
-			PDF3DPreferences prefs = (PDF3DPreferences)prefsCombo.getSelectedItem();
-			if (prefs != null)
-				return prefs;
-			else
-				return PDF3DPreferences.Default;
+		public PDF3DTool getTool() {
+			return (PDF3DTool)toolCombo.getSelectedItem();
 		}
 
 		public Dimension getPDFSize() {
 			return new Dimension(widthModel.getNumber().intValue(), heightModel.getNumber().intValue());
+		}
+		
+		public PDF3DLightingScene getLighting() {
+			return (PDF3DLightingScene)lightingCombo.getSelectedItem();
+		}
+		
+		public PDF3DRenderMode getRenderMode() {
+			return (PDF3DRenderMode)renderModeCombo.getSelectedItem();
+		}
+		
+		public PDF3DGridMode getGridMode() {
+			return (PDF3DGridMode)gridModeCombo.getSelectedItem();
+		}
+		
+		public boolean isUseGrid() {
+			return useGridChecker.isSelected();
+		}
+		
+		public boolean isUseAxes() {
+			return useAxesChecker.isSelected();
+		}
+		
+
+		public void actionPerformed(ActionEvent e) {
+			userFile = FileLoaderDialog.loadFile(parentComp, "js", "Java Script Files");
+			userFileField.setText(userFile.getName());
 		}
 		
 	}
