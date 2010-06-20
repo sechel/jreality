e7b0060455c906a0a8247f270d1ebd45357106f9
Paul Peters <peters@math.tu-berlin.de>
2010-06-20 13:39:05 +0000

    properties for the sliders max/min values added
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5158 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-plugin/de/jreality/plugin/geometry/ParametricSurfaceFactoryPlugin.java
+++ src-plugin/de/jreality/plugin/geometry/ParametricSurfaceFactoryPlugin.java
@@ -4,11 +4,14 @@ import java.awt.GridLayout;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import de.jreality.geometry.ParametricSurfaceFactory;
 import de.jreality.geometry.ParametricSurfaceFactoryCustomizer;
 import de.jreality.plugin.JRViewer;
 import de.jreality.plugin.scene.SceneShrinkPanel;
+import de.jreality.ui.widgets.TextSlider;
 import de.jtem.jrworkspace.plugin.Controller;
 import de.jtem.jrworkspace.plugin.Plugin;
 import de.jtem.jrworkspace.plugin.PluginInfo;
@@ -24,13 +27,15 @@ public class ParametricSurfaceFactoryPlugin extends SceneShrinkPanel {
 	private final ParametricSurfaceFactoryCustomizer psfCustomizer;
 	private final ParametricSurfaceFactory psf;
 	private final ArrayList<PropertyDescriptor> properties = new ArrayList<PropertyDescriptor>();
+	private final List<TextSlider<Integer>> intSliders = new ArrayList<TextSlider<Integer>>();
+	private final List<TextSlider<Double>> dblSliders = new ArrayList<TextSlider<Double>>();
 
 	public ParametricSurfaceFactoryPlugin(ParametricSurfaceFactory psf) {
 		this.psf = psf;
 		psfCustomizer = new ParametricSurfaceFactoryCustomizer(psf);
 		psfCustomizer.updateGuiFromFactory();
 		initPanel();
-		readProperties();
+		readPropertiesAndSliders();
 	}
 
 	private void initPanel() {
@@ -38,10 +43,14 @@ public class ParametricSurfaceFactoryPlugin extends SceneShrinkPanel {
 		getShrinkPanel().add(psfCustomizer);
 	}
 	
-	private void readProperties() {
-		properties.addAll(psfCustomizer.getIntegerSliders().keySet());
-		properties.addAll(psfCustomizer.getDoubleSliders().keySet());
+	private void readPropertiesAndSliders() {
+		final Map<PropertyDescriptor, TextSlider<Integer>> integerSliders = psfCustomizer.getIntegerSliders();
+		final Map<PropertyDescriptor, TextSlider<Double>> doubleSliders = psfCustomizer.getDoubleSliders();
+		properties.addAll(integerSliders.keySet());
+		properties.addAll(doubleSliders.keySet());
 		properties.addAll(psfCustomizer.getJToggleButtons().keySet());
+		intSliders.addAll(integerSliders.values());
+		dblSliders.addAll(doubleSliders.values());
 	}
 
 	@Override
@@ -60,15 +69,32 @@ public class ParametricSurfaceFactoryPlugin extends SceneShrinkPanel {
 			String name = property.getName();
 			Method readMethod = property.getReadMethod();
 			Method writeMethod = property.getWriteMethod();
-			writeMethod.invoke(psf, c.getProperty(this.getClass(), name, readMethod.invoke(psf)));
+			writeMethod.invoke(psf, c.getProperty(getClass(), name, readMethod.invoke(psf)));
 		}
 		psfCustomizer.updateGuiFromFactory();
+		
+		for (TextSlider<Integer> slider: intSliders) {
+			slider.setMax(c.getProperty(getClass(), slider.getName() + "Max", slider.getMax()));
+			slider.setMin(c.getProperty(getClass(), slider.getName() + "Min", slider.getMin()));
+		}
+		for (TextSlider<Double> slider: dblSliders) {
+			slider.setMax(c.getProperty(getClass(), slider.getName() + "Max", slider.getMax()));
+			slider.setMin(c.getProperty(getClass(), slider.getName() + "Min", slider.getMin()));
+		}
 	}
 
 	@Override
 	public void storeStates(Controller c) throws Exception {
 		super.storeStates(c);
 		
+		for (TextSlider<Integer> slider: intSliders) {
+			c.storeProperty(getClass(), slider.getName() + "Max", slider.getMax());
+			c.storeProperty(getClass(), slider.getName() + "Min", slider.getMin());
+		}
+		for (TextSlider<Double> slider: dblSliders) {
+			c.storeProperty(getClass(), slider.getName() + "Max", slider.getMax());
+			c.storeProperty(getClass(), slider.getName() + "Min", slider.getMin());
+		}
 		for (PropertyDescriptor property : properties) {
 			String name = property.getName();
 			Method readMethod = property.getReadMethod();
