171a75ca024fbe2bd129a43b3da00fea23b65a21
Stefan Sechelmann <sechel@math.tu-berlin.de>
2011-03-02 19:42:42 +0000

    clean window in SimpleViewer
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5303 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/viewer/SimpleViewer.java
+++ src-tutorial/de/jreality/tutorial/viewer/SimpleViewer.java
@@ -1,12 +1,19 @@
 package de.jreality.tutorial.viewer;
 
+import static de.jreality.plugin.icon.ImageHook.getIcon;
+
+import java.awt.Component;
+
+import javax.swing.JFrame;
+
 import de.jreality.geometry.Primitives;
 import de.jreality.plugin.JRViewer;
+import de.jreality.plugin.basic.View;
 import de.jreality.scene.SceneGraphComponent;
-import de.jreality.ui.viewerapp.ViewerApp;
+import de.jreality.scene.Viewer;
 
 /**
- * A simple class showing how to use a {@link ViewerApp} to get a viewing component 
+ * A simple class showing how to use a {@link JRViewer} to get a viewing component 
  * which is then packed into another frame.
  * @author Charles Gunn
  *
@@ -16,7 +23,21 @@ public class SimpleViewer {
 	public static void main(String[] args)	{
 		SceneGraphComponent world = new SceneGraphComponent();
 		world.setGeometry(Primitives.sharedIcosahedron);
-		JRViewer.display(world);
+		View.setTitle("The Icosahedron");
+		View.setIcon(getIcon("color_swatch.png"));
+		JRViewer v = JRViewer.createJRViewer(world);
+		
+		if (true) {
+			v.startup();
+		} else {
+			v.startupLocal();
+			Viewer viewer = v.getViewer();
+	        JFrame f = new JFrame();
+	        f.getContentPane().add((Component)viewer.getViewingComponent());
+	        f.setSize(512, 512);
+	        f.validate();
+	        f.setVisible(true);
+		}
 	}
  
 }
