8e0e2dbb355e6161e1d0bf22aad028d7434e22c6
Stefan Sechelmann <sechel@math.tu-berlin.de>
2011-03-02 19:30:39 +0000

    clean window in SimpleViewer
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5302 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/viewer/SimpleViewer.java
+++ src-tutorial/de/jreality/tutorial/viewer/SimpleViewer.java
@@ -1,13 +1,8 @@
 package de.jreality.tutorial.viewer;
 
-import java.awt.Component;
-
-import javax.swing.JFrame;
-
 import de.jreality.geometry.Primitives;
 import de.jreality.plugin.JRViewer;
 import de.jreality.scene.SceneGraphComponent;
-import de.jreality.scene.Viewer;
 import de.jreality.ui.viewerapp.ViewerApp;
 
 /**
@@ -21,17 +16,7 @@ public class SimpleViewer {
 	public static void main(String[] args)	{
 		SceneGraphComponent world = new SceneGraphComponent();
 		world.setGeometry(Primitives.sharedIcosahedron);
-		// if you don't want ViewerApp to provide lights and camera and some tools
-		// then use the constructor 	
-		// public ViewerApp(SceneGraphComponent root, SceneGraphPath cameraPath, SceneGraphPath emptyPick, SceneGraphPath avatar) {
-
-		Viewer v = JRViewer.display(world);
-        JFrame f = new JFrame();
-        f.getContentPane().add((Component) v.getViewingComponent());
-
-        f.setSize(512, 512);
-        f.validate();
-        f.setVisible(true);
+		JRViewer.display(world);
 	}
  
 }
