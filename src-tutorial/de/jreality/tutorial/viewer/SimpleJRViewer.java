771d5c76f6632776c2687e8c60e9879727d18575
Charles Gunn <gunn@math.tu-berlin.de>
2012-09-03 10:39:01 +0000

    change name of de.jreality.jogl.Viewer to de.jreality.jogl.JOGLViewer (leave a trivial subclass with old name to avoid broken code), add a Timer.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5528 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/viewer/SimpleJRViewer.java
+++ src-tutorial/de/jreality/tutorial/viewer/SimpleJRViewer.java
@@ -1,10 +1,16 @@
 package de.jreality.tutorial.viewer;
 
+import java.io.File;
+import java.io.IOException;
+
 import de.jreality.plugin.JRViewer;
+import de.jreality.reader.Readers;
+import de.jreality.scene.SceneGraphComponent;
 
 public class SimpleJRViewer {
-	public static void main(String[] args) {
-		JRViewer.display(null);
+	public static void main(String[] args) throws IOException {
+		SceneGraphComponent read = Readers.read(new File("/homes/geometer/gunn/Downloads/face.stl"));
+		JRViewer.display(read);
 	}
 
 }
