f2599d4c3e5445e4a8a942881488ccddb44d6aa8
Stefan Sechelmann <sechel@math.tu-berlin.de>
2012-09-05 08:48:08 +0000

    fixed vertex edge and face show tool mouse bug
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5531 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-plugin/de/jreality/plugin/content/ContentAppearance.java
+++ src-plugin/de/jreality/plugin/content/ContentAppearance.java
@@ -89,7 +89,9 @@ public class ContentAppearance extends SceneShrinkPanel implements ColorPickerMo
 		
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (e.getModifiers() != 0) return;
+				boolean wasMouceClick = (e.getModifiers() & ActionEvent.MOUSE_EVENT_MASK) != 0;
+				long keyMods = e.getModifiers() & ~ActionEvent.MOUSE_EVENT_MASK;
+				if (!wasMouceClick && keyMods != 0) return;
 				appearanceInspector.setShowPoints(showVerticesAction.isSelected());				
 			}
 		},
@@ -104,7 +106,9 @@ public class ContentAppearance extends SceneShrinkPanel implements ColorPickerMo
 		
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (e.getModifiers() != 0) return;
+				boolean wasMouceClick = (e.getModifiers() & ActionEvent.MOUSE_EVENT_MASK) != 0;
+				long keyMods = e.getModifiers() & ~ActionEvent.MOUSE_EVENT_MASK;
+				if (!wasMouceClick && keyMods != 0) return;
 				appearanceInspector.setShowLines(showEdgesAction.isSelected());				
 			}
 		},
@@ -119,7 +123,9 @@ public class ContentAppearance extends SceneShrinkPanel implements ColorPickerMo
 		
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (e.getModifiers() != 0) return;
+				boolean wasMouceClick = (e.getModifiers() & ActionEvent.MOUSE_EVENT_MASK) != 0;
+				long keyMods = e.getModifiers() & ~ActionEvent.MOUSE_EVENT_MASK;
+				if (!wasMouceClick && keyMods != 0) return;
 				appearanceInspector.setShowFaces(showFacesAction.isSelected());				
 			}
 		};
