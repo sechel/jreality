a9f3bca23a477c06e1939cdd9c02dddc83da79c4
Benjamin Kutschan <kutschan@math.tu-berlin.de>
2012-12-14 11:20:44 +0000

    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5659 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ test-backends-share/light/LightTest.java
@@ -0,0 +1,53 @@
+package light;
+
+import java.awt.Color;
+
+import de.jreality.math.MatrixBuilder;
+import de.jreality.plugin.JRViewer;
+import de.jreality.plugin.scene.Terrain;
+import de.jreality.scene.DirectionalLight;
+import de.jreality.scene.Light;
+import de.jreality.scene.PointLight;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.scene.Transformation;
+
+public class LightTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SceneGraphComponent cmp = new SceneGraphComponent();
+		
+		
+		//a directional light
+		DirectionalLight l1 = new DirectionalLight();
+		l1.setGlobal(true);
+		l1.setColor(Color.BLUE);
+		l1.setIntensity(0.5);
+		SceneGraphComponent light1 = new SceneGraphComponent();
+		light1.setLight(l1);
+		MatrixBuilder.euclidean().rotate(4, new double[]{1, 0, 0, 0}).assignTo(light1);
+		cmp.addChild(light1);
+		
+		//a point light with attenuation
+		PointLight l2 = new PointLight();
+		l2.setGlobal(true);
+		l2.setColor(Color.RED);
+		l2.setIntensity(0.5);
+		l2.setFalloff(0, 0, 1);
+		SceneGraphComponent light2 = new SceneGraphComponent();
+		light2.setLight(l2);
+		MatrixBuilder.euclidean().translate(new double[]{1, 1, 1, 1}).assignTo(light2);
+		cmp.addChild(light2);
+		
+		
+		JRViewer vr = new JRViewer();
+		vr.addVRSupport();
+		vr.addBasicUI();
+		vr.setContent(cmp);
+		vr.startup();
+	}
+
+}
