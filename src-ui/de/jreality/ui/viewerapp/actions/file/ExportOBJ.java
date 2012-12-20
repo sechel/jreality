56b0ff484f3cfcc5abc4d15fc6c4d3e0fb420c95
Charles Gunn <gunn@math.tu-berlin.de>
2012-12-20 17:45:03 +0000

    add ExportOBJ option to Export menu
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5685 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

---
+++ src-ui/de/jreality/ui/viewerapp/actions/file/ExportOBJ.java
@@ -0,0 +1,61 @@
+package de.jreality.ui.viewerapp.actions.file;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import javax.swing.JComponent;
+
+import de.jreality.geometry.GeometryMergeFactory;
+import de.jreality.geometry.IndexedFaceSetUtility;
+import de.jreality.geometry.RemoveDuplicateInfo;
+import de.jreality.scene.IndexedFaceSet;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.scene.Viewer;
+import de.jreality.scene.data.Attribute;
+import de.jreality.tutorial.geom.GeometryMergeExample;
+import de.jreality.ui.viewerapp.FileLoaderDialog;
+import de.jreality.ui.viewerapp.actions.AbstractJrAction;
+import de.jreality.util.SceneGraphUtility;
+import de.jreality.writer.WriterOBJ;
+import de.jreality.writer.WriterSTL;
+
+public class ExportOBJ extends AbstractJrAction {
+
+	private Viewer viewer;
+
+	private JComponent options;
+
+	public ExportOBJ(String name, Viewer viewer, Component parentComp) {
+		super(name, parentComp);
+
+		if (viewer == null)
+			throw new IllegalArgumentException("Viewer is null!");
+		this.viewer = viewer;
+
+		setShortDescription("Export the current scene as OBJ file");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		File file = FileLoaderDialog.selectTargetFile(parentComp, options, "obj", "OBJ Files");
+		if (file == null) return;  //dialog cancelled
+
+		try {
+//			WriterVRML.write(viewer.getSceneRoot(), new FileOutputStream(file));
+			// First have to combine all geometries into a single IFS
+	        GeometryMergeFactory mergeFact= new GeometryMergeFactory();
+	        IndexedFaceSet result=mergeFact.mergeGeometrySets(viewer.getSceneRoot());
+	        result = (IndexedFaceSet) RemoveDuplicateInfo.removeDuplicateVertices(result, (Attribute[]) null);
+	        boolean orient = IndexedFaceSetUtility.makeConsistentOrientation(result);
+	        System.err.println("Export OBJ: oriented = "+orient);
+	        IndexedFaceSetUtility.calculateAndSetFaceNormals(result);
+	        IndexedFaceSetUtility.calculateAndSetVertexNormals(result);
+			WriterOBJ.write(result, new FileOutputStream(file));
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}			
+	}
+}
