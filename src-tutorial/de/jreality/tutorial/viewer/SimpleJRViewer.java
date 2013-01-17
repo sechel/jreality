2d67d359c79304cd3bb2b20b29705dde36497d3a
Charles Gunn <gunn@math.tu-berlin.de>
2013-01-17 11:54:47 +0000

    fix bug in reading non-existent file
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5715 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-tutorial/de/jreality/tutorial/viewer/SimpleJRViewer.java
+++ src-tutorial/de/jreality/tutorial/viewer/SimpleJRViewer.java
@@ -1,15 +1,14 @@
 package de.jreality.tutorial.viewer;
 
-import java.io.File;
 import java.io.IOException;
 
+import de.jreality.geometry.Primitives;
 import de.jreality.plugin.JRViewer;
-import de.jreality.reader.Readers;
-import de.jreality.scene.SceneGraphComponent;
+import de.jreality.scene.SceneGraphNode;
 
 public class SimpleJRViewer {
 	public static void main(String[] args) throws IOException {
-		SceneGraphComponent read = Readers.read(new File("/homes/geometer/gunn/Downloads/face.stl"));
+		SceneGraphNode read = Primitives.cube(true); //Readers.read(SimpleJRViewer.class.getResource(face.stl"));
 		JRViewer.display(read);
 	}
 
