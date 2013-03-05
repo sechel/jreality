3583ab24277dcf8685d007c5a75cb6c350bf6f3f
Benjamin Kutschan <kutschan@math.tu-berlin.de>
2013-03-05 07:40:26 +0000

    - maybe fixed info overlay error on Mac
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5806 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-jogl/de/jreality/jogl/plugin/InfoOverlay.java
+++ src-jogl/de/jreality/jogl/plugin/InfoOverlay.java
@@ -101,87 +101,92 @@ public class InfoOverlay implements GLEventListener {
 			return;
 
 		// JOGLConfiguration.theLog.log(Level.FINE,"In info display");
+		try{
+			GL2 gl = glDrawable.getGL().getGL2();
+			GLU glu = new GLU();
 
-		GL2 gl = glDrawable.getGL().getGL2();
-		GLU glu = new GLU();
+			// Store old matrices
+			gl.glMatrixMode(GL2.GL_MODELVIEW);
+			gl.glPushMatrix();
+			gl.glLoadIdentity();
+			gl.glMatrixMode(GL2.GL_PROJECTION);
+			gl.glPushMatrix();
+			gl.glLoadIdentity();
 
-		// Store old matrices
-		gl.glMatrixMode(GL2.GL_MODELVIEW);
-		gl.glPushMatrix();
-		gl.glLoadIdentity();
-		gl.glMatrixMode(GL2.GL_PROJECTION);
-		gl.glPushMatrix();
-		gl.glLoadIdentity();
+			Dimension size = new Dimension(glDrawable.getWidth(),
+					glDrawable.getHeight());
+			gl.glViewport(0, 0, size.width, size.height);
 
-		Dimension size = new Dimension(glDrawable.getWidth(),
-				glDrawable.getHeight());
-		gl.glViewport(0, 0, size.width, size.height);
+			// Store enabled state and disable lighting, texture mapping and the
+			// depth buffer
+			gl.glPushAttrib(GL2.GL_ENABLE_BIT);
+			gl.glDisable(GL.GL_BLEND);
+			gl.glDisable(GL2.GL_LIGHTING);
+			gl.glDisable(GL.GL_TEXTURE_2D);
+			gl.glDisable(GL.GL_DEPTH_TEST);
+			for (int i = 0; i < 6; ++i)
+				gl.glDisable(i + GL2.GL_CLIP_PLANE0);
 
-		// Store enabled state and disable lighting, texture mapping and the
-		// depth buffer
-		gl.glPushAttrib(GL2.GL_ENABLE_BIT);
-		gl.glDisable(GL.GL_BLEND);
-		gl.glDisable(GL2.GL_LIGHTING);
-		gl.glDisable(GL.GL_TEXTURE_2D);
-		gl.glDisable(GL.GL_DEPTH_TEST);
-		for (int i = 0; i < 6; ++i)
-			gl.glDisable(i + GL2.GL_CLIP_PLANE0);
-
-		// Retrieve the current viewport and switch to orthographic mode
-		int viewPort[] = new int[4];
-		gl.glGetIntegerv(GL.GL_VIEWPORT, viewPort, 0);
-		try {
-			glu.gluOrtho2D(0, viewPort[2], viewPort[3], 0);
-		} catch (GLException e) {
-			// e.printStackTrace();
-			return;
-		}
-
-		// Render the text
-		gl.glColor3f(1, 1, 1);
-
-		int offset = (position == LOWER_LEFT || position == LOWER_RIGHT) ? -OFFSET
-				: OFFSET;
-		int x = (position == LOWER_RIGHT || position == UPPER_RIGHT) ? viewPort[2] / 2
-				: OFFSET;
-		x += INDENT;
-		int maxx = 0;
-		int y = (position == LOWER_LEFT || position == LOWER_RIGHT) ? (viewPort[3])
-				- CHAR_HEIGHT
-				: CHAR_HEIGHT;
-		y += offset;
-
-		if (info != null && info.size() > 0) {
-			gl.glRasterPos2i(x, y);
-			// glut.glutBitmapString(gl, GLUT.BITMAP_HELVETICA_12, "Info");
-			// maxx = Math.max(maxx, OFFSET +
-			// glut.glutBitmapLength(GLUT.BITMAP_HELVETICA_12,
-			// KEYBOARD_CONTROLS));
-
-			y += offset;
-			x += INDENT;
-			for (int i = 0; i < info.size(); i++) {
-				gl.glRasterPos2f(x, y);
-				String text = null;
-				synchronized (info) {
-					text = (String) info.get(i);
-				}
-				glut.glutBitmapString(GLUT.BITMAP_HELVETICA_12, text);
-				maxx = Math.max(
-						maxx,
-						OFFSET
-								+ glut.glutBitmapLength(
-										GLUT.BITMAP_HELVETICA_12, text));
-				y += offset;
+			// Retrieve the current viewport and switch to orthographic mode
+			int viewPort[] = new int[4];
+			gl.glGetIntegerv(GL.GL_VIEWPORT, viewPort, 0);
+			try {
+				glu.gluOrtho2D(0, viewPort[2], viewPort[3], 0);
+			} catch (GLException e) {
+				// e.printStackTrace();
+				return;
 			}
+
+			// Render the text
+			gl.glColor3f(1, 1, 1);
+
+			int offset = (position == LOWER_LEFT || position == LOWER_RIGHT) ? -OFFSET
+					: OFFSET;
+			int x = (position == LOWER_RIGHT || position == UPPER_RIGHT) ? viewPort[2] / 2
+					: OFFSET;
+			x += INDENT;
+			int maxx = 0;
+			int y = (position == LOWER_LEFT || position == LOWER_RIGHT) ? (viewPort[3])
+					- CHAR_HEIGHT
+					: CHAR_HEIGHT;
+			y += offset;
+
+			if (info != null && info.size() > 0) {
+				gl.glRasterPos2i(x, y);
+				// glut.glutBitmapString(gl, GLUT.BITMAP_HELVETICA_12, "Info");
+				// maxx = Math.max(maxx, OFFSET +
+				// glut.glutBitmapLength(GLUT.BITMAP_HELVETICA_12,
+				// KEYBOARD_CONTROLS));
+
+				y += offset;
+				x += INDENT;
+				for (int i = 0; i < info.size(); i++) {
+					gl.glRasterPos2f(x, y);
+					String text = null;
+					synchronized (info) {
+						text = (String) info.get(i);
+					}
+					glut.glutBitmapString(GLUT.BITMAP_HELVETICA_12, text);
+					maxx = Math.max(
+							maxx,
+							OFFSET
+									+ glut.glutBitmapLength(
+											GLUT.BITMAP_HELVETICA_12, text));
+					y += offset;
+				}
+			}
+
+			gl.glPopAttrib();
+
+			// Restore old matrices
+			gl.glPopMatrix();
+			gl.glMatrixMode(GL2.GL_MODELVIEW);
+			gl.glPopMatrix();
+		}catch(Exception e){
+			System.out.println("No Info InfoOverlay in jogl3 backend, because cannot mix GL2 and GL3 on your system, more exactly: " + e.getMessage() + 
+					"This can be fixed by writing a seperate InfoOverlay implementation for both jogl and jogl3. Basically" +
+					"there must not be openGL code outside the backend to avoid this problem");
 		}
-
-		gl.glPopAttrib();
-
-		// Restore old matrices
-		gl.glPopMatrix();
-		gl.glMatrixMode(GL2.GL_MODELVIEW);
-		gl.glPopMatrix();
 	}
 
 	public void displayChanged(GLAutoDrawable glDrawable, boolean b, boolean b1) {
