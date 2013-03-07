7ab10f07ec40fdadd4bb443a1f21e77923bb2bae
Benjamin Kutschan <kutschan@math.tu-berlin.de>
2013-03-07 10:30:46 +0000

    lineDiffuseColor rather than diffuseColor
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5818 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-jogl3/de/jreality/jogl3/glsl/nontransp/edge.f
+++ src-jogl3/de/jreality/jogl3/glsl/nontransp/edge.f
@@ -3,7 +3,7 @@
 #version 330
 
 out vec4 glFragColor;
-uniform vec4 diffuseColor;
+uniform vec4 lineDiffuseColor;
 
 in vec4 edgeColor;
 in vec4 vertexColor;
@@ -13,7 +13,7 @@ uniform int has_edge_colors;
 
 void main(void)
 {
-	glFragColor = diffuseColor;
+	glFragColor = lineDiffuseColor;
 	
 	if(has_edge_colors == 1)
 		glFragColor = edgeColor;
