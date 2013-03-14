b5db4f66dcc1d66bc0dbdcaebc63343b932cfa99
Benjamin Kutschan <kutschan@math.tu-berlin.de>
2013-03-14 09:31:55 +0000

    - fixed a bug in jogl3 where tube radius became 0 when one of the endpoints is at the origin, which was especially dangerous when radii_world_coordinates was false. - normals are still wrong when radii_world_coordinates is false and matrix is not isotropic. But this is not important.
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5844 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-jogl3/de/jreality/jogl3/glsl/nontransp/tubeEdge.v
+++ src-jogl3/de/jreality/jogl3/glsl/nontransp/tubeEdge.v
@@ -60,8 +60,8 @@ void main(void)
 	mat4 trafo;
 	vec4 scaledTubeCoords;
 	//here is a serious problem if v1 or v2 become 0. then newZ becomes 0.
-	//SOLUTION: WORK IN PROJECTIVE SPACE!
 	if(lineShader_radiiWorldCoordinates == 1){
+		//when v1 or v2 are at the origin, i.e. the camera center, then the line is not drawn
 		vec3 v1 = (modelview*vertex_coordinates).xyz;
 		vec3 v2 = (modelview*_vertex_coordinates).xyz;
 		vec3 newZ = normalize(v1-(v2-v1)*dot(v1, v2-v1)/dot(v2-v1, v2-v1));
@@ -76,10 +76,22 @@ void main(void)
 		camSpaceNormal = normalize(rotation*Normal);
 		camSpaceCoord = trafo*scaledTubeCoords;
 	}else{
+		//here we have to do something differently, because lines must not be rotated
 		vec3 v1 = (vertex_coordinates).xyz;
 		vec3 v2 = (_vertex_coordinates).xyz;
-		vec3 newZ = normalize(v1-(v2-v1)*dot(v1, v2-v1)/dot(v2-v1, v2-v1));
-		vec3 newY = normalize(cross(normalize(v2-v1), newZ));
+		
+		vec3 newX = v2-v1;
+		vec3 Y;
+		if(abs(newX.x) < abs(newX.y)){
+			Y = vec3(1, 0, 0);
+		}else{
+			//use y = (0, 1, 0) as new basis element and gramschmidt it.
+			Y = vec3(0, 1, 0);
+		}
+		vec3 normalX = normalize(v2-v1);
+		vec3 newZ = Y-newX*dot(Y, normalX);
+		vec3 newY = normalize(cross(normalX, newZ));
+		
 		trafo = mat4(v2-v1, 0, newY, 0, newZ, 0, v1, 1);
 		scaledTubeCoords = vec4(tube_coords.x, tube_coords.y*lineShader_tubeRadius*relRad, tube_coords.z*lineShader_tubeRadius*relRad, 1);
 		
@@ -90,7 +102,7 @@ void main(void)
 		vec3 Normal = vec3(0, tube_coords.yz);
 		mat3 rotation = mat3(vec3(together[0][0], together[0][1], together[0][2]), vec3(together[1][0], together[1][1], together[1][2]), vec3(together[2][0], together[2][1], together[2][2]));
 		camSpaceNormal = normalize(rotation*Normal);
-		camSpaceCoord = modelview*trafo*scaledTubeCoords;
+		camSpaceCoord = together*scaledTubeCoords;
 	}
 	//gl_Position = gl_Position / gl_Position.w;
 	//gl_Position += _jitter;
