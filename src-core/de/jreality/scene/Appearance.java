801edf16ee798a0bbab2372af280ddf70dedb8bc
Andre Heydt <heydt@math.tu-berlin.de>
2013-07-22 17:43:16 +0000

    minor format change
    git-svn-id: http://fs.math.tu-berlin.de:8000/svn/jreality/trunk@5900 6a571d9c-c4e9-67b8-d2a0-cd0d85a3e679

--- src-core/de/jreality/scene/Appearance.java
+++ src-core/de/jreality/scene/Appearance.java
@@ -69,160 +69,163 @@ import de.jreality.scene.event.AppearanceListener;
  * TODO: fire ONE single event that reports all changed attributes
  * @author Unknown
  */
-public class Appearance extends SceneGraphNode
-{
-  // steffen: i changed these to objects otherwise an effective appearance can never return them...
-  // obsolete now: it would be convenient if these were no inner classes but just objects....
-  public static final Object DEFAULT = new Object();
-  public static final Object INHERITED = new Object();
+public class Appearance extends SceneGraphNode {
+	// steffen: i changed these to objects otherwise an effective appearance can
+	// never return them...
+	// obsolete now: it would be convenient if these were no inner classes but
+	// just objects....
+	public static final Object DEFAULT = new Object();
+	public static final Object INHERITED = new Object();
 
-  private transient AppearanceEventMulticaster appearanceListener = new AppearanceEventMulticaster();
-  private HashMap<String, Object> attributes=new HashMap<String, Object>();
-  private Set<String> storedAttributes = Collections.unmodifiableSet(attributes.keySet());
-  
-  private transient HashMap<String, Object> changedAttributes=new HashMap<String, Object>();
-  
-  private static int UNNAMED_ID;
+	private transient AppearanceEventMulticaster appearanceListener = new AppearanceEventMulticaster();
+	private HashMap<String, Object> attributes = new HashMap<String, Object>();
+	private Set<String> storedAttributes = Collections
+			.unmodifiableSet(attributes.keySet());
 
-  public Appearance(String name) {
-	  super(name);
-  }
-  
-  public Appearance() {
-	  super("app "+(UNNAMED_ID++));
-  }
-  
-  public Object getAttribute(String key)
-  {
-    startReader();
-    try {
-      Object aa=attributes.get(key);
-      return aa!=null? aa : INHERITED;
-    } finally {
-      finishReader();
-    }
-  }
+	private transient HashMap<String, Object> changedAttributes = new HashMap<String, Object>();
 
-  public HashMap getAttributes()	{
-	  return attributes;
-  }
-  
-  public String toString() {
-	  return attributes.toString();
-  }
-  
-  public Object getAttribute(String key, Class type)
-  {
-    startReader();
-    try {
-      Object val=getAttribute(key);
-      if(val==DEFAULT||type.isInstance(val)) return val;
-      return INHERITED;
-    } finally {
-      finishReader();
-    }
-  }
+	private static int UNNAMED_ID;
 
-  public void setAttribute(String key, Object value)
-  {
-    setAttribute(key, value, value.getClass());
-  }
+	public Appearance(String name) {
+		super(name);
+	}
 
-  public void setAttribute(String key, Object value, Class declaredType)
-  {
-    checkReadOnly();
-    startWriter();
-    try {
-      Object old=null;
-      
-      if(declaredType==null||value==null) throw new NullPointerException();
-      if(value==INHERITED)
-      {
-        old=attributes.remove(key);
-      }
-      else
-      {
-        // TODO: is this check ok? (cheap enough?)
-        if (AttributeEntity.class.isAssignableFrom(value.getClass()))
-          throw new IllegalArgumentException("no proxies allowed");
-        old=attributes.put(key, value);
-      }
-      // TODO: if (!new.equals(old)) ... ???
-      if (old != value ) fireAppearanceChanged(key, old);
-    } finally {
-      finishWriter();
-    }
-  }
-  public void setAttribute(String key, double value)
-  {
-    setAttribute(key, new Double(value));
-  }
-  public void setAttribute(String key, float value)
-  {
-    setAttribute(key, new Float(value));
-  }
-  public void setAttribute(String key, int value)
-  {
-    setAttribute(key, new Integer(value));
-  }
-  public void setAttribute(String key, long value)
-  {
-    setAttribute(key, new Long(value));
-  }
-  public void setAttribute(String key, boolean value)
-  {
-    setAttribute(key, Boolean.valueOf(value));
-  }
-  public void setAttribute(String key, char value)
-  {
-    setAttribute(key, new Character(value));
-  }
+	public Appearance() {
+		super("app " + (UNNAMED_ID++));
+	}
 
-  public void addAppearanceListener(AppearanceListener listener) {
-     startReader();
-	   appearanceListener.add(listener);
-     finishReader();
-   }
-   public void removeAppearanceListener(AppearanceListener listener) {
-     startReader();
-	   appearanceListener.remove(listener);
-     finishReader();
-   }
+	public Object getAttribute(String key) {
+		startReader();
+		try {
+			Object aa = attributes.get(key);
+			return aa != null ? aa : INHERITED;
+		} finally {
+			finishReader();
+		}
+	}
 
- /**
-	* Tell the outside world that this appearance has changed.
-	*/
-   protected void writingFinished() {
-     try {
-       for (Entry<String, Object> e : changedAttributes.entrySet() ) {
-         appearanceListener.appearanceChanged(new AppearanceEvent(this, e.getKey(), e.getValue()));
-       }
-     } finally {
-       changedAttributes.clear();
-     }
-   };
-   
-   protected void fireAppearanceChanged(String key, Object old) {
-	 	 if(appearanceListener != null) changedAttributes.put(key, old);
-   }
+	public HashMap getAttributes() {
+		return attributes;
+	}
 
-   public Set getStoredAttributes() {
-     return storedAttributes;
-   }
-   
-  public void accept(SceneGraphVisitor v) {
-    startReader();
-    try {
-      v.visit(this);
-    } finally {
-      finishReader();
-    }
-  }
-  static void superAccept(Appearance a, SceneGraphVisitor v) {
-    a.superAccept(v);
-  }
-  private void superAccept(SceneGraphVisitor v) {
-    super.accept(v);
-  }
+	public String toString() {
+		return attributes.toString();
+	}
+
+	public Object getAttribute(String key, Class type) {
+		startReader();
+		try {
+			Object val = getAttribute(key);
+			if (val == DEFAULT || type.isInstance(val))
+				return val;
+			return INHERITED;
+		} finally {
+			finishReader();
+		}
+	}
+
+	public void setAttribute(String key, Object value) {
+		setAttribute(key, value, value.getClass());
+	}
+
+	public void setAttribute(String key, Object value, Class declaredType) {
+		checkReadOnly();
+		startWriter();
+		try {
+			Object old = null;
+
+			if (declaredType == null || value == null)
+				throw new NullPointerException();
+			if (value == INHERITED) {
+				old = attributes.remove(key);
+			} else {
+				// TODO: is this check ok? (cheap enough?)
+				if (AttributeEntity.class.isAssignableFrom(value.getClass()))
+					throw new IllegalArgumentException("no proxies allowed");
+				old = attributes.put(key, value);
+			}
+			// TODO: if (!new.equals(old)) ... ???
+			if (old != value)
+				fireAppearanceChanged(key, old);
+		} finally {
+			finishWriter();
+		}
+	}
+
+	public void setAttribute(String key, double value) {
+		setAttribute(key, new Double(value));
+	}
+
+	public void setAttribute(String key, float value) {
+		setAttribute(key, new Float(value));
+	}
+
+	public void setAttribute(String key, int value) {
+		setAttribute(key, new Integer(value));
+	}
+
+	public void setAttribute(String key, long value) {
+		setAttribute(key, new Long(value));
+	}
+
+	public void setAttribute(String key, boolean value) {
+		setAttribute(key, Boolean.valueOf(value));
+	}
+
+	public void setAttribute(String key, char value) {
+		setAttribute(key, new Character(value));
+	}
+
+	public void addAppearanceListener(AppearanceListener listener) {
+		startReader();
+		appearanceListener.add(listener);
+		finishReader();
+	}
+
+	public void removeAppearanceListener(AppearanceListener listener) {
+		startReader();
+		appearanceListener.remove(listener);
+		finishReader();
+	}
+
+	/**
+	 * Tell the outside world that this appearance has changed.
+	 */
+	protected void writingFinished() {
+		try {
+			for (Entry<String, Object> e : changedAttributes.entrySet()) {
+				appearanceListener.appearanceChanged(new AppearanceEvent(this,
+						e.getKey(), e.getValue()));
+			}
+		} finally {
+			changedAttributes.clear();
+		}
+	};
+
+	protected void fireAppearanceChanged(String key, Object old) {
+		if (appearanceListener != null)
+			changedAttributes.put(key, old);
+	}
+
+	public Set getStoredAttributes() {
+		return storedAttributes;
+	}
+
+	public void accept(SceneGraphVisitor v) {
+		startReader();
+		try {
+			v.visit(this);
+		} finally {
+			finishReader();
+		}
+	}
+
+	static void superAccept(Appearance a, SceneGraphVisitor v) {
+		a.superAccept(v);
+	}
+
+	private void superAccept(SceneGraphVisitor v) {
+		super.accept(v);
+	}
 
 }
