3ee07efbe0efe84944193611660467a3d5a684f0
Benjamin Kutschan <kutschan@math.tu-berlin.de>
2013-09-18 13:59:58 +0200

    - cleaned up the GLShadingLangExample01NewBackend

--- src-tutorial/de/jreality/tutorial/app/GLShadingLangExample01NewBackend.java
+++ src-tutorial/de/jreality/tutorial/app/GLShadingLangExample01NewBackend.java
@@ -26,42 +26,31 @@ public class GLShadingLangExample01NewBackend {
    			ShaderUtility.createDefaultGeometryShader(ap, true);
 		dgs.setShowLines(false);
 		dgs.setShowPoints(false);
-		//dgs.createPolygonShader("glsl");
-		//GlslProgram brickProg = null;		
+		
 		world.setGeometry(SphereUtility.tessellatedIcosahedronSphere(3, true)); 
 		
+		//setting the file name of the custom GLSL shader by writing it into the Appearance
 		String[] source = new String[2];
 		source[0] = "custom/brick.vert";
 		source[1] = "custom/brick.frag";
 		ap.setAttribute("polygonShader.glsl330-source", source);
 		
-//		try {
-//			brickProg = new GlslProgram(ap, "polygonShader",   
-//					Input.getInput("de/jreality/jogl/shader/resources/brick.vert"),
-//					Input.getInput("de/jreality/jogl/shader/resources/brick.frag")
-//			    );
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+		//setting all uniform variables by writing them into the Appearance
 		double[] brickSize = {.2, .25};
 		double[] brickPct = {.5, .75};
 		double[] mortarPct = new double[2];
 		double[] lightPosition = {-2,0,4};
 		mortarPct[0] = 1.0 - brickPct[0];
 		mortarPct[1] = 1.0 - brickPct[1];
-		
-		
 		ap.setAttribute("polygonShader.SpecularContribution", .5);
 		ap.setAttribute("polygonShader.DiffuseContribution", 1.0);
-		
 		ap.setAttribute("polygonShader.BrickColor", new double[]{.2,0f,.8});
 		ap.setAttribute("polygonShader.MortarColor", new double[] {.8,1f,.2});
-		
 		ap.setAttribute("polygonShader.BrickSize", brickSize);
-		
 		ap.setAttribute("polygonShader.BrickPct", brickPct);
 		ap.setAttribute("polygonShader.MortarPct", mortarPct);
 		ap.setAttribute("polygonShader.LightPosition", lightPosition);
+		
 		JRViewer.display(world);
 	}
 }
