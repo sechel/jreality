2bdcf6690c514af41a2ce835caa35abd784dc3fe
Stefan Sechelmann <sechel@math.tu-berlin.de>
2013-12-17 11:42:45 +0100

    SpaceExplorer works, probably only for my harware setting. updated jinput library

--- src-jinput/de/jreality/toolsystem/raw/DetectControllers.java
+++ src-jinput/de/jreality/toolsystem/raw/DetectControllers.java
@@ -2,11 +2,14 @@ package de.jreality.toolsystem.raw;
 
 import java.util.Arrays;
 
+import de.jreality.util.NativePathUtility;
+
 import net.java.games.input.Controller;
 import net.java.games.input.ControllerEnvironment;
 
 public class DetectControllers {
 	public static void main(String[] args) {
+		NativePathUtility.set("jni");
 		ControllerEnvironment env = ControllerEnvironment.getDefaultEnvironment();
 		Controller[] controllers = env.getControllers();
 		for (Controller ctrl : controllers) {
